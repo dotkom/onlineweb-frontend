5e7357db1e215368a61d2a1c396a64ee0694c922
oleast <oleast@stud.ntnu.no>
2018-05-31 18:49:54 +0200

    Update main application and configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Frontpage from './frontpage';
+import Career from './career/';
+import Hobbys from './hobbygroups';
+import Resources from './resources';
+import NotFound from './core/components/NotFound';
+import Core from './core';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+export const routes = {
+  events: '/events',
+  home: '/',
+  career: '/career',
+  hobbygroups: '/hobbygroups',
+  resources: '/resources',
+  wiki: '/wiki',
+  webshop: '/webshop',
+}
+
+export const App = () => {
   return (
-    <p>Hello, world!</p>
+    <Router>
+      <Core>
+        <Switch>
+          <Route exact path={routes.home} component={Frontpage} />
+          <Route path={routes.career} component={Career} />
+          <Route path={routes.hobbygroups} component={Hobbys} />
+          <Route path={routes.resources} component={Resources} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Core>
+    </Router>
   );
 }
 
