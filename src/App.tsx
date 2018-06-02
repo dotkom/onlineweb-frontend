0d8d3a265c58216cce6e6a93aafbccce5e68d0c3
oleast <oleast@stud.ntnu.no>
2018-06-02 23:38:06 +0200

    Fix toQueryObject and add profile route to main router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Frontpage from './frontpage';
 import Career from './career/';
 import Hobbys from './hobbygroups';
 import Resources from './resources';
 import NotFound from './core/components/NotFound';
 import Core from './core';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Profile from './profile';
 
 export const routes = {
   events: '/events',
@@ -15,6 +16,7 @@ export const routes = {
   resources: '/resources',
   wiki: '/wiki',
   webshop: '/webshop',
+  profile: '/profile'
 }
 
 export const App = () => {
@@ -26,6 +28,7 @@ export const App = () => {
           <Route path={routes.career} component={Career} />
           <Route path={routes.hobbygroups} component={Hobbys} />
           <Route path={routes.resources} component={Resources} />
+          <Route path={routes.profile} component={Profile} />
           <Route path="*" component={NotFound} />
         </Switch>
       </Core>
