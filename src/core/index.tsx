4ecaed27b0f238d3f95db88bafea12b847c451f5
oleast <oleast@stud.ntnu.no>
2018-06-17 11:06:00 +0200

    Small restructure of Core index

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,21 +1,9 @@
 import React, { ReactNode } from 'react';
-import store from 'core/store';
-import { logIn } from './actions';
 import './less/core.less'
 import { Link } from 'react-router-dom';
 import { routes } from 'App';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-declare global {
-  interface Window {
-    store: any;
-    logIn: Function;
-  }
-};
-
-window.store = store;
-window.logIn = logIn;
+import Header from './components/Header/index';
+import Footer from './components/Footer/index';
 
 const Core = ({ children }: any) => (
   <div>
