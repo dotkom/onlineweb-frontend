df0b117964c1abb1ab833b14c453433fb7303a22
oleast <oleast@stud.ntnu.no>
2018-06-17 11:21:06 +0200

    Start building reducer for router, not used currently

---
+++ src/reducers/routerReducer.ts
@@ -0,0 +1,29 @@
+import rootReducer from "core/reducers";
+
+export enum ActionType {
+  LOCATION_CHANGE
+}
+
+export interface IAction<T> {
+  type: ActionType;
+  payload: T;
+}
+
+export interface IState {
+  location: any;
+}
+
+export const initialState: IState = {
+  location: {},
+}
+
+const routerReducer = (state: IState = initialState, action: IAction<IState>) => {
+  switch(action.type) {
+    case(ActionType.LOCATION_CHANGE): 
+      return { ...state, ...action.payload };
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;
