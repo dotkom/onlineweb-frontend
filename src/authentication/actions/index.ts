42fe8af7e64e2cdb248bf099c97d933797a8b0f4
oleast <oleast@stud.ntnu.no>
2018-06-17 10:59:24 +0200

    Add redux to authentication

---
+++ src/authentication/actions/index.ts
@@ -0,0 +1,11 @@
+import { ActionType, IAction } from '../reducers';
+import { logIn } from '../api';
+import { IAuthUser } from '../models/User';
+
+export const apiLogIn =
+  async (username: string, password: string): Promise<IAction<IAuthUser>> => {
+    return {
+      type: ActionType.LOG_IN,
+      payload: await logIn(username, password)
+    }
+  };
---
+++ src/authentication/reducers/index.ts
@@ -0,0 +1,42 @@
+import { IAuthUser } from "../models/User";
+
+export enum ActionType {
+  LOG_IN,
+  LOG_OUT
+}
+
+export interface IAction<T> {
+  type: ActionType;
+  payload: T;
+}
+
+export interface IState {
+  user: IAuthUser;
+}
+
+export const initialState: IState = {
+  user: {
+    first_name: 'anon',
+    last_name: 'user',
+    username: 'anonymoususer',
+    email: 'anon@online.ntnu.no',
+    groups: [
+      { name: 'None', permissions: []}
+    ],
+    permissions: ['view_wiki'],
+    field_of_study: 0
+  }
+}
+
+const rootReducer = (state: IState = initialState, action: IAction<IState>) => {
+  switch(action.type) {
+    case ActionType.LOG_IN:
+      return { ...state, ...action.payload };
+    case ActionType.LOG_OUT:
+      return { ...state, user: initialState.user };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
