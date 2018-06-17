83bcf486623e18f29ccec3a6bd6da74e02c89310
oleast <oleast@stud.ntnu.no>
2018-06-17 11:16:02 +0200

    Build profile privacy system

---
+++ src/profile/models/Privacy.ts
@@ -0,0 +1,15 @@
+export interface IPrivacy {
+  visible_for_other_users: boolean;
+  expose_nickname: boolean;
+  expose_email: boolean;
+  expose_phone_number: boolean;
+  expose_address: boolean;
+}
+
+export const translations: { [Key in keyof IPrivacy]: string } = {
+  visible_for_other_users: 'Synlig for andre brukere',
+  expose_nickname: 'Vis brukernavn',
+  expose_email: 'Vis epostadresse',
+  expose_phone_number: 'Vis telefonnummer',
+  expose_address: 'Vis adresse'
+}
