4e8a6a408a9ab43c5e5322f9fa675ab635ee3d81
oleast <oleast@stud.ntnu.no>
2018-06-17 11:02:09 +0200

    Rework common models

--- src/common/models/Date.ts
+++ src/common/models/Date.ts
@@ -1,2 +1,12 @@
 
+/**
+ * Date formatted as a string:
+ * E.g. "2000-12-31T23:59:01"
+ */
 export type IsoDateTime = string;
+
+/**
+ * Date formatted as a string:
+ * E.g. "2000-12-31"
+ */
+export type IsoDate = string;
