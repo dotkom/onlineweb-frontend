7c88373302be2ac557331ed11a3ef8c1d44de8ca
oleast <oleast@stud.ntnu.no>
2018-06-17 10:58:25 +0200

    Begin authentication app

---
+++ src/authentication/index.tsx
@@ -0,0 +1,18 @@
+
+import { createStore, applyMiddleware } from 'redux';
+import asyncAwait from 'redux-async-await';
+import store from "./store";
+import { apiLogIn } from "./actions";
+import rootReducer from './reducers'
+
+declare global {
+  interface Window {
+    store: any;
+    apiLogIn: Function;
+  }
+}
+
+window.store = applyMiddleware(asyncAwait)(createStore)(rootReducer);
+window.apiLogIn = apiLogIn;
+
+export default store;
