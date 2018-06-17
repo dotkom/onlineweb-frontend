39aa39c98760adc22697beb91b41848cfe3bea3a
oleast <oleast@stud.ntnu.no>
2018-06-17 11:00:01 +0200

    Add authentication component

---
+++ src/authentication/components/Authenticate.tsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import { IGroup } from 'core/models/Group';
+import { connect } from 'react-redux';
+import { IState } from '../reducers';
+import { Permission } from 'core/models/Permission';
+import { AuthUser } from '../models/User';
+
+export interface Props extends IState {
+  user: AuthUser
+  children: JSX.Element;
+  alt?: JSX.Element;
+  authentication: IGroup | Permission;
+}
+
+export const mapStateToProps = (state: IState): IState => {
+  return { user: new AuthUser(state.user) };
+}
+
+export const mapDispatchToProps = (dispatch: Function) => ({/** Not going to change the state for user */})
+
+const GroupAccess = ({ children, authentication, user, alt = null }: Props) => (
+  <Fragment>
+    { user.hasPermission(authentication) ? children : alt }
+  </Fragment>
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(GroupAccess)
