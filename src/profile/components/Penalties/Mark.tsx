6f5e421032b5e488b4519716b7c98698bb530d10
oleast <oleast@stud.ntnu.no>
2018-06-24 20:51:38 +0200

    Display penalties with more than just text..

--- src/profile/components/Penalties/Mark.tsx
+++ src/profile/components/Penalties/Mark.tsx
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
 import Penalty from './Penalty';
 import { IMark } from '../../models/Penalty';
+import { DateTime } from 'luxon';
 
 class Mark extends Penalty<IMark> {
   render() {
     const { penalty } = this.props;
     const { collapsed } = this.state;
+    const expiration = DateTime.fromISO(penalty.expiration_date)
+    const added = DateTime.fromISO(penalty.added_date)
     return (
-      <div className="row row-space">
+      <div className="grid-row">
         <div className="col-md-12" onClick={() => this.toggleCollapse()}>
           <h4>
             { penalty.title }
@@ -18,11 +21,11 @@ class Mark extends Penalty<IMark> {
             { /** TIL: (<></>) === (<Fragment></Fragment>) */ }
               <p>{ penalty.description }</p>
               <p><b>Katogori: </b>{ penalty.category }</p>
-              <p><b>Utløpsdato: </b>{ penalty.expiration_date }</p>
-              <p><b>{ this.getPenaltyCompletion(penalty) }</b></p>
+              <p><b>Utløpsdato: </b>{ expiration.toFormat('d MMMM y') }</p>
             </>
           }
         </div>
+        <div className="progress-bar" style={{ width: this.getPenaltyCompletion(penalty) + '%' }} />
       </div>
     )
   }
