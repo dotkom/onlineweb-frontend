2bedde8a1aaecaeabcd378eda946bff8a6cbe4a6
oleast <oleast@stud.ntnu.no>
2018-06-29 22:17:49 +0200

    Decouple Event ListView

--- src/events/components/ListView/index.tsx
+++ src/events/components/ListView/index.tsx
@@ -1,39 +1,44 @@
 import React, { Component } from 'react';
-import { IFrontpageEvent } from '../../models/Event';
+import { INewEvent, EventViewProps, getEventColor, getEventType } from '../../models/Event';
 import { DateTime } from 'luxon';
 import './list.less';
-
-export interface IProps {
-  events: IFrontpageEvent[];
-}
+import { getEvents } from '../../api/events';
 
 export interface IState {
-
+  events: INewEvent[];
 }
 
-class ListView extends Component<IProps, IState> {
+class ListView extends Component<EventViewProps, IState> {
+  state: IState = {
+    events: []
+  }
+
+  public async componentDidMount() {
+    const events = await getEvents();
+    this.setState({ events });
+  }
+
   public render() {
-    const { events } = this.props;
+    const { events } = this.state;
     return (
       <>
       <div className="event-list-grid">
-        { events.map((event) => <ListEvent {...event} />) }
+        { events.map((event) => <ListEvent key={event.id} {...event} />) }
       </div>
-      <div className="box" />
       </>
     )
   }
 }
 
 
-const ListEvent = ({ title, startDate, event_start }: IFrontpageEvent) => (
+const ListEvent = ({ title, event_start, attendance_event, event_type }: INewEvent) => (
   <div className="event-list-element-grid event-list-grid-row">
     { /** First one has to be 1/6 of the line? */ }
-    <div className="event-list-element-grid-row"><p>{ 'Bedkom' }</p></div>
+    <div style={{ background: getEventColor(event_type) }} className="event-list-element-grid-row"><p>{ getEventType(event_type) }</p></div>
     <div className="event-list-element-grid-row"><p>{ title }</p></div>
-    <div className="event-list-element-grid-row"><p>{ '23/50' }</p></div>
-    <div className="event-list-element-grid-row"><p>{ DateTime.fromISO(startDate).toFormat('d.MM') }</p></div>
-    <div className="event-list-element-grid-row"><p>{ 'Y' }</p></div>
+    <div className="event-list-element-grid-row"><p>{ attendance_event && attendance_event.attendees ? `${attendance_event.attendees.length}/${attendance_event.max_capacity}` : 'ALLE'}</p></div>
+    <div className="event-list-element-grid-row"><p>{ DateTime.fromISO(event_start).toFormat('d.MM') }</p></div>
+    <div className="event-list-element-grid-row"><p>{ attendance_event ? 'Y' : 'N' }</p></div>
   </div>
 )
 
