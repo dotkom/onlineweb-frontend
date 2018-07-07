7325e74bb46bab2ce32831cad489954325b3226c
Torjus Iveland <torjusti@gmail.com>
2018-07-07 20:09:09 +0200

    Make the event type boxes arrow-shaped

--- src/events/components/ListView/index.tsx
+++ src/events/components/ListView/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classNames from 'class-names';
 import { INewEvent, EventViewProps, getEventColor, getEventType } from '../../models/Event';
 import { DateTime } from 'luxon';
 import './list.less';
@@ -34,7 +35,16 @@ class ListView extends Component<EventViewProps, IState> {
 const ListEvent = ({ title, event_start, attendance_event, event_type }: INewEvent) => (
   <div className="event-list-element-grid event-list-grid-row">
     { /** First one has to be 1/6 of the line? */ }
-    <div style={{ background: getEventColor(event_type) }} className="event-list-element-grid-row"><p>{ getEventType(event_type) }</p></div>
+    <div className="event-list-element-grid-row">
+      <div className="event-type" style={{
+          background: getEventColor(event_type),
+          color: getEventColor(event_type),
+        }}
+      >
+        <p>{getEventType(event_type)}</p>
+      </div>
+    </div>
+
     <div className="event-list-element-grid-row"><p>{ title }</p></div>
     <div className="event-list-element-grid-row"><p>{ attendance_event && attendance_event.attendees ? `${attendance_event.attendees.length}/${attendance_event.max_capacity}` : 'ALLE'}</p></div>
     <div className="event-list-element-grid-row"><p>{ DateTime.fromISO(event_start).toFormat('d.MM') }</p></div>
