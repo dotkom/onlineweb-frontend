19f740cf43beb7ac39e7b7bd584de69bc25a4c5e
oleast <oleast@stud.ntnu.no>
2018-09-17 14:37:27 +0200

    Add method for getting all pages of an event api request

--- src/events/api/events.ts
+++ src/events/api/events.ts
@@ -1,18 +1,49 @@
 import { get } from 'common/utils/api';
 import { EventType, getEventType, INewEvent } from '../models/Event';
+import { getPackedSettings } from 'http2';
 
 export interface IEventAPIArguemnts {
-  event_end__gte?: string;
-  event_start__gte?: string;
+  event_period_start?: string;
+  event_period_end?: string;
   event_type?: string[] | string
   page?: number;
 }
 
+export interface IAPIData<T> {
+  results: T[];
+  next: string | null;
+  previous: string | null;
+  count: number;
+}
+
 const API_URL = '/api/v1/events/';
 
-export const getEvents = async (args?: IEventAPIArguemnts): Promise<INewEvent[]> => {
-  const { results }: { results:  INewEvent[] } = await get(API_URL, { format: 'json', ...args });
+export const getEvents = async (args?: IEventAPIArguemnts, disablePagination = false): Promise<INewEvent[]> => {
+  const data: IAPIData<INewEvent> = await get(API_URL, { format: 'json', ...args });
+  return data.results;
+}
+
+export const getAllEvents = async (args?: IEventAPIArguemnts): Promise<INewEvent[]> => {
+  const data: IAPIData<INewEvent> = await get(API_URL, { format: 'json', ...args });
+  let { results } = data;
+  let nextPage = data.next;
+  if (nextPage) {
+    let next: IAPIData<INewEvent>
+    for await(next of getPages(nextPage)) {
+      results = [...results, ...next.results];
+      if (!next.next) { break };
+      nextPage = next.next
+    }
+  }
   return results;
+}
+
+async function* getPages(page: string) {
+  while(true) {
+    const response = await fetch(page);
+    const data = await response.json();
+    yield data;
+  }
 }
 
 export const getEvent = async (id: number): Promise<INewEvent> => {
