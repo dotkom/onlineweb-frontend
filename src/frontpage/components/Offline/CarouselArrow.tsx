065b1260fc26650250a1fc33a30f49baf72f93ae
oleast <oleast@stud.ntnu.no>
2018-10-06 23:12:05 +0200

    Start creating new Frontpage Offline Carousel

---
+++ src/frontpage/components/Offline/CarouselArrow.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export interface IProps {
+  direction: 'right' | 'left';
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+const CarouselArrow = ({ direction, onClick, disabled = false }: IProps) => (
+  <button onClick={onClick} disabled={disabled}>
+    { direction === 'left' ? '<' : null }
+    { direction === 'right' ? '>' : null }
+  </button>
+)
+
+export default CarouselArrow;
---
+++ src/frontpage/components/Offline/OfflineCarousel.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { IOfflineIssue } from '../../models/Offline';
+import style from './carousel.less';
+
+export interface IProps {
+  offlines: IOfflineIssue[];
+}
+
+const IMAGE_SUFFIX = '.thumb.png'
+
+const OfflineCarousel = ({ offlines }: IProps) => (
+  <div className={style.container}>
+    { offlines.map((offline) => <CarouselItem key={offline.id} {...offline}/>) }
+  </div>
+)
+
+const CarouselItem = ({ description, id, issue, release_date, title }: IOfflineIssue) => {
+  const thumbnail = issue + IMAGE_SUFFIX;
+  return (
+    <div>
+      <a href={ issue }>
+        <img src={ thumbnail } />
+        <p>{ title }</p>
+      </a>
+    </div>
+  )
+}
+
+export default OfflineCarousel;
