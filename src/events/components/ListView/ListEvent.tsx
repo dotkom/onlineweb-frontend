9911c3085724c819e0b6b503deedd4ab0ed41897
oleast <oleast@stud.ntnu.no>
2018-10-07 13:37:25 +0200

    Try usage of optional chaining in ListView for events, a bit broken with typescript, works with babel

--- src/events/components/ListView/ListEvent.tsx
+++ src/events/components/ListView/ListEvent.tsx
@@ -5,15 +5,17 @@ import HostPolygon from './HostPolygon';
 import StatusPolygon from './StatusPolygon';
 import style from './list.less';
 
-const ListEvent = ({ title, event_start, attendance_event, event_type }: INewEvent) => {
+const ListEvent = ({ title, event_start, attendance_event, event_type, company_event }: INewEvent) => {
   const eventColor = getEventColor(event_type);
   const eventType = getEventType(event_type);
   const eventDate = DateTime.fromISO(event_start).toFormat('d.MM')
+  console.log(company_event ? company_event : null)
   return (
     <div className={style.gridRow}>
       <div className={style.elementGridRow} style={{ left: 0 }}>
         <HostPolygon color={ eventColor } />
-        <p>{ eventType }</p>
+        { /* Optional chainging, tc39 stage 1 proposal. Not supported by Typescript  */ }
+        <p>{ company_event[0]?.company.name || eventType }</p>
       </div>
 
       <div className={style.elementGridRow}>
@@ -21,8 +23,9 @@ const ListEvent = ({ title, event_start, attendance_event, event_type }: INewEve
       </div>
       <div className={style.elementGridRow}>
         <p>
-          { (attendance_event && attendance_event.attendees)
-            ? `${attendance_event.attendees.length}/${attendance_event.max_capacity}`
+          { /* Optional chainging, tc39 stage 1 proposal. Not supported by Typescript  */ }
+          { attendance_event
+            ? `${attendance_event?.attendees?.length || 0}/${attendance_event.max_capacity}`
             : 'ALLE'
           }
         </p>
