e10289e76bb1de24289c220c015cec120efc6cf6
oleast <oleast@stud.ntnu.no>
2018-10-07 16:03:07 +0200

    Tweak height of ListEvent

--- src/events/components/ListView/HostPolygon.tsx
+++ src/events/components/ListView/HostPolygon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HostPolygon = ({ color }: { color: string }) => (
-  <svg width="220" height="60">
+  <svg viewBox="0 0 220 60" height="50">
     <path d="M200 60L220 30L200.5 0H0V60H200Z" fill={ color }/>
   </svg>
 );
