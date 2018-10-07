35b1600d36bcec96b517f3a91d851fd4e467e74a
oleast <oleast@stud.ntnu.no>
2018-10-07 13:38:47 +0200

    Update frontpage to use new Article components and standard header components

--- src/frontpage/index.tsx
+++ src/frontpage/index.tsx
@@ -1,38 +1,34 @@
 import React, { Fragment } from 'react';
 import ReactDom from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import ArticlesContainer from 'articles/containers/ArticlesContainer';
 import EventsContainer from 'events/components/EventsContainer';
 import Offline from './components/Offline';
 import Business from './components/Business';
 import Subnav from './components/Subnav';
+import Header from './components/Header';
 import './initFrontpage';
 import './less/frontpage.less';
 import '../career/less/career.less';
 import { IFrontpageEvent } from 'events/models/Event';
 import { IArticle } from 'articles/models/Article';
+import Articles from './components/Articles';
 
 const Frontpage = () => (
   <Fragment>
-    <AppContainer>
-      <div className="container">
-        <section id="events">
-          <EventsContainer />
-        </section>
-      </div>
-    </AppContainer>
-    <AppContainer>
-      <div className="container">
-        <section id="articles">
-          <ArticlesContainer />
-        </section>
-      </div>
-    </AppContainer>
     <div className="container">
-      <Offline />
+      <section id="events">
+        <EventsContainer />
+      </section>
     </div>
     <div className="container">
-      <Business />
+      <section id="articles">
+      <Header title="artikler"/>
+        <Articles />
+      </section>
+    </div>
+    <div className="container">
+      <Header title="offline" />
+      <Offline />
     </div>
   </Fragment>
 );
