54502386ec69f84ca3aefaac1dcc7a2d8acda7fd
oleast <oleast@stud.ntnu.no>
2018-10-07 13:36:24 +0200

    Update Articles API to add APIData wrapper

--- src/articles/api/index.ts
+++ src/articles/api/index.ts
@@ -1,8 +1,9 @@
-import { get } from 'common/utils/api';
+import { get, IAPIData } from 'common/utils/api';
+import { IArticle } from '../models/Article';
 
 const API_URL = '/api/v1/articles/';
 
-export const getArticles = async () => {
-  const data = await get(API_URL, { format: 'json' });
-  return data;
+export const getArticles = async (): Promise<IArticle[]> => {
+  const data: IAPIData<IArticle> = await get(API_URL, { format: 'json' });
+  return data.results;
 }
