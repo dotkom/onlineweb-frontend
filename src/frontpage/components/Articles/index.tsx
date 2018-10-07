2ade79ea154c42cd704cc614f804c9eb3cacd9e0
oleast <oleast@stud.ntnu.no>
2018-10-07 13:29:41 +0200

    Create new frontpage Articles view

---
+++ src/frontpage/components/Articles/index.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { getArticles } from 'articles/api';
+import { IArticle } from 'articles/models/Article';
+import MainArticle from './MainArticle';
+import SmallArticle from './SmallArticle';
+import style from './articles.less'
+
+export interface IProps {
+
+}
+
+export interface IState {
+  articles: IArticle[];
+  index: number;
+}
+
+const DISPLAY_NUMBER = 3
+
+class Articles extends Component<IProps, IState> {
+  state: IState = {
+    articles: [],
+    index: 0
+  }
+
+  public async componentDidMount() {
+    const articles = await getArticles();
+    this.setState({ articles })
+  }
+
+  public render() {
+    const { articles, index } = this.state;
+    const start = index + 1;
+    const end = start + DISPLAY_NUMBER;
+    return (
+      <div className={style.container}>
+        { articles.length ? <>
+          <MainArticle {...articles[index]} />
+          <div className={style.smallContainer}>
+            { articles.slice(start, end).map((article, i) => <SmallArticle key={article.absolute_url} {...article} />) }
+          </div>
+        </> : null }
+      </div>
+    )
+  }
+}
+
+export default Articles;
