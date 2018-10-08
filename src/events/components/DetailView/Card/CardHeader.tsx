5a6b09e67dde113f5b94d23321a1b1dc31f2c3d9
oleast <oleast@stud.ntnu.no>
2018-10-08 18:27:03 +0200

    Add basic header for event detail cards

---
+++ src/events/components/DetailView/Card/CardHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './card.less';
+
+export interface IProps extends React.HTMLAttributes<HTMLHeadingElement> {
+  text?: string;
+  color?: string;
+}
+
+const CardHeader = ({ text, children, color = '#454545', ...props }: IProps) => (
+  <p {...props} className={style.cardHeader} style={{ color }}>
+    { children || text }
+  </p>
+)
+
+export default CardHeader;
