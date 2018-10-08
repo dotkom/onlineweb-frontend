ab8cc9fe8ebda992c1171303441650fb26238e99
oleast <oleast@stud.ntnu.no>
2018-10-08 14:59:50 +0200

    Add localStorage for EventView settings

---
+++ src/events/api/eventSettings.ts
@@ -0,0 +1,28 @@
+import { EventView } from "../models/Event";
+
+/**
+ * EventSettings
+ * Settings for the event views.
+ */
+
+const EVENT_SETTINGS_KEY = 'event-settings'
+
+export interface IEventSettings {
+  view: EventView;
+  accessible: boolean;
+}
+
+const DEFAULT_SETTINGS: IEventSettings = {
+  view: EventView.IMAGE,
+  accessible: false,
+}
+
+export const getEventSettings = async (): Promise<IEventSettings> => {
+  const settings = await window.localStorage.getItem(EVENT_SETTINGS_KEY);
+  return settings ? JSON.parse(settings) : DEFAULT_SETTINGS;
+}
+
+export const saveEventSettings = async (settings: IEventSettings) => {
+  const str = JSON.stringify(settings);
+  await window.localStorage.setItem(EVENT_SETTINGS_KEY, str);
+}
