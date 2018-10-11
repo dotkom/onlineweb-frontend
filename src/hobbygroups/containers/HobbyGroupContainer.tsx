d1626e6e9b57d2c99e9ca43bce743dd69737d538
oleast <oleast@stud.ntnu.no>
2018-10-11 17:13:50 +0200

    Refactor hobbygroups to use css modules and remove bootstrap

--- src/hobbygroups/containers/HobbyGroupContainer.tsx
+++ src/hobbygroups/containers/HobbyGroupContainer.tsx
@@ -1,40 +1,16 @@
 import React from 'react';
 import HobbyGroupList from '../components/HobbyGroupList';
-import Authenticate from 'authentication/components/Authenticate';
-import HttpError from 'core/components/errors/HttpError';
+import Header from 'frontpage/components/Header';
+import Intro from '../components/Intro';
 
 const HobbyGroupContainer = () => (
-  <Authenticate authentication="view_hobbys" alt={<HttpError code={404} />}>
-    <section id="hobbygroups">
-      <div className="container">
-        <div className="col-md-12">
-          <div className="page-header">
-            <h1 id="resourcecenter-heading">Interessegrupper</h1>
-          </div>
-        </div>
-
-        <div className="row" id="introduction">
-          <div className="col-xs-12 col-sm-12 col-md-12">
-            <div className="col-md-12">
-              <p>
-                På denne siden finner du informasjon om alle de forskjellige interessegruppene i online.
-                Ser du noe som ser interessant ut? Ta kontakt og møt noen med samme interesser som deg.
-                Interessegruppene i Online er grupper for alle mulige slags interesser.
-                Har du og en kompis eller to en sær/stilig/fantastisk interesse? Opprett en interessegruppe!
-                Da kan du finne flere som deler din interesse og har mulighet til å søke om støtte fra hovedstyre.
-              </p>
-              <hr />
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <HobbyGroupList />
-        </div>
-
-      </div>
-    </section>
-  </Authenticate>
+  <section id="hobbygroups">
+    <div className="container">
+      <Header>Interessegrupper</Header>
+      <Intro />
+      <HobbyGroupList />
+    </div>
+  </section>
 );
 
 export default HobbyGroupContainer;
--- src/hobbygroups/index.tsx
+++ src/hobbygroups/index.tsx
@@ -1,4 +1,3 @@
 import Hobbys from './containers/HobbyGroupContainer';
-import './less/hobbygroups.less';
 
 export default Hobbys;
