9ece775ef727daa29b944298f8499af929b1c9f6
oleast <oleast@stud.ntnu.no>
2018-10-11 18:54:16 +0200

    Create css module version of the career detail view

--- src/career/components/InfoOnOpportunity.tsx
+++ src/career/components/InfoOnOpportunity.tsx
@@ -3,6 +3,9 @@ import Markdown from 'react-markdown';
 import { formatLocations } from './Job';
 import style from '../less/career.less';
 import globalStyle from 'core/less/core.less';
+import Header from 'frontpage/components/Header';
+import { Link } from 'react-router-dom';
+import Img from 'common/components/Img';
 
 export interface IInfoBox {
   title: string;
@@ -17,68 +20,30 @@ export interface IInfoBox {
 };
 
 const InfoBox = (props: IInfoBox) => (
-  <section className={style.careeropportunities}>
+  <section>
     <div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <div className={globalStyle.pageHeader}>
-            <h2>{props.title}</h2>
-          </div>
-        </div>
-      </div>
+      <Header>{ props.title }</Header>
 
-      <div className="row">
-        <div className={`col-md-8 col-sm-7 ${style.careerdescription}`}>
-          <Markdown
-            source={props.description}
-            escapeHtml
-          />
+      <div className={style.detail}>
+        <div className={style.jobDescription}>
+          <Markdown source={props.description} escapeHtml />
         </div>
-
-        <div className="col-md-4 col-sm-5">
-          <div className="row">
-            <div className="col-md-12">
-              <a href={`/company/${props.companyId}`}>
-                <picture>
-                  <source srcSet={props.companyImage.lg} media="(max-width: 992px)" />
-                  <img src={props.companyImage.md} width="100%" alt={props.companyName} />
-                </picture>
-              </a>
+        <div>
+          <div className={style.company}>
+            <Link to={`/company/${props.companyId}`}>
+              <Img src={props.companyImage.lg} alt={props.companyName}></Img>
+            </Link>
+            <div>
+              <div>
+                <Link to={`/company/${props.companyId}`}>
+                  <h3>{ props.companyName }</h3>
+                </Link>
+                <Markdown source={props.companyDescription} escapeHtml />
+              </div>
             </div>
-          </div>
-
-          <div className={style.company}>
-            <a href={`/company/${props.companyId}`}>
-              <div className="row">
-                <div className="col-md-12">
-                  <h3>{props.companyName}</h3>
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-md-12">
-                  <div className={style.companyIngress}>
-                    <Markdown
-                      source={props.companyDescription}
-                      escapeHtml
-                    />
-                  </div>
-
-                  <p className={`${globalStyle.pullRight} ${style.companyImageCaption}`}>Trykk for mer info</p>
-                </div>
-              </div>
-            </a>
-          </div>
-
-          <div className={style.company}>
-            <div className="row">
-              <div className="col-md-12">
+            <div>
+              <div>
                 <h3>Nøkkelinformasjon</h3>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col-md-12">
                 <p>Type: {props.type}</p>
                 <p>Sted: {formatLocations(props.locations)}</p>
                 <p>Frist: {props.deadline}</p>
