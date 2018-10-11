ef5086ffdc2801316b29621cc9440899302065bf
oleast <oleast@stud.ntnu.no>
2018-10-11 14:13:35 +0200

    Update basic app header with support for children as content

--- src/frontpage/components/Header/index.tsx
+++ src/frontpage/components/Header/index.tsx
@@ -1,14 +1,13 @@
 import React, { Props } from 'react';
 import style from './header.less';
 
-export interface IProps extends Props<IProps> {
-  title: string;
+export interface IProps extends React.HtmlHTMLAttributes<IProps> {
+  title?: string;
 }
 
 const Header = ({ title, children }: IProps) => (
   <div className={style.grid}>
-    <h3>{ title }</h3>
-    { children }
+    <h3>{ title || children }</h3>
   </div>
 )
 
