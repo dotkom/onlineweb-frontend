ad0e2eaa4a59ba547b6175d8d2eace6c6e7196a3
oleast <oleast@stud.ntnu.no>
2018-10-11 14:52:20 +0200

    Redefine resources with css-modules and non-bootstrap

--- src/resources/components/Resource.tsx
+++ src/resources/components/Resource.tsx
@@ -1,24 +1,16 @@
 import React, { Fragment } from 'react';
 import { IResource } from '../models/Resource';
 import ReactMarkdown from 'react-markdown';
+import style from './resources.less';
 
-const getImage = (resource: IResource) => {
-  return resource.image ? { src: resource.image.url } : { alt: resource.title };
-};
-
-const Resource = (resource: IResource) => {
-  const imageProps = getImage(resource);
-    return (
-    <div className="col-xs-12 col-sm-6 col-md-6">
-      <div className="col-md-5">
-        <img {...imageProps} />
-      </div>
-      <div className="col-md-7 resource-description">
-        <h3>{resource.title}</h3>
-        <span>
-          <ReactMarkdown source={resource.description} />
-        </span>
-      </div>
+const Resource = ({ title, description, image }: IResource) => {
+  return (
+    <div className={style.resource}>
+      <img src={image} alt={title} />
+      <span>
+        <h3>{title}</h3>
+        <ReactMarkdown source={description} />
+      </span>
     </div>
   )
 }
--- src/resources/components/ResourceList.tsx
+++ src/resources/components/ResourceList.tsx
@@ -2,6 +2,7 @@ import React,  { Component, Fragment } from 'react';
 import { getResources } from '../api';
 import { IResource } from '../models/Resource';
 import Resource from './Resource';
+import style from './resources.less';
 
 export interface IResourceListState {
   resources: IResource[];
@@ -18,15 +19,11 @@ export default class ResourceList extends Component<{}, IResourceListState> {
   render() {
     const { resources } = this.state;
     return(
-      <Fragment>
-        {this.state.resources.map((resource, index) => {
-          return index % 2 === 0 ? 
-            <div className="row" key={resource.title + resources[index].title}>
-              <Resource {...resource} />
-              <Resource {...resources[index + 1]} />
-            </div> : undefined
-        })}
-      </Fragment>
+      <div className={style.container}>
+        { resources.map((resource) => (
+          <Resource key={resource.title} {...resource} />
+        )) }
+      </div>
     );
   };
 };
