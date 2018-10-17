aa3508b08cfb640fee0d45ba2a3497de490d99d8
oleast <oleast@stud.ntnu.no>
2018-10-17 15:12:44 +0200

    initial tslint autofix articles app

--- src/articles/api/index.ts
+++ src/articles/api/index.ts
@@ -6,4 +6,4 @@ const API_URL = '/api/v1/articles/';
 export const getArticles = async (): Promise<IArticle[]> => {
   const data: IAPIData<IArticle> = await get(API_URL, { format: 'json' });
   return data.results;
-}
+};
