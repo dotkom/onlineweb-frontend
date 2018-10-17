87b53a546049383abae58b432f34e0a6d08080c7
oleast <oleast@stud.ntnu.no>
2018-10-17 15:18:09 +0200

    initial tslint autofix common app

--- src/common/components/Collapsible.tsx
+++ src/common/components/Collapsible.tsx
@@ -7,21 +7,23 @@ export interface IState {
 /**
  * @summary Component defining the ability to toggle displaying its content.
  * @description Component defines a method toggleCollapse that lets it content be collapsed and shown again by toggling.
- * To implement it, simply extend this Component, in the render method; use the toggleCollapse method on the trigger button.
- * Then implement a condition for which part(s) will be hidden, e.g. { this.state.collapsed ? <if-hidden> : <if-visible> }.
+ * To implement it, simply extend this Component.
+ * In the render method; use the toggleCollapse method on the trigger button.
+ * Then implement a condition for which part(s) will be hidden,
+ * e.g. { this.state.collapsed ? <if-hidden> : <if-visible> }.
  * @param {generic} T Props definition for the Component.
  */
 abstract class Collapsible<T> extends Component<T, IState> {
-  state = {
-    collapsed: true
-  }
+  public state = {
+    collapsed: true,
+  };
 
   /**
    * @summary Method from the abstract Component; Collapsible.
    * @description Method toggles the state of the Component to show or hide a given portion of its content.
    */
-  toggleCollapse() {
-    this.setState({ collapsed: !this.state.collapsed })
+  public toggleCollapse() {
+    this.setState({ collapsed: !this.state.collapsed });
   }
 }
 
