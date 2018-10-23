fec3a4bf6025ea215a746aa54211b77ac100582e
oleast <oleast@stud.ntnu.no>
2018-10-23 12:20:41 +0200

    Configure authentication api to use oidc manager, and set up authentication model for oidc

--- src/authentication/models/User.ts
+++ src/authentication/models/User.ts
@@ -1,75 +1,34 @@
 import { IGroup } from 'core/models/Group';
 import { Permission } from 'core/models/Permission';
+<<<<<<< 3e8809a5d36ae5659f867926f74f6e4cecd8fb2f
 import { Email, FieldOfStudy, IUser } from 'core/models/User';
+=======
+import { User } from 'oidc-client';
+>>>>>>> Configure authentication api to use oidc manager, and set up authentication model for oidc
 
-export interface IAuthUser extends IUser {
-  field_of_study: FieldOfStudy;
-  permissions: Permission[];
+export interface IAuthUser extends User {
+  profile: IAuthProfile;
 }
 
-export class AuthUser implements IAuthUser {
-  public first_name: string;
-  public last_name: string;
-  public username: string;
-  public email: Email;
-  public groups: IGroup[];
-  public field_of_study: FieldOfStudy;
-  public permissions: Permission[];
-
-  constructor(u: IAuthUser) {
-    this.first_name = u.first_name;
-    this.last_name = u.last_name;
-    this.username = u.username;
-    this.email = u.email;
-    this.groups = u.groups;
-    this.field_of_study = u.field_of_study;
-    this.permissions = u.permissions;
-  }
-
-  /**
-   * Get all the permissions of this user as a single array of Permission strings
-   * @returns {Permission[]}
-   */
-  public getAllPermissions(): Permission[] {
-    return (
-      this.groups
-        /** Create an Permission[][] */
-        .map((group: IGroup): Permission[] => group.permissions)
-        /** Join the given arrays to one array: Permission[] */
-        .reduce((accumulator, current) => accumulator.concat(current))
-        /** Add the other, non-group related permissions on the user */
-        .concat(this.permissions)
-        /** Remove duplicates by filtering */
-        .filter((permission, index, array) => array.indexOf(permission) === index)
-    );
-  }
-
-  /**
-   * Returns wether this user has the given permission or group permission.
-   * @param {IGroup | Permission} authentication Permission string or a Group to evaluate against
-   * @returns {boolean} Boolean value for is the user has the permission
-   */
-  public hasPermission(authentication: IGroup | Permission): boolean {
-    /** Check if authetication is a string, it is then a single permission */
-    if (typeof authentication === 'string') {
-      return (
-        this.getAllPermissions()
-          /** Check if the given Permission[] includes the required authentication */
-          .includes(authentication)
-      );
-
-      /** If authentication is not a string */
-    } else if (authentication.name) {
-      return (
-        this.groups
-          /** Map to an array of group names */
-          .map((group) => group.name)
-          /** Check if the given groups name is in the group names for the user */
-          .includes(authentication.name)
-      );
-    } else {
-      /** If wrong argument is given, return false */
-      return false;
-    }
-  }
+/**
+ * Profile informastion returned from authenticating with
+ * OpenID Connect to Onlineweb4.
+ */
+export interface IAuthProfile {
+  /** Unix timestamp when the user was authenticated */
+  auth_time: number;
+  /** Last name of the user */
+  family_name: string;
+  /** First name(s) of the user */
+  given_name: string;
+  /** Full name of the user */
+  name: string;
+  /** Personally set nickname */
+  nickname: string;
+  /** Full URL to profile picture */
+  picture: string;
+  /** Onlineweb username */
+  preferred_username: string;
+  /** Onlineweb UserId */
+  sub: string;
 }
