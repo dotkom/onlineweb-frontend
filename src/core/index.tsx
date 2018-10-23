b4bf4b7f2112f37e4e9999f6577b59514f48b318
oleast <oleast@stud.ntnu.no>
2018-10-23 19:22:03 +0200

    Remove unused imports in app entrypoints

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,18 +1,11 @@
-import React, { ReactNode } from 'react';
-// import './less/core.less'
-import { Link } from 'react-router-dom';
-import { routes } from 'App';
+import React from 'react';
 import Header from './components/Header/index';
 import Footer from './components/Footer/index';
 
 const Core = ({ children }: any) => (
   <div style={{ backgroundColor: '#f3f3f3' }}>
     <Header />
-    {/*% block submenu %}{% endblock %*/}
-    {/*% if messages %*/}
-    {/*% block content %}{% endblock %*/}
     { children }
-    <div className="container" id="isloading-component"></div>
     <Footer />
   </div>
 );
