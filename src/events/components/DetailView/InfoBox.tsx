94b5130022ac63e1612286bcd2e52afeb497ec1d
Torjus Iveland <torjusti@gmail.com>
2018-10-25 15:55:04 +0200

    Add ReactMarkdown and render Markdown in event descriptions

--- src/events/components/DetailView/InfoBox.tsx
+++ src/events/components/DetailView/InfoBox.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 import { INewEvent } from '../../models/Event';
 import style from './detail.less';
 
 const InfoBox = ({ description, ingress }: INewEvent) => (
   <div className={style.infoBox}>
     <div className={style.cardMargin}>
-      <p className={style.infoBoxHeader}>{ ingress }</p>
-      <p className={style.infoBoxContent}>{ description }</p>
+      <p className={style.infoBoxHeader}>
+        <ReactMarkdown source={ingress} />
+      </p>
+
+      <p className={style.infoBoxContent}>
+        <ReactMarkdown source={description} />
+      </p>
     </div>
   </div>
 );
