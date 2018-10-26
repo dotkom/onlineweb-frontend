7148108b191f853a55d9fb0c26ddcd3999abe056
sardred <andelar96@gmail.com>
2018-10-26 14:28:12 +0200

    Added first image from companies if image is null

--- src/events/components/ImageView/index.tsx
+++ src/events/components/ImageView/index.tsx
@@ -6,6 +6,7 @@ import {
   getEventType,
   EventTypeEnum,
   IAttendanceEvent,
+  ICompanyEvent,
 } from '../../models/Event';
 import { DateTime } from 'luxon';
 import style from './image.less';
@@ -22,9 +23,11 @@ export interface IState {
   fetched: boolean;
 }
 
-const getEventImage = (image: IImage | null): string => {
+const getEventImage = (image: IImage | null, company_event: ICompanyEvent[]) => {
   return image
-  ? (DOMAIN + image.wide)
+  ? DOMAIN + image.wide
+  : company_event[0]
+  ? DOMAIN + company_event[0].company.image.wide
   : 'https://online.ntnu.no/media/images/responsive/md/86b20aca-4368-4b3a-8f10-707c747eb03f.png';
 };
 
@@ -102,13 +105,13 @@ const SmallEventColumn = ({ events }: { events: INewEvent[] }) => {
   );
 };
 
-const LargeEvent = ({ image, event_type, title, event_start, attendance_event, id }: INewEvent) => (
+const LargeEvent = ({ image, event_type, title, event_start, attendance_event, id, company_event }: INewEvent) => (
   <Link to={`/events/${id}`}>
     <div className={style.large}>
       <p className={style.imageLargeType} style={{ background: getEventColor(event_type) }}>
         { getEventType(event_type) }
       </p>
-      <img className={style.largeImage} src={getEventImage(image)} />
+      <img className={style.largeImage} src={getEventImage(image, company_event)} />
       <div className={style.largeContent}>
         <p> { title } </p>
         <p> { getEventAttendees(attendance_event) } </p>
