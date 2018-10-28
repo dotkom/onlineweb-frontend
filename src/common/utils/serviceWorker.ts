dc612d59357feee886ebc072b1ba2440b0ada964
oleast <oleast@stud.ntnu.no>
2018-10-28 16:32:18 +0100

    Begin creating wrapper for browser ServiceWorkers

---
+++ src/common/utils/serviceWorker.ts
@@ -0,0 +1,24 @@
+/**
+ * Wrapper for Service Workers.
+ */
+const serviceWorker = navigator.serviceWorker;
+
+/**
+ * Verify browser support for Service Workers.
+ */
+export const verifyServiceWorker = (): boolean => {
+  return 'serviceWorker' in navigator;
+};
+
+export const registerServiceWorker = async (): Promise<ServiceWorkerRegistration | undefined> => {
+  try {
+    if (verifyServiceWorker()) {
+      const registration = await serviceWorker.register('/notifications.js');
+      return registration;
+    } else {
+      throw new Error('Could not register serviceWorker for push notifications. Browser is not supported');
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
