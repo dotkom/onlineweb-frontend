296cd416baca007cd01f4d56ae75987a2da8fac7
MartinLunde <martinlunde96@gmail.com>
2018-10-31 19:25:56 +0800

    Filter out repos with no activity older than 2 years

--- src/contribution/components/RepositoryList.tsx
+++ src/contribution/components/RepositoryList.tsx
@@ -3,6 +3,7 @@ import { getRepositories } from '../api/github';
 import { IRepository } from '../models/Repository';
 import style from '../less/contribution.less';
 import Repository from '../components/Repository';
+import {DateTime} from 'luxon';
 
 export interface IRepositoryListState {
     repositories: IRepository[];
@@ -12,8 +13,17 @@ export default class RepositoryList extends Component<{}, IRepositoryListState> 
     public readonly state = { repositories: [] } as IRepositoryListState;
 
     public async componentDidMount() {
+        const latest_repos: IRepository[] = [];
         const results = await getRepositories();
-        this.setState({ repositories: results });
+
+        results.map((repo: IRepository) => {
+            /* sort out repos recently updated (within 2 years) */
+            if (DateTime.fromISO(repo.updated_at).year > (DateTime.local().year - 2)) {
+                latest_repos.push(repo);
+            }
+        });
+
+        this.setState({ repositories: latest_repos });
     }
 
     public render() {
