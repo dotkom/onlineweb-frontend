11f9df18a34cf5caaf8f9f5748dbda97e0c33566
MartinLunde <martinlunde96@gmail.com>
2018-10-31 18:11:09 +0800

    Add intro and add contribution as path

---
+++ src/contribution/components/Intro.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import style from '../less/contribution.less';
+
+const Intro = () => (
+  <div className={style.intro}>
+    <p>
+      Her finner du alle open source prosjekter som online og dotkom jobber med. Er du interessert
+      i koding, samt ønsker å bidra til våre github repositorier? Da er dette stedet for deg!
+      Ved å bidra med utvikle vår kodebase, kan du ha direkte påvirkning på onlineres hverdag!
+      Husk å følg våre retningslinjer, men gjerne kom med kreative og nyvinnende innspill.
+      Et godt sted å begynne kan f.eks være issues i et av de forskjellige repositoriene.
+    </p>
+    <p>
+      Ikke bare oppnår du heder og ære ved å bidra til og forbedre studenters hverdag, men du får
+      også en kul sticker som bemerkelse på at du har tatt del i det spesifikke prosjektet!
+      Kanskje det tilogmed kommer godt med på cv'en?
+    </p>
+    <h1>Hvordan komme i gang?</h1>
+    <p>
+      For å komme i gang med kodingen, er du først nødt til å velge deg ut et prosjekt som høres
+      spennende ut! Det er også lurt å plukke et prosjekt som passer dine tidligere erfaring med
+      programmering for å komme enklere i gang.
+      Happy Hacking!
+    </p>
+    <hr/>
+  </div>
+);
+
+export default Intro;
