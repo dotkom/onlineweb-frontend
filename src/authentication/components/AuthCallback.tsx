5bb608a0d927595568f18ab58dcb71d75465f438
Aborysa <aborysa@gmail.com>
2018-11-03 22:12:51 +0100

    Removed decorator for injecting UserContext.Consumer Renamed setAuth to setUser to make it more readable

--- src/authentication/components/AuthCallback.tsx
+++ src/authentication/components/AuthCallback.tsx
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
 import { authCallback } from '../api';
-import { injectUserContext, IUserContext } from '../providers/UserProvider';
+import { injectUserContext, IUserContext, UserContext } from '../providers/UserProvider';
 
 export interface IProps {
   auth?: IUserContext;
 }
 
-@injectUserContext
 class AuthCallback extends Component<IProps> {
   public async componentDidMount() {
     const { auth } = this.props;
-    const user = await authCallback();
     if (auth) {
-      auth.setAuth(user);
+      const user = await authCallback();
+      auth.setUser(user);
     }
   }
 
   public render() {
     return (
       <div>
-        AuthCallback
+        thonkery
       </div>
     );
   }
 }
 
-export default AuthCallback;
+export default (...props) => {
+  return (
+    <UserContext.Consumer>
+      {(context) => <AuthCallback {...props} auth={context} />}
+    </UserContext.Consumer>
+  );
+};
