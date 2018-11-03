72f5fd1f69054ca3fc4462ced81e915a8f04fa34
Aborysa <aborysa@gmail.com>
2018-11-03 22:12:51 +0100

    Removed decorator for injecting UserContext.Consumer Renamed setAuth to setUser to make it more readable

--- src/authentication/providers/UserProvider.tsx
+++ src/authentication/providers/UserProvider.tsx
@@ -4,11 +4,11 @@ import { User } from 'oidc-client';
 
 export interface IUserContext {
   user?: IAuthUser;
-  setAuth: (auth: IAuthUser) => void;
+  setUser: (auth: IAuthUser) => void;
 }
 
 const initialState: IUserContext = {
-  setAuth: (user: IAuthUser) => console.error('setAuth called before UserProvider is initialized'),
+  setUser: (user: IAuthUser) => console.error('setUser called before UserProvider is initialized'),
 };
 
 export const UserContext = createContext(initialState);
@@ -17,7 +17,7 @@ class UserProvider extends Component<{}, IUserContext> {
 
   public state: IUserContext = {
     ...initialState,
-    setAuth: this.completeLogin,
+    setUser: this.completeLogin,
   };
 
   public completeLogin = (user: IAuthUser) => {
@@ -25,20 +25,13 @@ class UserProvider extends Component<{}, IUserContext> {
   }
 
   public render() {
-    const setAuth = (auth: User) => this.completeLogin(auth);
+    const setUser = (user: User) => this.completeLogin(user);
     return(
-      <UserContext.Provider value={{...this.state, setAuth }}>
+      <UserContext.Provider value={{...this.state, setUser }}>
         { this.props.children }
       </UserContext.Provider>
     );
   }
-}
-
-export function injectUserContext<C extends React.ComponentClass<any>>(Comp: C): C {
-  return ((props: any) =>
-    <UserContext.Consumer>
-      {(context) => <Comp {...props} auth={context} />}
-    </UserContext.Consumer>) as any as C;
 }
 
 export default UserProvider;
