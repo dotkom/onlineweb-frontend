e93b757c4d3381fff87edbf5753e5cd413106ba0
Torjus Iveland <torjusti@gmail.com>
2018-11-03 14:10:33 +0100

    Conditional entering does not lint :angry:

--- src/events/components/DetailView/Contact.tsx
+++ src/events/components/DetailView/Contact.tsx
@@ -9,20 +9,23 @@ const Contact = ({ event_type, organizer_name, company_event }: INewEvent) => {
 
   return (
     <div className={style.contact}>
-      <CardHeader className={style.detailHeader} color={color}>Kontakt</CardHeader>
+      <CardHeader className={style.detailHeader} color={color}>
+        Kontakt
+      </CardHeader>
 
       <Block title="Arrangør">
         <p>{organizer_name}</p>
         <p>komite@online.ntnu.no</p>
       </Block>
 
-      {company_event && company_event.length > 0 && (
-        <Block title="Medarrangører">
-          {company_event.map(({ company }) => (
-            <p key={company.id}>{company.name}</p>
-          ))}
-        </Block>
-      )}
+      {company_event &&
+        company_event.length > 0 && (
+          <Block title="Medarrangører">
+            {company_event.map(({ company }) => (
+              <p key={company.id}>{company.name}</p>
+            ))}
+          </Block>
+        )}
     </div>
   );
 };
