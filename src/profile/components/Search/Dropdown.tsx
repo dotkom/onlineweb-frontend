fd14359b1c85f02687bf22fe616b094b8398eff6
Marius Hauge Tobiassen <mariuhto@stud.ntnu.no>
2018-11-04 15:30:34 +0100

    Restyled the dropdown menu

--- src/profile/components/Search/Dropdown.tsx
+++ src/profile/components/Search/Dropdown.tsx
@@ -1,10 +1,10 @@
+import style from './search.less';
 import React, { ReactChild } from 'react';
 import Collapsible from 'common/components/Collapsible';
 import { IGroup } from 'core/models/Group';
 import classnames from 'classnames';
-import style from './search.less';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons/faArrowDown';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons/faSortDown';
 
 
 export interface IProps {
@@ -21,20 +21,21 @@ class Dropdown extends Collapsible<IProps> {
       <div className={classnames(style.dropdownContainer, style.searchField)}>
         {collapsed ? (
           <Item selected={!selected || true} onClick={() => this.toggleCollapse()}>
-            {selected || 'Velg en gruppe'}
-            <FontAwesomeIcon icon={faArrowDown} />
+            <h4>{selected || 'Velg en gruppe'}</h4>
+            <FontAwesomeIcon icon={faSortDown} />
           </Item>
         ) : (
           groups.map((group) => (
             <Item
               key={group.name}
               selected={group.name === selected}
+              className={style.dropped}
               onClick={() => {
                 onClick(group);
                 this.toggleCollapse();
               }}
             >
-              {group.name}
+              <h4>{group.name}</h4>
             </Item>
           ))
         )}
@@ -51,7 +52,7 @@ export interface IItemProps {
 
 const Item = ({ children, selected = false, onClick }: IItemProps) => (
   <div className={classnames(style.dropdownItem, { [style.dropdownSelected]: selected })} onClick={() => onClick()}>
-    <h4>{children}</h4>
+    {children}
   </div>
 );
 
