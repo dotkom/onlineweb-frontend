bb654d01bb3c003934bd8861791be2827aa2c025
oleast <oleast@stud.ntnu.no>
2018-11-06 00:14:26 +0100

    Replace Context.Consumer wrapper with React 16.6 static context shortcut

--- src/events/components/CalendarView/index.tsx
+++ src/events/components/CalendarView/index.tsx
@@ -5,16 +5,18 @@ import CalendarTile, { createDayList, CalendarFillerTiles } from './CalendarTile
 import { CalendarEventsContext, ICalendarEventsState } from '../../providers/CalendarEvents';
 import style from './calendar.less';
 
-export type IProps = IEventViewProps & ICalendarEventsState;
+export type IProps = IEventViewProps;
 
 class CalendarView extends Component<IProps> {
+  public static contextType = CalendarEventsContext;
+
   public async componentDidMount() {
-    const { init } = this.props;
+    const { init }: ICalendarEventsState = this.context;
     await init();
   }
 
   public render() {
-    const { eventMonth, month, changeMonth } = this.props;
+    const { eventMonth, month, changeMonth }: ICalendarEventsState = this.context;
 
     const firstWeekDay = getFirstWeekdayOfMonth(month.toJSDate());
     const lastDayPrevMonth = getPreviousMonthLength(month.toJSDate());
@@ -45,8 +47,4 @@ class CalendarView extends Component<IProps> {
   }
 }
 
-const Provider = (props: IEventViewProps) => (
-  <CalendarEventsContext.Consumer>{(state) => <CalendarView {...props} {...state} />}</CalendarEventsContext.Consumer>
-);
-
-export default Provider;
+export default CalendarView;
