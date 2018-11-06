7349c034fca07a5c34e09cd0f60e5c585410838f
sardred <andelar96@gmail.com>
2018-11-06 16:43:46 +0100

    Added Markdown wrapper to InfoOnOpportinity

--- src/career/components/InfoOnOpportunity.tsx
+++ src/career/components/InfoOnOpportunity.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Markdown from 'react-markdown';
 import { formatLocations } from './Job';
 import style from '../less/career.less';
 import Heading from 'common/components/Heading';
+import Markdown from 'common/components/Markdown';
 import { Link } from 'react-router-dom';
 import Img from 'common/components/Img';
 
@@ -23,31 +23,25 @@ const InfoBox = (props: IInfoBox) => (
     <Heading>Karrieremulighet - {props.companyName}</Heading>
     <div className={style.detail}>
       <div className={style.jobDescription}>
-        <div>
-          <h1>{props.title}</h1>
-          <Markdown source={props.description} escapeHtml />
-        </div>
+        <h1 className={style.jobTitle}>{props.title}</h1>
+        <Markdown source={props.description} escapeHtml />
       </div>
       <div>
         <div className={style.company}>
-          <Link to={`/company/${props.companyId}`}>
+          <Link className={style.companyImage} to={`/company/${props.companyId}`}>
             <Img src={props.companyImage.lg} alt={props.companyName} />
           </Link>
-          <div>
-            <div>
-              <Link to={`/company/${props.companyId}`}>
-                <h3>{props.companyName}</h3>
-              </Link>
-              <Markdown source={props.companyDescription} escapeHtml />
-            </div>
+          <div className={style.companyDescriptionBox}>
+            <Link className={style.companyDescriptionTitle} to={`/company/${props.companyId}`}>
+              <h3>{props.companyName}</h3>
+            </Link>
+            <Markdown className={style.companyDescriptionContent} source={props.companyDescription} escapeHtml />
           </div>
-          <div>
-            <div>
-              <h3>Nøkkelinformasjon</h3>
-              <p>Type: {props.type}</p>
-              <p>Sted: {formatLocations(props.locations)}</p>
-              <p>Frist: {props.deadline}</p>
-            </div>
+          <div className={style.keyInfo}>
+            <h3>Nøkkelinformasjon</h3>
+            <p>Type: {props.type}</p>
+            <p>Sted: {formatLocations(props.locations)}</p>
+            <p>Frist: {props.deadline}</p>
           </div>
         </div>
       </div>
