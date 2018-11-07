e1b1f44e2632504f70376c332c566e712e8771d4
Torjus Iveland <torjusti@gmail.com>
2018-11-07 21:13:25 +0100

    Fetch all offlines

--- src/frontpage/api/offline.ts
+++ src/frontpage/api/offline.ts
@@ -1,9 +1,12 @@
-import { get, IAPIData } from 'common/utils/api';
+import { get, getAllPages, IAPIData } from 'common/utils/api';
 import { IOfflineIssue } from '../models/Offline';
 
 const API_URL = '/api/v1/offline/';
 
-export const getOfflines = async (page: number = 0): Promise<IOfflineIssue[]> => {
-  const res: IAPIData<IOfflineIssue> = await get(API_URL, { format: 'json', page });
-  return res.results;
+export const getOfflines = async (page: number = 1): Promise<IAPIData<IOfflineIssue>> => {
+  return await get(API_URL, { format: 'json', page });
+};
+
+export const getRemaindingOfflines = (): Promise<IOfflineIssue[]> => {
+  return getAllPages(API_URL, 2);
 };
