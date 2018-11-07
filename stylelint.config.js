690c78e9146df621e1fcce2cabf07b573213050f
sardred <andelar96@gmail.com>
2018-11-07 14:35:38 +0100

    Moved prettier and stylelint configs to own files

---
+++ stylelint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'string-quotes': 'single',
+    'at-rule-no-unknown': null,
+    'at-rule-empty-line-before': null,
+    'color-named': 'never',
+    'declaration-empty-line-before': 'never',
+    'shorthand-property-no-redundant-values': true,
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
+    'declaration-block-no-redundant-longhand-properties': true,
+    'selector-max-type': [
+      0,
+      {
+        ignore: ['child', 'descendant'],
+      },
+    ],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global', 'local'],
+      },
+    ],
+  },
+};
