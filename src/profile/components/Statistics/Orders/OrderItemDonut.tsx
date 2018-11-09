c53cbccce0729b02b1ecef77bb40c42c9d75c4bd
oleast <oleast@stud.ntnu.no>
2018-11-09 17:38:41 +0100

    Create chart for order category pie

---
+++ src/profile/components/Statistics/Orders/OrderItemDonut.tsx
@@ -0,0 +1,41 @@
+import { Pie, PieDatum } from '@nivo/pie';
+import Markdown from 'common/components/Markdown';
+import { IOrder, IOrderLine } from 'profile/models/Orders';
+import React from 'react';
+
+const ABOUT_ORDER_CALENDAR = `
+  # Ordrehistorikk
+`;
+
+export interface IProps {
+  orderLines: IOrderLine[];
+}
+
+interface ITest {
+  [name: string]: number;
+}
+
+const OrderItemDonut = ({ orderLines }: IProps) => {
+  /** TODO: Allow flatMap */
+  const orders: IOrder[] = orderLines.reduce<IOrder[]>((prev, curr) => ([...prev, ...curr.orders]), [])
+  const categories = orders.reduce<{ [name: string]: number }>((prev, curr) => {
+    const { name } = curr.content_object.category;
+    const prevValues = prev.hasOwnProperty(name) ? prev[name] + 1 : 1
+    return { ...prev, [name]: prevValues}
+  }, {});
+  const values = Object.keys(categories).map((name) => ({ id: name, label: name, value: categories[name] }))
+
+  return (
+    <div>
+      <Markdown source={ABOUT_ORDER_CALENDAR} />
+      <Pie
+        data={values}
+        height={350}
+        width={275}
+        fit
+      />
+    </div>
+  )
+}
+
+export default OrderItemDonut;
