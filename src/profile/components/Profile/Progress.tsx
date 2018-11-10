7f35b0fae85a656a8dfb7e322df2d5ead6569715
oleast <oleast@stud.ntnu.no>
2018-11-10 22:55:36 +0100

    Tweak profile progress and add add numbers to the grades

--- src/profile/components/Profile/Progress.tsx
+++ src/profile/components/Profile/Progress.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import style from '../../less/profile.less';
 import { CheckMark, Circle, CircleCheck, DividerBar } from './ClassProgress';
 
 export interface IProps {
   ongoingYear: number;
   completedYear: number;
-  name: string;
 }
 
 class Progress extends React.Component<IProps> {
@@ -30,34 +28,29 @@ class Progress extends React.Component<IProps> {
       }
 
       return (
-        <g>
-          <Circle x={x} radius={radius} lineWidth={lineWidth} />
-          {year <= ongoingYear && <CircleCheck x={x} radius={checkRadius} />}
-          {year <= completedYear && <CheckMark x={x} radius={checkRadius} />}
-        </g>
+        <>
+          <g>
+            <text x={x-1.333} y="5" fontSize="5">{ year }</text>
+            <Circle x={x} radius={radius} lineWidth={lineWidth} />
+            {year <= ongoingYear && <CircleCheck x={x} radius={checkRadius} />}
+            {year <= completedYear && <CheckMark x={x} radius={checkRadius} />}
+          </g>
+        </>
       );
     };
 
     return (
-      <div className={style.infoGroup}>
-        {/*<img className="profile-group-icon" src={`${STATIC_URL + icon}.svg`} />*/}
-        <h1>{this.props.name}</h1>
-        <div className={style.progressWrapper}>
-          <div>
-            <svg width="100%" height="100%" viewBox="0 0 120 20">
-              <rect x="10" y={10 - lineWidth / 2} width="90" height={lineWidth} />
-              <Year year={1} />
-              <Year year={2} />
-              <Year year={3} />
-              <DividerBar offset={offset} radius={radius} scale={scale} lineWidth={lineWidth} a={4.5} b={30} />
-              <Year year={4} />
-              <Year year={5} />
-              <DividerBar offset={offset} radius={radius} scale={scale} lineWidth={lineWidth} a={7.5} b={90} />
-              <Year year={6} />
-            </svg>
-          </div>
-        </div>
-      </div>
+      <svg width="100%" height="100%" viewBox="0 0 112 20">
+        <rect x="10" y={12 - lineWidth / 2} width="90" height={lineWidth} />
+        <Year year={1} />
+        <Year year={2} />
+        <Year year={3} />
+        <DividerBar offset={offset} radius={radius} scale={scale} lineWidth={lineWidth} a={4.5} b={30} />
+        <Year year={4} />
+        <Year year={5} />
+        <DividerBar offset={offset} radius={radius} scale={scale} lineWidth={lineWidth} a={7.5} b={90} />
+        <Year year={6} />
+      </svg>
     );
   }
 }
