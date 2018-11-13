022fd02c3b522de0458e1be6431a8099f221d0a9
oleast <oleast@stud.ntnu.no>
2018-11-13 12:22:18 +0100

    Create top level contexts in both ssr backend and frontend to support caching of important content for first paint

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as Sentry from '@sentry/browser';
 import { __SSR__ } from 'common/constants/environment';
 import { GA_KEY } from 'common/constants/google';
 import { OWF_SENTRY_DSN } from 'common/constants/sentry';
+import { getStateCache } from 'common/utils/stateCacheResolver';
+import ContextWrapper from 'core/providers/ContextWrapper';
 import Settings from 'core/providers/Settings';
 import { getEventView } from 'events/components/EventsContainer';
 import { createBrowserHistory } from 'history';
@@ -11,6 +13,7 @@ import React from 'react';
 import * as ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 import { Router } from 'react-router-dom';
+import { EMPTY_STATE_CACHE } from 'server/stateCache';
 
 LuxonSettings.defaultLocale = 'nb';
 
@@ -26,13 +29,17 @@ const history = createBrowserHistory();
 history.listen((location) => ReactGA.pageview(location.pathname));
 
 const render = (RootComponent: any) => {
+  const cache = getStateCache() || EMPTY_STATE_CACHE;
+  console.log(cache)
   const eventView = getEventView(cookies.get('eventView'));
   /** Define renderer to use, hydrate if SSR back-end is enabled, render if no back-end */
   const reactRender = __SSR__ ? ReactDOM.render : ReactDOM.hydrate;
   reactRender(
     <Router history={history}>
       <Settings eventView={eventView}>
-        <RootComponent />
+        <ContextWrapper {...cache}>
+          <RootComponent />
+        </ContextWrapper>
       </Settings>
     </Router>,
     document.getElementById('root')
