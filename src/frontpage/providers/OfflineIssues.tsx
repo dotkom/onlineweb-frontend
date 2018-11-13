7ed0c233330ab13bdf842b1be06392b08cc96b20
oleast <oleast@stud.ntnu.no>
2018-11-13 12:20:45 +0100

    Set up frontapge Articles and Offlines to use a top level provider for cache input

---
+++ src/frontpage/providers/OfflineIssues.tsx
@@ -0,0 +1,50 @@
+import { getOfflines } from 'frontpage/api/offline';
+import { IOfflineIssue } from 'frontpage/models/Offline';
+import React, { Component, createContext } from 'react';
+
+export interface IOfflineIssuesState {
+  offlines: IOfflineIssue[];
+  init: () => void;
+}
+
+const INITIAL_STATE: IOfflineIssuesState = {
+  offlines: [],
+  init: async () => {
+    throw new Error('Init state was called before component was initialized');
+  },
+};
+
+export interface IProps {
+  /** Cache from SSR */
+  cache?: IOfflineIssue[];
+}
+
+export const OfflineContext = createContext(INITIAL_STATE);
+
+class OfflineIssues extends Component<IProps, IOfflineIssuesState> {
+  constructor(props: IProps) {
+    super(props);
+
+    /** If there is a cache from SSR, set it to state */
+    this.state = { ...INITIAL_STATE, offlines: props.cache || [] };
+  }
+
+  public init = async () => this.getOfflines();
+
+  public async getOfflines() {
+    const offlines = await getOfflines(1);
+    this.setState({ offlines });
+  }
+
+  public render() {
+    const { init } = this;
+    const value = { ...this.state, init };
+    return (
+      <OfflineContext.Provider value={value}>
+        { this.props.children }
+      </OfflineContext.Provider>
+    )
+  }
+}
+
+export default OfflineIssues;
