a34db5948b269eac38256b92bad6185bca80c2ea
Håkon Solbjørg <hlsolbjorg@gmail.com>
2018-11-13 17:19:37 +0100

    chore: Correct import statement.

--- src/career/components/JobListItem.tsx
+++ src/career/components/JobListItem.tsx
@@ -2,7 +2,7 @@ import Img from 'common/components/Img';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import style from '../less/career.less';
-import { IJob } from '../models/JobListItem';
+import { IJob } from '../models/Job';
 
 // Accepts a list of locations and returns a comma-separated list of locations
 // with 'og' inserted before the last element, and 'Ikke spesifisert' if no
