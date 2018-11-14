df28f2360b3b2e2e0bc03ab857a19a9bb2eb2c2c
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-11-15 00:36:54 +0100

    Add menu button to header and make it toggable

--- src/core/components/Header/index.tsx
+++ src/core/components/Header/index.tsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import style from './header.less';
 import HeaderLogo from './HeaderLogo';
 import MainSponsor from './MainSponsor';
+import MenuButton from './MenuButton';
 
 export interface IProps {}
 
@@ -18,28 +19,34 @@ class Header extends Component<IProps, IState> {
     isOpen: false,
   };
 
+  public toggleMenu() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
 
   public render() {
     return (
-  <header className={style.header}>
-    <div className={style.grid}>
-      <HeaderLogo />
-      <div className={style.links}>
-        <Link to={routes.profile}>Profil</Link>
-        <Link to={routes.events}>Arkiv</Link>
-        <Link to={routes.career}>Karriere</Link>
-        <Link to={routes.resources}>Ressurser</Link>
-        <Link to={routes.contribution}>Bidra</Link>
-        <Link to={routes.hobbygroups}>Interessegrupper</Link>
-        <Authenticate authentication={'view_wiki'}>
-          <Link to={routes.wiki}>Wiki</Link>
-        </Authenticate>
-        <Link to={routes.webshop}>Webshop</Link>
-      </div>
-      <MainSponsor />
-    </div>
-  </header>
-);
+      <header className={style.header}>
+        <div className={style.grid}>
+          <MenuButton isOpen={this.state.isOpen} onClick={this.toggleMenu.bind(this)} />
+          <HeaderLogo />
+          <div className={style.links}>
+            <Link to={routes.profile}>Profil</Link>
+            <Link to={routes.events}>Arkiv</Link>
+            <Link to={routes.career}>Karriere</Link>
+            <Link to={routes.resources}>Ressurser</Link>
+            <Link to={routes.contribution}>Bidra</Link>
+            <Link to={routes.hobbygroups}>Interessegrupper</Link>
+            <Authenticate authentication={'view_wiki'}>
+              <Link to={routes.wiki}>Wiki</Link>
+            </Authenticate>
+            <Link to={routes.webshop}>Webshop</Link>
+          </div>
+          <MainSponsor />
+        </div>
+      </header>
+    );
   }
 }
 
