f7d9ce966efb57a41f3435a6006efa2bc3662a95
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-11-15 00:32:19 +0100

    Copy hamburger menu from OW4

---
+++ src/core/components/Header/MenuButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import style from './header.less';
+
+export interface IProps {
+  isOpen: Boolean;
+  onClick: any;
+}
+
+const MenuButton = (props: IProps) => (
+  <button className={`${style.menuButton} ${props.isOpen ? style.open : ''}`} onClick={props.onClick}>
+    <svg
+      x="0px"
+      y="0px"
+      width="100%"
+      viewBox="0 0 96 96"
+      className={style.menuButtonSVG}
+      enableBackground="new 0 0 96 96"
+    >
+      <rect width="32" height="4" x="32" y="46" className={style.menuButtonSVGTop} />
+    </svg>
+    <svg
+      x="0px"
+      y="0px"
+      width="100%"
+      viewBox="0 0 96 96"
+      className={style.menuButtonSVG}
+      enableBackground="new 0 0 96 96"
+    >
+      <rect width="32" height="4" x="32" y="46" className={style.menuButtonSVGBottom} />
+    </svg>
+  </button>
+);
+
+export default MenuButton;
