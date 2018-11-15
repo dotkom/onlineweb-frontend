eb3ef5652d4baffcc1aa6b0e81372adda5a19eb8
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-11-16 00:34:42 +0100

    Use working links in user dropdown

--- src/core/components/Header/Login.tsx
+++ src/core/components/Header/Login.tsx
@@ -1,8 +1,10 @@
+import { routes } from 'App';
 import LoginView from 'authentication/components/Login';
 import { IAuthUser } from 'authentication/models/User';
 import { UserContext } from 'authentication/providers/UserProvider';
 import React, { Component } from 'react';
 import style from './header.less';
+import { Link } from 'react-router-dom';
 
 export interface IProps {
   // auth?: IUserContext
@@ -45,15 +47,22 @@ class Login extends Component<IProps, IState> {
 
 interface IHeaderUserProps {
   user: IAuthUser;
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick: (event: React.MouseEvent<HTMLButtonElement | HTMLElement>) => void;
   isOpen: boolean;
 }
 
 const HeaderUser = (props: IHeaderUserProps) => (
   <div className={style.user}>
     <button onClick={props.onClick} className={style.dropdownButton} />
-    <div className={style.username}>{false && props.user.profile.preferred_username}</div>
-    {props.isOpen && <div className={style.userMenu}>Dropdown</div>}
+    <div className={style.username}>niklasmh{false && props.user.profile.preferred_username}</div>
+    {props.isOpen && (
+      <div className={style.userMenu} onClick={props.onClick}>
+        <Link to={routes.profile}>Min side: niklasmh{false && props.user.profile.preferred_username}</Link>
+        <Link to={routes.home}>Kontakt oss</Link>
+        <Link to={routes.home}>Finn brukere</Link>
+        <Link to={routes.home}>Logg ut</Link>
+      </div>
+    )}
   </div>
 );
 
