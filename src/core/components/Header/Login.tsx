a76e3383beb734e03b09dbe9a3ef801e337c9509
oleast <oleast@stud.ntnu.no>
2018-11-15 21:49:05 +0100

    Fix up authenticaion user models misunderstandings

--- src/core/components/Header/Login.tsx
+++ src/core/components/Header/Login.tsx
@@ -1,23 +1,23 @@
 import LoginView from 'authentication/components/Login';
 import { IAuthUser } from 'authentication/models/User';
-import { UserContext } from 'authentication/providers/UserProvider';
+import { IUserContext, UserContext } from 'authentication/providers/UserProvider';
 import React, { Component } from 'react';
 
-/*
-export interface IProps {
-  auth?: IUserContext
-}
-*/
-
-// @injectUserContext
 class Login extends Component<{}> {
-  public render = () => (
-    <UserContext.Consumer>
-      {({ user }) => <div>{user ? <HeaderUser {...user} /> : <LoginView />}</div>}
-    </UserContext.Consumer>
-  );
+  public static contextType = UserContext;
+
+  public render() {
+    const { user }: IUserContext = this.context;
+    return (
+      <div>{user ? <HeaderUser user={user} /> : <LoginView />}</div>
+    )
+  };
 }
 
-const HeaderUser = (user: IAuthUser) => <div>{user.profile.preferred_username}</div>;
+export interface IProps {
+  user: IAuthUser;
+}
+
+const HeaderUser = ({ user }: IProps) => <div>{user.profile.preferred_username}</div>;
 
 export default Login;
