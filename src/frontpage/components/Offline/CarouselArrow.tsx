0d8d822ebeab51e649fa821771552892b514bc8d
Torjus Iveland <torjusti@gmail.com>
2018-11-16 18:37:15 +0100

    Lint offline code

--- src/frontpage/components/Offline/CarouselArrow.tsx
+++ src/frontpage/components/Offline/CarouselArrow.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons/faArrowAltCircleLeft';
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons/faArrowAltCircleRight';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 import styles from './offline.less';
 
 export interface IProps {
@@ -12,10 +12,7 @@ export interface IProps {
 
 const CarouselArrow = ({ direction, onClick, disabled = false }: IProps) => (
   <button onClick={onClick} disabled={disabled} className={styles.carouselArrow}>
-    <FontAwesomeIcon
-      icon={direction === 'left' ? faArrowAltCircleLeft : faArrowAltCircleRight}
-      size="2x"
-    />
+    <FontAwesomeIcon icon={direction === 'left' ? faArrowAltCircleLeft : faArrowAltCircleRight} size="2x" />
   </button>
 );
 
