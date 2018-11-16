9828f228bbbcb990e0315da8405b3337f8fccf85
oleast <oleast@stud.ntnu.no>
2018-11-16 10:33:45 +0100

    Small fixes to career API and styling

--- src/career/api/index.ts
+++ src/career/api/index.ts
@@ -9,7 +9,7 @@ export type FilterJobs = [
   Array<ISelectable<IApiCompany>>,
   Array<ISelectable<IEmployment>>,
   Array<ISelectable<ILocation>>
-]
+];
 
 export const getCareerOpportunities = async (): Promise<FilterJobs> => {
   const { results }: IAPIData<ICareerOpportunity> = await get(API_URL, { format: 'json', mode: 'no-cors' });
@@ -17,30 +17,33 @@ export const getCareerOpportunities = async (): Promise<FilterJobs> => {
 };
 
 const configureFilters = (jobs: ICareerOpportunity[]): FilterJobs => {
-  const companies = jobs.map((job) => job.company)
-                        .filter(removeDuplicates)
-                        .sort(sortTags)
-                        .map(addSelectable) as Array<ISelectable<IApiCompany>>;
-  const jobTypes = jobs.map((job) => job.employment)
-                       .filter(removeDuplicates)
-                       .sort(sortTags)
-                       .map(addSelectable) as Array<ISelectable<IEmployment>>;
-  const locations = jobs.flatMap((job) => job.location)
-                        .filter(removeDuplicates)
-                        .sort(sortTags)
-                        .map(addSelectable) as Array<ISelectable<ILocation>>;
+  const companies = jobs
+    .map((job) => job.company)
+    .filter(removeDuplicates)
+    .sort(sortTags)
+    .map(addSelectable) as Array<ISelectable<IApiCompany>>;
+  const jobTypes = jobs
+    .map((job) => job.employment)
+    .filter(removeDuplicates)
+    .sort(sortTags)
+    .map(addSelectable) as Array<ISelectable<IEmployment>>;
+  const locations = jobs
+    .flatMap((job) => job.location)
+    .filter(removeDuplicates)
+    .sort(sortTags)
+    .map(addSelectable) as Array<ISelectable<ILocation>>;
   return [jobs, companies, jobTypes, locations];
 };
 
-const sortTags = (a: TagTypes, b: TagTypes) => a.name > b.name ? -1 : 1;
+const sortTags = (a: TagTypes, b: TagTypes) => (a.name > b.name ? -1 : 1);
 
 const addSelectable = (tag: TagTypes): ISelectable<TagTypes> => {
-  return ({ value: tag, selected: false });
-}
+  return { value: tag, selected: false };
+};
 
 const removeDuplicates = (tag: TagTypes, i: number, all: TagTypes[]): boolean => {
   return all.map((arr) => arr.name).indexOf(tag.name) === i;
-}
+};
 
 export const getCareerOpportunity = async (id: number): Promise<ICareerOpportunity> => {
   const data: ICareerOpportunity = await get(API_URL + id, { format: 'json', mode: 'no-cors' });
