4e54d62a0ef60d4eb0ea1382bbc0cde09bf37d8a
oleast <oleast@stud.ntnu.no>
2018-11-16 12:16:53 +0100

    Add comments to career logic, fix small less mistake in filter and add cache to career detail

--- src/career/api/index.ts
+++ src/career/api/index.ts
@@ -4,6 +4,7 @@ import { IApiCompany } from 'core/models/Company';
 
 const API_URL = '/api/v1/career/';
 
+/** All selectable tags are wrapped in an ISelectable object */
 export type FilterJobs = [
   ICareerOpportunity[],
   Array<ISelectable<IApiCompany>>,
@@ -16,6 +17,16 @@ export const getCareerOpportunities = async (): Promise<FilterJobs> => {
   return configureFilters(results);
 };
 
+/**
+ * @summary Prepares the jobs from the API for usage in the filterable list.
+ * @description Creates the tags which are needed for filtering jobs in the list. All are:
+ * - Mapped to the correct type.
+ * - Filtered to remove duplicate values.
+ * - Sorted so the representation will not change when the list if changed.
+ * - Mapped to be wrapped in a selectable container, which marks if the tag has been selected.
+ * @param {ICareerOpportunity[]} jobs Career opportunities fetched from the API.
+ * @returns {FilterJobs} All the jobs, and the configured selectable tags.
+ */
 const configureFilters = (jobs: ICareerOpportunity[]): FilterJobs => {
   const companies = jobs
     .map((job) => job.company)
@@ -35,16 +46,20 @@ const configureFilters = (jobs: ICareerOpportunity[]): FilterJobs => {
   return [jobs, companies, jobTypes, locations];
 };
 
+/** Sorting expects a number, but values are string. This makes Typescript happy */
 const sortTags = (a: TagTypes, b: TagTypes) => (a.name > b.name ? -1 : 1);
 
+/** Wrap a TagType in a selectable container */
 const addSelectable = (tag: TagTypes): ISelectable<TagTypes> => {
   return { value: tag, selected: false };
 };
 
+/** Removes all duplicate TagType objects from a list of a given TagType */
 const removeDuplicates = (tag: TagTypes, i: number, all: TagTypes[]): boolean => {
   return all.map((arr) => arr.name).indexOf(tag.name) === i;
 };
 
+/** Fetch a single career opportunity */
 export const getCareerOpportunity = async (id: number): Promise<ICareerOpportunity> => {
   const data: ICareerOpportunity = await get(API_URL + id, { format: 'json', mode: 'no-cors' });
   return data;
