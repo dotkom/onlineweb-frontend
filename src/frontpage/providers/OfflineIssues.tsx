a01673add36e35dbfc91498c906e706a80698efa
oleast <oleast@stud.ntnu.no>
2018-11-18 20:27:08 +0100

    Implement Precache for frontpage articles and frontpage offlines

--- src/frontpage/providers/OfflineIssues.tsx
+++ src/frontpage/providers/OfflineIssues.tsx
@@ -1,3 +1,4 @@
+import { prefetch } from 'common/utils/prefetch';
 import { getOfflines } from 'frontpage/api/offline';
 import { IOfflineIssue } from 'frontpage/models/Offline';
 import React, { Component, createContext } from 'react';
@@ -15,18 +16,23 @@ const INITIAL_STATE: IOfflineIssuesState = {
 };
 
 export interface IProps {
-  /** Cache from SSR */
-  cache?: IOfflineIssue[];
+  prefetch?: IOfflineIssue[];
 }
 
 export const OfflineContext = createContext(INITIAL_STATE);
 
+@prefetch('FrontpageOfflines')
 class OfflineIssues extends Component<IProps, IOfflineIssuesState> {
+  public static async getServerState(_: IProps): Promise<IOfflineIssue[]> {
+    const offlines = await getOfflines(1);
+    return offlines;
+  }
+
   constructor(props: IProps) {
     super(props);
 
     /** If there is a cache from SSR, set it to state */
-    this.state = { ...INITIAL_STATE, offlines: props.cache || [] };
+    this.state = { ...INITIAL_STATE, offlines: props.prefetch || [] };
   }
 
   public init = async () => this.getOfflines();
