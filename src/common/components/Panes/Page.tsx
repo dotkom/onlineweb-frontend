57ffb863056ba0980b6da9c8d1c43387f7ba98e5
oleast <oleast@stud.ntnu.no>
2018-11-20 12:25:10 +0100

    Add loading spinner option on pane pages

--- src/common/components/Panes/Page.tsx
+++ src/common/components/Panes/Page.tsx
@@ -1,6 +1,16 @@
-import React, { Props } from 'react';
+import React, { ReactNode } from 'react';
+import Spinner from '../Spinner';
 import style from './profileCommon.less';
 
-export const Page = ({ children }: Props<any>) => <div className={style.page}>{children}</div>;
+export interface IProps {
+  children: ReactNode;
+  loading?: boolean;
+}
+
+export const Page = ({ children, loading }: IProps) => (
+  <div className={style.page}>
+    { loading ? <Spinner /> : children }
+  </div>
+);
 
 export default Page;
