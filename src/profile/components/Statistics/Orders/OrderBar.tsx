5ae5914d53dd9a63658fc18fae2e7fa5ec06330f
Torjus Iveland <torjusti@gmail.com>
2018-11-21 20:39:46 +0100

    Improve readability of Nibble statistics chart

--- src/profile/components/Statistics/Orders/OrderBar.tsx
+++ src/profile/components/Statistics/Orders/OrderBar.tsx
@@ -1,4 +1,4 @@
-import { Bar } from '@nivo/bar';
+import { ResponsiveBar } from '@nivo/bar';
 import { LIGHT_EVENT_COLORS } from 'events/models/Event';
 import { IOrder, IOrderLine } from 'profile/models/Orders';
 import React from 'react';
@@ -7,6 +7,12 @@ import style from './orders.less';
 export interface IProps {
   orderLines: IOrderLine[];
 }
+
+const Tooltip = ({ id, value, index, indexValue, color, data }) => (
+  <span>
+    {indexValue} - {value}
+  </span>
+);
 
 const OrderBar = ({ orderLines }: IProps) => {
   const orders: IOrder[] = orderLines.reduce<IOrder[]>((prev, curr) => [...prev, ...curr.orders], []);
@@ -20,43 +26,22 @@ const OrderBar = ({ orderLines }: IProps) => {
   return (
     <div className={style.centerChart}>
       <h1>Varefordeling</h1>
-      <Bar
+      <ResponsiveBar
         data={values}
-        height={450}
-        width={1050}
         margin={{
-          top: 50,
-          right: 130,
-          bottom: 50,
-          left: 60,
+          top: 65,
+          right: 65,
+          bottom: 150,
+          left: 65,
         }}
+        enableLabel={false}
+        tooltip={Tooltip}
         colors={LIGHT_EVENT_COLORS}
         colorBy="value"
         animate
-        legends={[
-          {
-            dataFrom: 'indexes',
-            anchor: 'bottom-right',
-            direction: 'column',
-            justify: false,
-            translateX: 130,
-            translateY: 0,
-            itemsSpacing: 4,
-            itemWidth: 120,
-            itemHeight: 20,
-            itemDirection: 'left-to-right',
-            itemOpacity: 0.85,
-            symbolSize: 20,
-            effects: [
-              {
-                on: 'hover',
-                style: {
-                  itemOpacity: 1,
-                },
-              },
-            ],
-          },
-        ]}
+        axisBottom={{
+          tickRotation: -90,
+        }}
       />
     </div>
   );
