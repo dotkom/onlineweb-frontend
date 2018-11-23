72f934d32a0bf010f8adf48c51a33a02b7c1cd6a
oleast <oleast@stud.ntnu.no>
2018-11-23 13:59:48 +0100

    Name pane prop interfaces to not have conflicting exports for index

--- src/common/components/Panes/Content.tsx
+++ src/common/components/Panes/Content.tsx
@@ -1,12 +1,12 @@
 import React, { ReactNode } from 'react';
 import style from './profileCommon.less';
 
-export interface IProps {
+export interface IContentProps {
   title: string;
   children: ReactNode;
 }
 
-export const Content = ({ children, title }: IProps) => (
+export const Content = ({ children, title }: IContentProps) => (
   <div className={style.content}>
     <h2>{title}</h2>
     <div>{children}</div>
