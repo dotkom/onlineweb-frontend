f205b3f04381d0c4522d0708f6cd290e9d9c10c8
kharann <anhkhav@gmail.com>
2018-11-23 15:59:58 +0100

    fixing list view in career

--- src/career/components/JobListItem.tsx
+++ src/career/components/JobListItem.tsx
@@ -2,6 +2,7 @@ import { ICareerOpportunity } from 'career/models/Career';
 import Img from 'common/components/Img';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { formatDeadline } from './JobDetails';
 import style from '../less/career.less';
 
 // Accepts a list of locations and returns a comma-separated list of locations
@@ -37,7 +38,7 @@ const JobListItem = ({ location, deadline, company, title, ingress, id, employme
       <div className={style.jobMeta}>
         <p>Type: {employment.name}</p>
         <p>Sted: {formatLocations(location.map((loc) => loc.name))}</p>
-        <p>Frist: {deadline}</p>
+        <p>Frist: {formatDeadline(deadline)}</p>
       </div>
     </div>
   </div>
