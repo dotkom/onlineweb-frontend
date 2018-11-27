889e6dc1cbec5d764a054cf084d9366964c4a57e
oleast <oleast@stud.ntnu.no>
2018-11-27 20:48:05 +0100

    Fix user privacy models, make privacy components use real API, and update privacy settings text to Markdown

--- src/profile/models/Privacy.ts
+++ src/profile/models/Privacy.ts
@@ -4,12 +4,14 @@ export interface IPrivacy {
   expose_email: boolean;
   expose_phone_number: boolean;
   expose_address: boolean;
+  visible_as_attending_events: boolean;
 }
 
 export const translations: { [Key in keyof IPrivacy]: string } = {
-  visible_for_other_users: 'Synlig for andre brukere',
-  expose_nickname: 'Vis brukernavn',
-  expose_email: 'Vis epostadresse',
-  expose_phone_number: 'Vis telefonnummer',
-  expose_address: 'Vis adresse',
+  visible_for_other_users: 'Brukerprofil synlig for andre brukere',
+  expose_nickname: 'Vis brukernavn i brukerprofil',
+  expose_email: 'Vis epostadresse i brukerprofil',
+  expose_phone_number: 'Vis telefonnummer i brukerprofil',
+  expose_address: 'Vis adresse i brukerprofil',
+  visible_as_attending_events: 'Synlig på offentlige påmeldinglister',
 };
