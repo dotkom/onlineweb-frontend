a1f72aa83ce792a2f2f4ed7e5ee65b95486222dc
oleast <oleast@stud.ntnu.no>
2018-11-28 19:48:14 +0100

    Create an optional authentication wrapper for routes. Create Route wrapper to require authentication

---
+++ src/core/components/Route.tsx
@@ -0,0 +1,13 @@
+import PrivateRoute from 'authentication/components/PrivateRoute';
+import React from 'react';
+import { Route as DefaultRoute, RouteProps } from 'react-router-dom';
+
+export interface IProps extends RouteProps {
+  requireLogin?: boolean;
+}
+
+const Route = ({ requireLogin, ...props }: IProps) => {
+  return requireLogin ? <PrivateRoute {...props} /> : <DefaultRoute {...props} />
+};
+
+export default Route;
