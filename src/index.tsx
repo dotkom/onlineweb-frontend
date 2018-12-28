f336cfe0f18e3052c234d0a5bd644bca3c897adc
oleast <oleast@stud.ntnu.no>
2018-12-28 20:37:46 +0100

    Add google analytics proxy for back-end, and make front-end use it

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ Sentry.init({
   dsn: OWF_SENTRY_DSN,
 });
 
-ReactGA.initialize(GA_KEY);
+ReactGA.initialize(GA_KEY, { gaAddress: '/analytics-script.js' });
 ReactGA.pageview(window.location.pathname);
 const history = createBrowserHistory();
 history.listen((location) => ReactGA.pageview(location.pathname));
