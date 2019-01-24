82245d7b3d734f87290e93a2b493010d2b8a3307
oleast <oleast@stud.ntnu.no>
2019-01-24 19:40:04 +0100

    Create API method for partially updating loggen n users' rfid

--- src/profile/api/index.ts
+++ src/profile/api/index.ts
@@ -1,5 +1,5 @@
 import { IAuthUser } from 'authentication/models/User';
-import { get, withUser } from 'common/utils/api';
+import { get, put, withUser } from 'common/utils/api';
 import { IFullProfileUser } from '../models/User';
 
 /** Base URL for the profile API */
@@ -10,5 +10,12 @@ const API_URL = '/api/v1/profile/';
  */
 export const getProfile = async (user: IAuthUser): Promise<IFullProfileUser> => {
   const data: IFullProfileUser = await get(API_URL, { format: 'json' }, withUser(user));
+  return data;
+};
+
+export type PartialProfile = Partial<IFullProfileUser>;
+
+export const putProfile = async (profileSettings: PartialProfile, user: IAuthUser): Promise<IFullProfileUser> => {
+  const data = await put({ query: API_URL, data: profileSettings, options: { user } });
   return data;
 };
