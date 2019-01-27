ac604c88a51749fce26edf5fd4cc70840dfb4b9f
oleast <oleast@stud.ntnu.no>
2019-01-27 14:44:06 +0100

    Switch main profile to use context via hooks

--- src/profile/components/Profile/index.tsx
+++ src/profile/components/Profile/index.tsx
@@ -1,39 +1,18 @@
-import { UserContext } from 'authentication/providers/UserProvider';
+import React, { useContext } from 'react';
+
 import { Page } from 'common/components/Panes';
-import { IProfileProps } from 'profile';
-import { getProfile } from 'profile/api';
-import React, { ContextType } from 'react';
-import { IFullProfileUser } from '../../models/User';
+import { UserProfileContext } from 'profile/providers/UserProfile';
+
 import { MainProfile } from './MainProfile';
 import style from './profile.less';
 
-export interface IProps extends IProfileProps {}
-
-export interface IState {
-  user?: IFullProfileUser;
-}
-
-class Profile extends React.Component<IProps, IState> {
-  public static contextType = UserContext;
-  public context!: ContextType<typeof UserContext>;
-  public state: IState = {};
-
-  public async componentDidMount() {
-    const auth = this.context;
-    if (auth.user) {
-      const user = await getProfile(auth.user);
-      this.setState({ user });
-    }
-  }
-
-  public render() {
-    const { user } = this.state;
-    return (
-      <div className={style.profileContainer}>
-        <Page loading={!user}>{!!user ? <MainProfile user={user} /> : null}</Page>
-      </div>
-    );
-  }
-}
+export const Profile = () => {
+  const { user } = useContext(UserProfileContext);
+  return (
+    <div className={style.profileContainer}>
+      <Page loading={!user}>{!!user ? <MainProfile /> : null}</Page>
+    </div>
+  );
+};
 
 export default Profile;
