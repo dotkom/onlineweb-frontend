e5fe414ecdb249e316be8f5ce3143090756a61ce
oleast <oleast@stud.ntnu.no>
2019-01-29 12:20:09 +0100

    Expand and correct models for user profile

--- src/profile/models/User.ts
+++ src/profile/models/User.ts
@@ -1,22 +1,40 @@
+import { FieldOfStudy } from './FieldOfStudy';
 import { IMedal } from './Medal';
+
+export type Gender = 'male' | 'female';
 
 export interface IFullProfileUser {
   first_name: string;
   last_name: string;
-  username: string;
-  ntnu_username: string;
-  nickname: string;
-  year: number;
+  readonly username: string;
+  readonly ntnu_username: string;
+  nickname: string | null;
+  readonly year: number;
   email: string;
-  online_mail: string;
-  phone_number: string;
-  address: string;
-  website: string;
-  github: string;
-  linkedin: string;
+  readonly online_mail: string | null;
+  phone_number: string | null;
+  address: string | null;
+  website: string | null;
+  github: string | null;
+  linkedin: string | null;
   positions: IMedal[];
   special_positions: any[];
   rfid: string;
+  readonly field_of_study: FieldOfStudy;
+  readonly started_date: string;
+  readonly compiled: boolean;
+  infomail: boolean;
+  jobmail: boolean;
+  allergies: string;
+  mark_rules: boolean;
+  gender: Gender;
+  /** Defaults to empty string */
+  bio: string;
+  readonly saldo: number;
+  readonly is_committee: boolean;
+  readonly is_member: boolean;
+  readonly image: string | null;
+  readonly has_expiring_membership: boolean;
 }
 
 export interface ISearchUser {
