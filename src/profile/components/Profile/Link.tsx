8c254c1c367abdf127bd7f134023a94abd926d71
oleast <oleast@stud.ntnu.no>
2019-01-29 12:22:00 +0100

    Add Bio to profile, extract panes from MainProfile to own components

--- src/profile/components/Profile/Link.tsx
+++ src/profile/components/Profile/Link.tsx
@@ -3,14 +3,16 @@ import style from './lk.less';
 
 export interface IProps {
   k: string;
-  v: string;
+  v: string | null;
 }
 
-export const Link = ({ k, v }: IProps) => (
-  <div className={style.lk}>
-    <p>{k}</p>
-    <a href={v}>{v}</a>
-  </div>
-);
+export const Link = ({ k, v }: IProps) => {
+  return (
+    <div className={style.lk}>
+      <p>{k}</p>
+      {v ? <a href={v}>{v}</a> : <p>Ikke tilgjengelig</p>}
+    </div>
+  );
+};
 
 export default Link;
