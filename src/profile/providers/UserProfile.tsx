304e244f9ff249a91ee28f18b211a98b45df52fe
oleast <oleast@stud.ntnu.no>
2019-01-29 17:03:02 +0100

    Make profile provider usable with SearchUser

--- src/profile/providers/UserProfile.tsx
+++ src/profile/providers/UserProfile.tsx
@@ -2,11 +2,16 @@ import React, { ContextType, createContext } from 'react';
 
 import { UserContext } from 'authentication/providers/UserProvider';
 import { getProfile } from 'profile/api';
-import { IFullProfileUser } from 'profile/models/User';
+import { publicProfile } from 'profile/api/search';
+import { IFullProfileUser, ISearchUser } from 'profile/models/User';
+
+export interface IProps {
+  userId?: number;
+}
 
 export interface IState {
   refetch: () => void;
-  user?: IFullProfileUser;
+  user?: IFullProfileUser | ISearchUser;
 }
 
 const INITIAL_STATE: IState = {
@@ -17,7 +22,7 @@ const INITIAL_STATE: IState = {
 
 export const UserProfileContext = createContext(INITIAL_STATE);
 
-export class UserProfileProvider extends React.Component<{}, IState> {
+export class UserProfileProvider extends React.Component<IProps, IState> {
   public static contextType = UserContext;
   public context!: ContextType<typeof UserContext>;
   public state: IState = INITIAL_STATE;
@@ -25,8 +30,14 @@ export class UserProfileProvider extends React.Component<{}, IState> {
   public init = async () => {
     const auth = this.context;
     if (auth.user) {
-      const user = await getProfile(auth.user);
-      this.setState({ user });
+      const { userId } = this.props;
+      if (userId) {
+        const user = await publicProfile(userId, auth.user);
+        this.setState({ user });
+      } else {
+        const user = await getProfile(auth.user);
+        this.setState({ user });
+      }
     }
   };
 
