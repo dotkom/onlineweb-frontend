d6aeee7195e4adfd69218761a208e945bdf24ada
oleast <oleast@stud.ntnu.no>
2019-01-29 17:00:35 +0100

    require authentication when searching profiles

--- src/profile/api/search.ts
+++ src/profile/api/search.ts
@@ -1,3 +1,4 @@
+import { IAuthUser } from 'authentication/models/User';
 import { get, IAPIData, IBaseAPIParameters } from 'common/utils/api';
 
 import { ISearchUser } from '../models/User';
@@ -10,7 +11,12 @@ export interface IUserSearchParameters extends IBaseAPIParameters {
 
 const API_URL = '/api/v1/profile/search/';
 
-export const searchUsers = async (params: IUserSearchParameters): Promise<ISearchUser[]> => {
-  const { results = [] }: IAPIData<ISearchUser> = await get(API_URL, { format: 'json', ...params });
+export const searchUsers = async (params: IUserSearchParameters, user: IAuthUser): Promise<ISearchUser[]> => {
+  const { results = [] }: IAPIData<ISearchUser> = await get(API_URL, { format: 'json', ...params }, { user });
   return results;
+};
+
+export const publicProfile = async (userId: number, user: IAuthUser): Promise<ISearchUser> => {
+  const profile = await get(API_URL + userId, { format: 'json' }, { user });
+  return profile;
 };
