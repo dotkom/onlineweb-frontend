35c35f3d38d13d75f9788aca6ce333b65635651a
oleast <oleast@stud.ntnu.no>
2019-02-01 13:37:26 +0100

    Refactor profile and public profile models

--- src/profile/models/User.ts
+++ src/profile/models/User.ts
@@ -3,60 +3,60 @@ import { IMedal } from './Medal';
 
 export type Gender = 'male' | 'female';
 
-export interface IFullProfileUser {
+/**
+ * Base profile which contains all common fields for profile types.
+ */
+interface IBaseProfile {
+  readonly id: number;
   first_name: string;
   last_name: string;
   readonly username: string;
   readonly ntnu_username: string;
-  nickname: string | null;
   readonly year: number;
-  email: string;
   readonly online_mail: string | null;
-  phone_number: string | null;
-  address: string | null;
   website: string | null;
   github: string | null;
   linkedin: string | null;
   positions: IMedal[];
   special_positions: any[];
-  rfid: string;
   readonly field_of_study: FieldOfStudy;
   readonly started_date: string;
   readonly compiled: boolean;
-  infomail: boolean;
-  jobmail: boolean;
-  allergies: string;
-  mark_rules: boolean;
-  gender: Gender;
-  /** Defaults to empty string */
   bio: string;
-  readonly saldo: number;
   readonly is_committee: boolean;
   readonly is_member: boolean;
   readonly image: string | null;
-  readonly has_expiring_membership: boolean;
 }
 
-export interface ISearchUser {
-  id: number;
-  first_name: string;
-  last_name: string;
-  nickname: string | null;
-  online_mail: string | null;
-  phone_number: string | null;
-  username: string;
-  address: string;
-  zip_code: string;
+/**
+ * Fields on user which can be hidden by Privacy settings.
+ */
+interface IProfilePrivate {
+  address: string | null;
   email: string;
-  website: string | null;
-  github: string | null;
-  linkedin: string | null;
-  ntnu_username: string;
-  field_of_study: FieldOfStudy;
-  year: number;
-  bio: string;
-  positions: IMedal[];
-  special_positions: any[];
-  image: string;
-  started_date: string;
+  nickname: string | null;
+  phone_number: string | null;
 }
+
+/** A basic Profile, just used to create IUserProfile */
+type IProfile = IBaseProfile & IProfilePrivate;
+
+/**
+ * The full profile of the loggen in user.
+ */
+export interface IUserProfile extends IProfile {
+  allergies: string;
+  gender: Gender;
+  readonly has_expiring_membership: boolean;
+  infomail: boolean;
+  jobmail: boolean;
+  mark_rules: boolean;
+  rfid: string;
+  readonly saldo: number;
+}
+
+/** Make every field in a type/interface nullable */
+export type Nullable<T> = { [K in keyof T]: T[K] | null };
+
+/** The public profile for a user */
+export type IPublicProfile = IBaseProfile & Nullable<Readonly<IProfilePrivate>>;
