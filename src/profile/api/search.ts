1cdffe2ac5eb8bf7358241749afc0cacd4d9701f
oleast <oleast@stud.ntnu.no>
2019-02-01 13:34:45 +0100

    Replace withUser with user? option

--- src/profile/api/search.ts
+++ src/profile/api/search.ts
@@ -1,7 +1,7 @@
 import { IAuthUser } from 'authentication/models/User';
 import { get, IAPIData, IBaseAPIParameters } from 'common/utils/api';
 
-import { ISearchUser } from '../models/User';
+import { IPublicProfile } from '../models/User';
 
 export interface IUserSearchParameters extends IBaseAPIParameters {
   search: string;
@@ -11,12 +11,12 @@ export interface IUserSearchParameters extends IBaseAPIParameters {
 
 const API_URL = '/api/v1/profile/search/';
 
-export const searchUsers = async (params: IUserSearchParameters, user: IAuthUser): Promise<ISearchUser[]> => {
-  const { results = [] }: IAPIData<ISearchUser> = await get(API_URL, { format: 'json', ...params }, { user });
+export const searchUsers = async (params: IUserSearchParameters, user: IAuthUser): Promise<IPublicProfile[]> => {
+  const { results = [] }: IAPIData<IPublicProfile> = await get(API_URL, { format: 'json', ...params }, { user });
   return results;
 };
 
-export const publicProfile = async (userId: number, user: IAuthUser): Promise<ISearchUser> => {
-  const profile = await get(API_URL + userId, { format: 'json' }, { user });
+export const getPublicProfile = async (profileId: number, user: IAuthUser): Promise<IPublicProfile> => {
+  const profile = await get(API_URL + profileId, { format: 'json' }, { user });
   return profile;
 };
