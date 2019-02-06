3548e6c5d0a1ca335c4991a861af1e6ea00e6334
oleast <oleast@stud.ntnu.no>
2019-02-06 12:06:35 +0100

    Replace fixed notification options with dynamic channels

--- src/profile/models/Notification.ts
+++ src/profile/models/Notification.ts
@@ -1,20 +1,4 @@
-export interface INotificationOption {
-  articles: boolean;
-  events: boolean;
-  offlines: boolean;
-  feedback: boolean;
+export interface IChannel {
+  name: string;
+  description: string;
 }
-
-export interface INotificationSetting extends INotificationOption {
-  allowNotifications: boolean;
-  subscription: boolean;
-}
-
-export const optionStrings: { [Key in keyof INotificationSetting]: string } = {
-  articles: 'Nye Artikler',
-  events: 'Arrangementpåmelding',
-  offlines: 'Nye Offline',
-  feedback: 'Nye tilbakemeldingsskjemaer',
-  allowNotifications: 'Tillat notifikasjoner på dette nettstedet',
-  subscription: 'Registrer denne enheten for å motta notifikasjoner',
-};
