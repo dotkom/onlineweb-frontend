d51382a8bffbe39d334e34ec544787e6e2aaff56
oleast <oleast@stud.ntnu.no>
2019-02-06 12:06:35 +0100

    Replace fixed notification options with dynamic channels

--- src/profile/components/Settings/Notifications/Option.tsx
+++ src/profile/components/Settings/Notifications/Option.tsx
@@ -1,18 +1,18 @@
 import ToggleSwitch from 'common/components/ToggleSwitch';
 import React from 'react';
-import { INotificationSetting, optionStrings } from '../../../models/Notification';
 import style from './notifications.less';
 
 export interface IProps {
-  option: keyof INotificationSetting | 'allowNotifications';
+  name: string;
+  description: string;
   value: boolean;
   toggle: () => void;
   disabled?: boolean;
 }
 
-const Option = ({ option, value, toggle, disabled }: IProps) => (
+const Option = ({ description, value, toggle, disabled }: IProps) => (
   <div className={style.element}>
-    <h4>{optionStrings[option]}</h4>
+    <h4>{description}</h4>
     <div className={style.toggle}>
       <ToggleSwitch checked={value} onChange={toggle} disabled={disabled} />
     </div>
