da32ebd174f8f2aabb359dd3f6c34bcbd78904e9
oleast <oleast@stud.ntnu.no>
2019-02-10 12:26:53 +0100

    Add push listener and notificationclick listener to serviceworker

--- src/serviceworker/index.js
+++ src/serviceworker/index.js
@@ -8,3 +8,30 @@ sw.addEventListener('install', (event) => {
 sw.addEventListener('fetch', (event) => {
   event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
+
+sw.addEventListener('push', (event) => {
+  console.log(event)
+  if (!(self.Notification && self.Notification.permission === 'granted')) {
+    return;
+  }
+
+  const { title, ...noti } = event.data.json()
+
+  return self.registration.showNotification(title, noti);
+})
+
+sw.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(clients
+    .matchAll({ type: 'window'})
+    .then((clientList) => {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url == '/' && 'focus' in client)
+          return client.focus();
+      }
+      if (clients.openWindow)
+        return clients.openWindow('/');
+    })
+  );
+});
