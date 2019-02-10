5f35b436b3cc23964eab692f84ce3440cd02ace3
oleast <oleast@stud.ntnu.no>
2019-02-10 12:26:14 +0100

    Add settings and API for setting channels and unsubing from push on server

--- src/profile/api/notifications.ts
+++ src/profile/api/notifications.ts
@@ -10,17 +10,22 @@ export const subscribe = async (sub: PushSubscription, user: IAuthUser) => {
   await post(API_URL + '/subscribe', json, {}, { user, domain: API_DOMAIN });
 };
 
+export const unsubscribe = async (sub: PushSubscription, user: IAuthUser) => {
+  const json = sub.toJSON();
+  await post(API_URL + '/unsubscribe', json, {}, { user, domain: API_DOMAIN });
+};
+
 export const getAllChannels = async (user: IAuthUser): Promise<IChannel[]> => {
   const channels = await get(API_URL + '/channels', {}, { user, domain: API_DOMAIN });
   return channels;
 };
 
-export const getUserChannels = async (user: IAuthUser): Promise<string[]> => {
+export const getUserChannels = async (user: IAuthUser): Promise<IChannel[]> => {
   const channels = await get(API_URL + '/user-channels', {}, { user, domain: API_DOMAIN });
   return channels;
 };
 
-export const postUserChannels = async (channels: string[], user: IAuthUser): Promise<string[]> => {
-  const updated = await post(API_URL + '/user-channels', { channels }, {}, { user, domain: API_DOMAIN });
-  return updated;
+export const postUserChannels = async (channelNames: string[], user: IAuthUser): Promise<IChannel[]> => {
+  const channels = await post(API_URL + '/user-channels', { channels: channelNames }, {}, { user, domain: API_DOMAIN });
+  return channels;
 };
