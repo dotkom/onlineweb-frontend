46f1a9922643f529de30f4cd993c7c8ab56b0e78
oleast <oleast@stud.ntnu.no>
2019-02-12 16:47:57 +0100

    Change DetailView to use EventsRepo and hooks

--- src/events/components/DetailView/index.tsx
+++ src/events/components/DetailView/index.tsx
@@ -1,7 +1,11 @@
-import { prefetch } from 'common/utils/prefetch';
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
+
+import { usePrefetch } from 'common/hooks/usePrefetch';
+import { PrefetchKey } from 'common/utils/PrefetchState';
+import { EventsRepo } from 'events/providers/EventsRepo';
+
 import { getEvent } from '../../api/events';
-import { INewEvent, mockEvent } from '../../models/Event';
+import { mockEvent } from '../../models/Event';
 import ListEvent from '../ListView/ListEvent';
 import Contact from './Contact';
 import style from './detail.less';
@@ -9,52 +13,26 @@ import InfoBox from './InfoBox';
 import PictureCard from './PictureCard';
 import Registration from './Registation';
 
-export interface IState {
-  eventId: number;
-  event: INewEvent | null;
-}
-
 export interface IProps {
   eventId: string;
-  prefetch?: INewEvent;
 }
 
-@prefetch('EventsDetailView')
-class DetailView extends Component<IProps, IState> {
-  public static async getServerState(props: IProps): Promise<INewEvent> {
-    const eventId = parseInt(props.eventId, 10);
-    const event = await getEvent(eventId);
-    return event;
-  }
+export const DetailView = (props: IProps) => {
+  const { eventMap } = useContext(EventsRepo);
+  const eventId = parseInt(props.eventId, 10);
 
-  constructor(props: IProps) {
-    super(props);
-    const eventId = parseInt(props.eventId, 10);
-    const event = props.prefetch && eventId === props.prefetch.id ? props.prefetch : null;
-    this.state = {
-      eventId,
-      event,
-    };
-  }
+  const prefetchEvent = usePrefetch(PrefetchKey.EVENT_SINGLE, async () => await getEvent(eventId));
 
-  public async componentDidMount() {
-    const { eventId } = this.state;
-    const event = await getEvent(eventId);
-    this.setState({ event });
-  }
-
-  public render() {
-    const event = this.state.event || mockEvent;
-    return (
-      <div className={style.container}>
-        <ListEvent {...event} />
-        <PictureCard {...event} />
-        <InfoBox {...event} />
-        <Registration {...event} />
-        <Contact {...event} />
-      </div>
-    );
-  }
-}
+  const event = prefetchEvent && prefetchEvent.id === eventId ? prefetchEvent : eventMap.get(eventId) || mockEvent;
+  return (
+    <div className={style.container}>
+      <ListEvent {...event} />
+      <PictureCard {...event} />
+      <InfoBox {...event} />
+      <Registration {...event} />
+      <Contact {...event} />
+    </div>
+  );
+};
 
 export default DetailView;
