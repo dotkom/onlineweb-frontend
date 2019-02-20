35dbb4e8cff83fdaa56e88b1e9f3986fd4aa5ddc
oleast <oleast@stud.ntnu.no>
2019-02-20 16:26:15 +0100

    Refactor frontpage offlines to use scroll

--- src/frontpage/components/Offline/index.tsx
+++ src/frontpage/components/Offline/index.tsx
@@ -1,7 +1,6 @@
 import Heading from 'common/components/Heading';
-import { IOfflineIssuesState, OfflineContext } from 'frontpage/providers/OfflineIssues';
-import React, { Component } from 'react';
-import { getOfflines, getRemaindingOfflines } from '../../api/offline';
+import React, { useEffect, useRef, useState } from 'react';
+import { getOfflines, getRemainingOfflines } from '../../api/offline';
 
 import { IOfflineIssue } from '../../models/Offline';
 import CarouselArrow from './CarouselArrow';
@@ -11,83 +10,104 @@ import OfflineCarousel from './OfflineCarousel';
 export interface IProps {}
 
 export interface IState {
-  dataRemainding: boolean;
+  dataRemaining: boolean;
   offlines: IOfflineIssue[];
   index: number;
   page: number;
 }
 
-const DISPLAY_NUMBER = 5;
-
-class Offline extends Component<IProps, IState> {
-  public static contextType = OfflineContext;
-  public state: IState = {
-    dataRemainding: false,
-    offlines: [],
-    index: 0,
-    page: 1,
-  };
-
-  public async componentDidMount() {
-    const data = await getOfflines(1);
-    const { init }: IOfflineIssuesState = this.context;
-    init();
-
-    this.setState({
-      dataRemainding: Boolean(data.next),
-      offlines: data.results,
-    });
-  }
-
-  public handleNext = async () => {
-    if (this.state.dataRemainding) {
-      const remainding = await getRemaindingOfflines();
-
-      this.setState({
-        offlines: [...this.state.offlines, ...remainding],
-        dataRemainding: false,
-      });
-    }
-
-    this.setState((prevState) => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  public handlePrevious = async () => {
-    this.setState((prevState) => ({
-      page: prevState.page - 1,
-    }));
-  };
-
-  public render() {
-    const { offlines, page } = this.state;
-    const lastPage = Math.ceil(offlines.length / DISPLAY_NUMBER);
-    const visibleOfflines = offlines.slice(DISPLAY_NUMBER * (page - 1), DISPLAY_NUMBER * page);
-    /*
-    const { index } = this.state;
-    const { offlines }: IOfflineIssuesState = this.context;
-    const start = index;
-    const end = start + DISPLAY_NUMBER;
-    */
-    return (
-      <section className={style.container}>
-        <Heading title="Offline" />
-
-        <div className={style.carouselContainer}>
-          {offlines && (
-            <>
-              <CarouselArrow direction="left" onClick={this.handlePrevious} disabled={page === 1} />
-
-              <OfflineCarousel offlines={visibleOfflines} />
-
-              <CarouselArrow direction="right" onClick={this.handleNext} disabled={page === lastPage} />
-            </>
-          )}
-        </div>
-      </section>
-    );
-  }
+export interface IOfflineRef {
+  issue: IOfflineIssue;
+  ref: React.RefObject<HTMLDivElement>;
 }
+
+const createOfflineRefs = (offlines: IOfflineIssue[]): IOfflineRef[] => {
+  return offlines.map((issue) => ({
+    issue,
+    ref: React.createRef<HTMLDivElement>(),
+  }));
+};
+
+export const Offline = ({  }: IProps) => {
+  const [offlines, setOfflines] = useState<IOfflineRef[]>([]);
+  const [currentScroll, setCurrentScroll] = useState(0);
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  const getSizes = () => {
+    if (offlines.length) {
+      const [first] = offlines;
+      if (first.ref.current && carouselRef.current) {
+        return {
+          element: first.ref.current.scrollWidth,
+          container: carouselRef.current.clientWidth,
+        };
+      }
+    }
+    return null;
+  };
+
+  const scroll = (amount: number) => {
+    const targetIndex = currentScroll + amount;
+    const target = offlines[targetIndex];
+    const ref = target ? target.ref.current : null;
+    console.log('reg:', ref);
+    if (ref) {
+      ref.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
+      setCurrentScroll(targetIndex);
+    }
+  };
+
+  const scrollToNextRef = () => {
+    const sizes = getSizes();
+    console.log(sizes);
+    if (sizes) {
+      const amount = Math.floor(sizes.container / sizes.element);
+      console.log('Amount:', amount);
+      scroll(amount);
+    }
+  };
+
+  const scrollToPrevRef = () => {
+    const sizes = getSizes();
+    console.log(sizes);
+    if (sizes) {
+      const amount = -Math.floor(sizes.container / sizes.element);
+      console.log('Amount:', amount);
+      scroll(amount);
+    }
+  };
+
+  /** Get the first batch of Offlines for feast loading */
+  const fetchInitial = async () => {
+    const { results } = await getOfflines(1);
+    const offlineRefs = createOfflineRefs(results);
+    setOfflines(offlineRefs);
+    fetchRemaining();
+  };
+
+  /** Get all the remaining Offlines to fill out the list */
+  const fetchRemaining = async () => {
+    const remaining = await getRemainingOfflines();
+    const offlineRefs = createOfflineRefs(remaining);
+    setOfflines([...offlines, ...offlineRefs]);
+  };
+
+  /** Fetch offlines on first mount */
+  useEffect(() => {
+    fetchInitial();
+  }, []);
+
+  return (
+    <section className={style.container}>
+      <Heading title="Offline" />
+
+      <CarouselArrow direction="left" onClick={scrollToPrevRef} />
+      <div className={style.carouselContainer} ref={carouselRef}>
+        {offlines.length && <OfflineCarousel offlines={offlines} />}
+      </div>
+      <CarouselArrow direction="right" onClick={scrollToNextRef} />
+    </section>
+  );
+};
 
 export default Offline;
