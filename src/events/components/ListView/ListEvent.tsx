2c94adc41d5455c356825350eaf73b1f9a2e2a95
Marius Hauge Tobiassen <mariuhto@stud.ntnu.no>
2019-03-04 14:17:36 +0100

    Redid the list-view, check PR for screenshot

--- src/events/components/ListView/ListEvent.tsx
+++ src/events/components/ListView/ListEvent.tsx
@@ -9,24 +9,24 @@ import style from './list.less';
 const ListEvent = ({ title, event_start, attendance_event, event_type, company_event }: INewEvent) => {
   const eventColor = getEventColor(event_type);
   const eventType = getEventType(event_type);
-  const eventDate = DateTime.fromISO(event_start).toFormat('d.MM');
+  const eventDate = DateTime.fromISO(event_start).toFormat('dd.MM');
   const eventAttendees = getEventAttendees(attendance_event);
 
   return (
     <div className={style.gridRow}>
-      <span style={{ background: eventColor }} />
-      <div className={style.eventText}>
-        <p style={{ color: eventColor }}>{eventType}</p>
-        <p className={style.eventTitle}>{company_event.length === 1 ? company_event[0].company.name : title}</p>
+      <div className={style.eventTypeDiv}>
+        <span style={{ background: eventColor }} />
+        <p className={style.eventType} style={{ color: eventColor }}>{eventType}</p>
       </div>
+      <p className={style.eventTitle}>{company_event.length === 1 ? company_event[0].company.name : title}</p>
       <div className={style.icon}>
         <FontAwesomeIcon icon={faCalendarAlt} fixedWidth />
       </div>
-      <p> {eventDate} </p>
+      <p className={style.suppText}> {eventDate} </p>
       <div className={style.icon}>
         <FontAwesomeIcon icon={faUser} fixedWidth />
       </div>
-      <p> {eventAttendees} </p>
+      <p className={style.suppText}> {eventAttendees} </p>
     </div>
   );
 };
