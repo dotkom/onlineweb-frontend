8ec1042a2101cdaf8684ed75ac5accf956beef6e
Henrik Hørlück Berg <henrik@horluck.no>
2019-04-11 15:41:33 +0200

    Move query context from Events to Search Module

--- src/events/components/SearchModule/index.tsx
+++ src/events/components/SearchModule/index.tsx
@@ -1,41 +1,30 @@
-import React, { ChangeEvent, FC } from 'react';
+import React, { ChangeEvent, FC, useContext } from 'react';
+import { QueryParams } from '../../../core/providers/QueryParams';
 import style from '../../../profile/components/Search/search.less';
-import { EventTypeEnum } from '../../models/Event';
 import DateInput from './DateInput';
 import { SelectMultiple } from './SelectMultiple';
 
 export interface IProps {
-  searchText: string;
-  dateStart: string;
-  dateEnd: string;
-  eventTypes: EventTypeEnum[];
   onTextInput: (event: ChangeEvent<HTMLInputElement>) => void;
   onTimeStartInput: (event: ChangeEvent<HTMLInputElement>) => void;
   onTimeEndInput: (event: ChangeEvent<HTMLInputElement>) => void;
   onEventTypesInput: (event: ChangeEvent<HTMLSelectElement>) => void;
 }
 
-const SearchModule: FC<IProps> = ({
-  searchText,
-  dateStart,
-  dateEnd,
-  eventTypes,
-  onTextInput,
-  onTimeEndInput,
-  onTimeStartInput,
-  onEventTypesInput,
-}) => {
+const SearchModule: FC<IProps> = ({ onTextInput, onTimeEndInput, onTimeStartInput, onEventTypesInput }) => {
+  const { search, dateStart, dateEnd, eventTypes } = useContext(QueryParams);
+
   return (
     <div className={style.grid}>
       <input
         className={style.searchInput}
         type="search"
-        defaultValue={searchText}
+        defaultValue={search}
         placeholder="Søk"
         onChange={onTextInput}
       />
-      <DateInput label="Fra: " time={dateStart} onChange={onTimeStartInput} />
-      <DateInput label="Til: " time={dateEnd} onChange={onTimeEndInput} />
+      <DateInput label="Fra: " time={dateStart.toFormat('yyyy-MM-dd')} onChange={onTimeStartInput} />
+      <DateInput label="Til: " time={dateEnd.toFormat('yyyy-MM-dd')} onChange={onTimeEndInput} />
       <SelectMultiple onEventTypesInput={onEventTypesInput} eventTypes={eventTypes} />
     </div>
   );
