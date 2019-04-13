8b9da2ea8dfaaad2a54ee9faf4f8f28f5b305712
Marius Hauge Tobiassen <mariuhto@stud.ntnu.no>
2019-04-13 17:57:34 +0200

    Continued styling for Dateinput component

--- src/events/components/SearchModule/DateInput.tsx
+++ src/events/components/SearchModule/DateInput.tsx
@@ -1,3 +1,5 @@
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons/';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { ChangeEvent, FC } from 'react';
 import style from './search.less';
 
@@ -9,12 +11,11 @@ export interface IProps {
 
 const DateInput: FC<IProps> = ({ label, time, onChange }) => {
   return (
-    <div className={style.dateInput}>
-      <label>
-        {label}
-      </label>
+    <label className={style.dateInput}>
+      <span>{label}</span>
       <input type="date" value={time} onChange={onChange} pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" required />
-    </div>
+      <FontAwesomeIcon className={style.dateIcon} icon={faCalendarAlt} fixedWidth />
+    </label>
   );
 };
 
