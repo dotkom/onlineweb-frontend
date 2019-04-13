ad5f88270fbc0e01f3709db9c8762cf7eed35398
oleast <oleast@stud.ntnu.no>
2019-04-13 19:25:46 +0200

    Add typings for __RouterContext for react-router

---
+++ types/ReactRouter.d.ts
@@ -0,0 +1,15 @@
+import * as H from 'history';
+import { Context } from 'react';
+import { match } from 'react-router-dom';
+
+declare module 'react-router-dom' {
+  // tslint:disable-next-line interface-name
+  export interface RouterContext {
+    history: H.History;
+    location: H.Location<H.LocationState>;
+    match: match<{}>;
+  }
+  
+  // tslint:disable-next-line variable-name
+  export const __RouterContext: Context<RouterContext>;
+}
