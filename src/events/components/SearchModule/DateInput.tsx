ae7d76763fbd2f9004fad38f7b2dc8362d67114c
Marius Hauge Tobiassen <mariuhto@stud.ntnu.no>
2019-04-16 13:05:46 +0200

    Simplified DateInput, should be done now.

--- src/events/components/SearchModule/DateInput.tsx
+++ src/events/components/SearchModule/DateInput.tsx
@@ -1,5 +1,3 @@
-import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons/';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { ChangeEvent, FC } from 'react';
 import style from './search.less';
 
@@ -14,7 +12,6 @@ const DateInput: FC<IProps> = ({ label, time, onChange }) => {
     <label className={style.dateInput}>
       <span>{label}</span>
       <input type="date" value={time} onChange={onChange} pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" required />
-      <FontAwesomeIcon className={style.dateIcon} icon={faCalendarAlt} fixedWidth />
     </label>
   );
 };
