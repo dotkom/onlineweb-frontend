cc41248e6bcd397e371e2ad1bb954381f71362c6
oleast <oleast@stud.ntnu.no>
2019-04-21 22:12:54 +0200

    Remove v0 user models

--- src/events/models/Attendee.ts
+++ src/events/models/Attendee.ts
@@ -1,10 +1,8 @@
-import { IUser } from 'core/models/User';
 import { IAttendanceEvent } from './Event';
 import { IExtra } from './Extras';
 
 export interface IAttendee {
   event: IAttendanceEvent;
-  user: IUser;
   timestamp: Date;
   attended: boolean;
   paid: boolean;
