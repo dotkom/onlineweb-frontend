da3e6b771316574f455be86c89592a1e1fdd2472
oleast <oleast@stud.ntnu.no>
2019-04-21 21:41:07 +0200

    Fix typings for react components, routers, and children

--- src/common/components/Panes/FourSplitPane.tsx
+++ src/common/components/Panes/FourSplitPane.tsx
@@ -1,6 +1,6 @@
-import React, { Props } from 'react';
+import React, { FC } from 'react';
 import style from './profileCommon.less';
 
-export const FourSplitPane = ({ children }: Props<any>) => <div className={style.fourSplitPane}>{children}</div>;
+export const FourSplitPane: FC = ({ children }) => <div className={style.fourSplitPane}>{children}</div>;
 
 export default FourSplitPane;
