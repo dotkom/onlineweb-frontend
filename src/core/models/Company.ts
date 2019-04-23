cc8a2546e42247910f8dec7163057ba7b977c7b9
oleast <oleast@stud.ntnu.no>
2019-04-23 11:39:55 +0200

    Type comment cleanup for company and orders

--- src/core/models/Company.ts
+++ src/core/models/Company.ts
@@ -3,10 +3,10 @@ import IResponsiveImage from 'common/models/ResponsiveImage';
 export interface ICompany {
   readonly id: number;
   readonly name: string;
-  readonly short_description: string; // TextField model?
-  readonly long_description: string; // TextField model?
-  readonly image: IResponsiveImage; // Image, or StaticContent model?
-  readonly site: string; // URL model?
-  readonly email_address: string; // Email model?
-  readonly phone_number: string; // Phone model?
+  readonly short_description: string;
+  readonly long_description: string;
+  readonly image: IResponsiveImage;
+  readonly site: string;
+  readonly email_address: string;
+  readonly phone_number: string;
 }
