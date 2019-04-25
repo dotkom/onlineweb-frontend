153e958657e26ebbf4613aa80e4a16009bc390d0
oleast <oleast@stud.ntnu.no>
2019-04-25 13:51:11 +0200

    Make ResponsiveImage responsive to its size on the dom

--- src/common/components/ResponsiveImage/index.tsx
+++ src/common/components/ResponsiveImage/index.tsx
@@ -1,16 +1,88 @@
-import React, { ImgHTMLAttributes } from 'react';
+import React, { FC, ImgHTMLAttributes, useEffect, useState } from 'react';
+
+import { useBoundingRect } from 'common/hooks/useBoundingRect';
+import { getKeys } from 'common/utils/tsHacks';
+
 import { DOMAIN } from '../../constants/endpoints';
-import IResponsiveImage, { IResponsiveImageSizes } from '../../models/ResponsiveImage';
+import IResponsiveImage, {
+  IMAGE_DIMENTIONS,
+  IResponsiveImageSizes,
+  ResponsiveImageTypes,
+} from '../../models/ResponsiveImage';
+
+export type ImageType = ResponsiveImageTypes;
+export type ImageSizes = IResponsiveImageSizes;
+export type ImageSize = keyof ImageSizes;
 
 export interface IProps extends ImgHTMLAttributes<HTMLImageElement> {
   image: IResponsiveImage;
-  size: keyof IResponsiveImageSizes;
+  size: ImageSize;
+  type: ImageType;
 }
 
-export const ResponsiveImage = ({ image, size, alt, ...props }: IProps) => {
-  const defaultImage = image[size];
+/**
+ * @summary Get the key types of images sorted by width.
+ * @example ['xs', 'sm', 'md', 'lg']
+ * @param {ImageType} type
+ */
+const getSortedDimentionKeys = (type: ImageType) => {
+  return getKeys(IMAGE_DIMENTIONS[type])
+    .sort((a, b) => {
+      const [aWidth] = IMAGE_DIMENTIONS[type][a];
+      const [bWidth] = IMAGE_DIMENTIONS[type][b];
+      return aWidth - bWidth;
+    })
+    .filter((t) => t !== 'wide'); // Filter wide because it is unstable, and just mimicks 'lg'.
+};
+
+/**
+ * @summary Get the most fitting image version to display on screen based on the client screen/pixel size.
+ * @param {ResponsiveImageTypes} type
+ * @param {number} displayWidth The actual width of the displayed image in real pixel in the screen.
+ * @param {ImageSize} currentSize The default/currently displayed size. Resolution should ne be downgraded.
+ */
+const getOptimalImageSize = (type: ImageType, displayWidth: number, currentSize: ImageSize): ImageSize => {
+  const dimentions = IMAGE_DIMENTIONS[type];
+  const keys = getSortedDimentionKeys(type);
+
+  /** Flag if the compared sizes are larger than the currently displayes size */
+  let passedCurrent = false;
+
+  for (let i = 0; i < keys.length - 1; i++) {
+    /** Mark current size as larger than current size */
+    if (keys[i] === currentSize) {
+      passedCurrent = true;
+    }
+
+    /** Find most fitting image size depending on currently displayed size in DOM */
+    const [currentWidth] = dimentions[keys[i]];
+    const [nextWidth] = dimentions[keys[i + 1]];
+    if (passedCurrent && displayWidth > currentWidth && displayWidth <= nextWidth) {
+      return keys[i];
+    }
+  }
+
+  /** If no fitting alternative is found, use the supplied default */
+  return currentSize;
+};
+
+export const ResponsiveImage: FC<IProps> = ({ image, size, alt, type, ...props }) => {
+  const [responsiveSize, setResponsiveSize] = useState<ImageSize>(size);
+  const [ref, boundingRect] = useBoundingRect<HTMLImageElement>();
+
+  useEffect(() => {
+    if (boundingRect) {
+      /** Calculate real pixel size based on image size in the DOM */
+      const width = boundingRect.width * window.devicePixelRatio;
+      const optimalSize = getOptimalImageSize(type, width, size);
+      setResponsiveSize(optimalSize);
+    }
+  }, [boundingRect]);
+
+  const defaultImage = image[responsiveSize];
   const altText = alt || image.name;
-  return <img {...props} src={DOMAIN + defaultImage} alt={altText} />;
+
+  return <img {...props} ref={ref} src={DOMAIN + defaultImage} alt={altText} />;
 };
 
 export default ResponsiveImage;
