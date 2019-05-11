a4b57d0f40d5548b4934914b75870ad5ce3e16a7
Henrik Hørlück Berg <henrik@horluck.no>
2019-05-11 23:21:56 +0200

    Implement related article carousel - Make the arrows only appear if there are enough elements - Change size of articleimage to lg from wide

--- src/common/components/Carousel/CarouselArrow.tsx
+++ src/common/components/Carousel/CarouselArrow.tsx
@@ -1,5 +1,6 @@
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons/';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import React from 'react';
 
 import styles from './carousel.less';
@@ -8,10 +9,11 @@ export interface IProps {
   direction: 'right' | 'left';
   onClick: () => void;
   disabled?: boolean;
+  className?: string;
 }
 
-const CarouselArrow = ({ direction, onClick, disabled = false }: IProps) => (
-  <button onClick={onClick} disabled={disabled} className={styles.carouselArrow}>
+const CarouselArrow = ({ direction, onClick, disabled = false, className }: IProps) => (
+  <button onClick={onClick} disabled={disabled} className={classNames(styles.carouselArrow, className)}>
     <FontAwesomeIcon icon={direction === 'left' ? faArrowLeft : faArrowRight} size="2x" />
   </button>
 );
