55b33c3f0f448f12f27d0c44fbf88043d1053533
Henrik Hørlück Berg <henrik@horluck.no>
2019-05-13 03:03:49 +0200

    Refactoring, change design to ~"On-Paper-style-v2" - Add regex to add space to the end of ## because ReactMarkdown wasn't recognizing it as a title

--- src/articles/components/ArticleView/ArticleMeta.tsx
+++ src/articles/components/ArticleView/ArticleMeta.tsx
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon';
-import React from 'react';
+import React, { FC } from 'react';
 
 import { IArticle } from 'articles/models/Article';
 
@@ -9,24 +9,20 @@ export interface IProps {
   article: IArticle;
 }
 
-export const ArticleMeta = ({ article }: IProps) => {
-  const { published_date, changed_date, authors, tags } = article;
+export const ArticleMeta: FC<IProps> = ({ article }) => {
+  const { published_date, changed_date, tags } = article;
   const pubDateTime = DateTime.fromISO(published_date);
   const changeDateTime = DateTime.fromISO(changed_date);
 
   return (
-    <div className={style.bylineContainer}>
-      <div className={style.byline}>
-        <span>Publisert </span>
-        <time dateTime={published_date}>{pubDateTime.toLocaleString()}</time>
-        <span> Skrevet av </span>
-        {authors}
-        {published_date !== changed_date && pubDateTime < changeDateTime ? (
-          <>
-            <span className={style.lastChanged}>Sist endret </span>
-            <time>{changeDateTime.toLocaleString()}</time>
-          </>
-        ) : null}
+    <div className={style.articleMeta}>
+      {published_date !== changed_date && pubDateTime < changeDateTime && (
+        <div>
+          <span className={style.lastChanged}>Sist endret </span>
+          <time>{changeDateTime.toLocaleString()}</time>
+        </div>
+      )}
+      <div>
         <span>Tags </span>
         {tags.map((tag) => (
           <span className={style.tags} key={tag}>
