e6643d2a62907d302a089be2e616840a141d4162
sardred <andelar96@gmail.com>
2019-05-15 21:37:12 +0200

    Moved sponsor to footer

--- src/core/components/Footer/index.tsx
+++ src/core/components/Footer/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import style from './footer.less';
 import SocialLinks from './SocialLinks';
+import Img from 'common/components/Img';
 
 const Footer = () => (
   <footer className={style.footer}>
-    <SocialLinks />
-    <p className={style.developers}>
+    <a href="http://www.knowit.no/" className={style.sponsor}>
+      <Img src="/static/img/knowit.svg" alt="Hovedsamarbeidspartner - Knowit" />
+    </a>
+    <p>
       Har du funnet en feil på nettsiden?
       <br />
       Ta kontakt med <a href="mailto:dotkom@online.ntnu.no">Utviklingsteamet</a>
     </p>
+    <SocialLinks />
   </footer>
 );
 
