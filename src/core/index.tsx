4224eab9f460219fb5372f97ae9a212a188fca25
oleast <oleast@stud.ntnu.no>
2019-05-25 01:00:46 +0200

    Create components to display toast messages

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -2,12 +2,14 @@ import React, { FC } from 'react';
 import Footer from './components/Footer/index';
 import Header from './components/Header/index';
 import './less/core.less';
+import { ToastMessages } from './utils/toast/ToastMessages';
 
 const Core: FC = ({ children }) => (
   <>
     <Header />
     <main>{children}</main>
     <Footer />
+    <ToastMessages />
   </>
 );
 
