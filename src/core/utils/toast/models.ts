445468ae46115bf9d0e433aabb5afcfb98fbb61a
oleast <oleast@stud.ntnu.no>
2019-05-25 00:52:21 +0200

    Create toast models

---
+++ src/core/utils/toast/models.ts
@@ -0,0 +1,31 @@
+export type ToastType = 'info' | 'success' | 'warning' | 'error';
+
+export interface IToastMessage {
+  id: number;
+  content: string;
+  /** milliseconds */
+  duration: number;
+  type: ToastType;
+}
+
+export const DEFAULT_DURATION = 6 * 1000;
+export const DEFAULT_TYPE: ToastType = 'info';
+
+export const DEFAULT_MESSAGE: Partial<IToastMessage> = {
+  duration: DEFAULT_DURATION,
+  type: DEFAULT_TYPE,
+};
+
+export const getToastColor = (type: ToastType) => {
+  switch (type) {
+    /** Use event colors for now, since we dont have specific colors for this */
+    case 'error':
+      return '#eb536e';
+    case 'warning':
+      return '#faa21b';
+    case 'success':
+      return '#43b171';
+    case 'info':
+      return '#fff';
+  }
+};
