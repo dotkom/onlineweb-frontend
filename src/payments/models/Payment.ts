37686cd0d10e6242aa2302b413fdb5a68d6debfc
oleast <oleast@stud.ntnu.no>
2019-05-28 01:26:29 +0200

    Add model definitions for Payments

---
+++ src/payments/models/Payment.ts
@@ -0,0 +1,17 @@
+export type PaymentStatus = 'pending' | 'succeeded' | 'done' | 'refunded' | 'removed';
+
+/** Payment Types are currently implemented by a 'used_stripe' variable. */
+export type PaymentType = 'cash' | 'stripe';
+
+export interface IPaymentPrice {
+  id: number;
+  price: number;
+  description: string;
+}
+
+export interface IPayment {
+  id: number;
+  payment_prices: IPaymentPrice[];
+  description: string;
+  stripe_public_key: string;
+}
