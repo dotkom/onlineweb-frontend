c064f52622b2a94b6683b3eb401bcf14abe3419f
Henrik Hørlück Berg <henrik@horluck.no>
2019-05-29 14:10:16 +0200

    Add lacking trailing slash, that made API-auth not work in Safari

--- src/profile/api/orders.ts
+++ src/profile/api/orders.ts
@@ -2,7 +2,7 @@ import { IAuthUser } from 'authentication/models/User';
 import { getAllPages } from 'common/utils/api';
 import { IOrderLine } from 'profile/models/Orders';
 
-const API_URL = '/api/v1/profile/orders';
+const API_URL = '/api/v1/profile/orders/';
 
 export const getOrders = async (user: IAuthUser): Promise<IOrderLine[]> => {
   const results = await getAllPages<IOrderLine>(API_URL, { page_size: 80 }, { user });
