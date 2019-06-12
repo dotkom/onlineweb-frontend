6584815cde5f9d6aa6ccbfaf76d8264be0c616c9
oleast <oleast@stud.ntnu.no>
2019-06-12 12:36:20 +0200

    Refactor payment request to a hook and separate components

---
+++ src/payments/components/Transactions/CreateTransaction/PaymentRequestButton/index.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import { PaymentRequestButtonElement, ReactStripeElements } from 'react-stripe-elements';
+
+import { IGenericReturn } from 'payments/api/paymentTransaction';
+import { usePaymentRequest } from 'payments/hooks/usePaymentRequest';
+
+import { PaymentMethodDescription } from '../PaymentMethodDescription';
+import { PlaceholderButton } from './PlaceholderButton';
+
+export type PaymentRequest = stripe.paymentRequest.StripePaymentRequest;
+
+const STYLE: stripe.elements.ElementsOptions['style'] = {
+  paymentRequestButton: {
+    type: 'buy',
+    theme: 'dark',
+    height: '44px',
+  },
+};
+
+export interface IProps {
+  stripe: ReactStripeElements.StripeProps;
+  amount: number;
+  label: string;
+  onPaymentMethod: (paymentMethod: {}) => Promise<IGenericReturn['status']>;
+}
+
+export const PaymentRequestButton: FC<IProps> = ({ stripe, amount, label, onPaymentMethod }) => {
+  const { canMakePayment, paymentRequest, loading } = usePaymentRequest(stripe, amount, label, onPaymentMethod);
+
+  return (
+    <div>
+      <PaymentMethodDescription>Betal med Google/Apple Pay</PaymentMethodDescription>
+      {canMakePayment === true && paymentRequest !== null ? (
+        <PaymentRequestButtonElement paymentRequest={paymentRequest} style={STYLE} />
+      ) : (
+        <PlaceholderButton>{loading ? 'Laster...' : 'Ikke støttet'}</PlaceholderButton>
+      )}
+    </div>
+  );
+};
