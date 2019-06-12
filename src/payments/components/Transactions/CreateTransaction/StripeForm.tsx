23963bed11fda4adcdc8a6f007043ea96584c88d
oleast <oleast@stud.ntnu.no>
2019-06-12 12:37:17 +0200

    Refacor payment transaction creation form to separate components

--- src/payments/components/Transactions/CreateTransaction/StripeForm.tsx
+++ src/payments/components/Transactions/CreateTransaction/StripeForm.tsx
@@ -1,5 +1,5 @@
-import React, { ChangeEvent, FC, FormEvent, useState } from 'react';
-import { CardElement, injectStripe, ReactStripeElements } from 'react-stripe-elements';
+import React, { FC, useState } from 'react';
+import { injectStripe, ReactStripeElements } from 'react-stripe-elements';
 
 import { md } from 'common/components/Markdown';
 import { useToast } from 'core/utils/toast/useToast';
@@ -10,27 +10,24 @@ import {
   IGenericReturn,
 } from 'payments/api/paymentTransaction';
 
+import { CardPayment } from './CardPayment';
 import style from './createTransaction.less';
 import { PaymentRequestButton } from './PaymentRequestButton';
+import { DEFAULT_SALDO_VALUE, SaldoSelect } from './SaldoSelect';
 
 const ABOUT_CREATE_TRANSACTION = md`
 ## Legg til Saldo
 `;
 
-const ABOUT_PAYMENT_REQUEST = md`
-### Betal med Google/Apple Pay
-`;
-
 export interface IProps extends ReactStripeElements.InjectedStripeProps {}
-
-const SALDO_VALUES = [100, 200, 500];
 
 export const Form: FC<IProps> = ({ stripe }) => {
   const [displayError] = useToast({ type: 'error', duration: 12000 });
   const [displayMessage] = useToast({ duration: 12000, overwrite: true });
   const [processing, setProcessing] = useState(false);
-  const [amount, setAmount] = useState(SALDO_VALUES[0]);
+  const [amount, setAmount] = useState(DEFAULT_SALDO_VALUE);
 
+  /** Handle payment statuses and display messages apropriatly to the user. */
   const handleResponse = ({ status, message }: IGenericReturn) => {
     if (status === 'error') {
       displayError(message);
@@ -39,19 +36,27 @@ export const Form: FC<IProps> = ({ stripe }) => {
     }
   };
 
-  const handlePaymentMethod = async (paymentMethod: {}) => {
+  /**
+   * Handle creation of a payment method. Send payment data to the server.
+   * Used by Card payments and Payment Request payments.
+   */
+  const handlePaymentMethod = async (paymentMethod: {}): Promise<IGenericReturn['status']> => {
     if (stripe) {
       const transactionResponse = await createTransaction(amount, paymentMethod);
       handleResponse(transactionResponse);
       if (transactionResponse.status === 'pending' && transactionResponse.transaction) {
         const verifyResponse = await handleCardVerification(stripe, transactionResponse.transaction);
         handleResponse(verifyResponse);
+        return verifyResponse.status;
+      } else {
+        return transactionResponse.status;
       }
     }
+    return 'error';
   };
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  /** Handle submit from the CardPayment Form */
+  const handleSubmit = async () => {
     if (!stripe) {
       displayError('Det skjedde noe galt med koblingen til betalingssystemet.');
       return;
@@ -66,35 +71,22 @@ export const Form: FC<IProps> = ({ stripe }) => {
     setProcessing(false);
   };
 
-  const onAmountChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setAmount(Number(event.target.value));
-  };
-
   return (
     <div className={style.container}>
       {ABOUT_CREATE_TRANSACTION}
-      <form onSubmit={handleSubmit} className={style.form}>
-        <CardElement style={{ base: { fontSize: '18px' } }} />
-        <div className={style.subForm}>
-          <select onChange={onAmountChange} value={amount}>
-            {SALDO_VALUES.map((value) => (
-              <option value={value} key={value}>{`${value} kr`}</option>
-            ))}
-          </select>
-          <button disabled={processing} className={style.payButton}>
-            Betal
-          </button>
-        </div>
-      </form>
-      {ABOUT_PAYMENT_REQUEST}
-      {stripe ? (
-        <PaymentRequestButton
-          stripe={stripe}
-          amount={amount}
-          label="Saldoinnskudd"
-          onPaymentMethod={handlePaymentMethod}
-        />
-      ) : null}
+      <SaldoSelect onChange={setAmount} selected={amount} />
+      <div className={style.paymentMethods}>
+        <CardPayment onSubmit={handleSubmit} processing={processing} />
+        <div className={style.paymentsDivider} />
+        {stripe ? (
+          <PaymentRequestButton
+            stripe={stripe}
+            amount={amount}
+            label="Saldoinnskudd"
+            onPaymentMethod={handlePaymentMethod}
+          />
+        ) : null}
+      </div>
     </div>
   );
 };
