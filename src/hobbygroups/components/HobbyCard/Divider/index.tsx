903513d09d3cc78369a0d6126e585363e2e9244e
plusk <andelar96@gmail.com>
2019-06-13 11:27:57 +0200

    Merged some files, re'd some designs

--- src/hobbygroups/components/HobbyCard/Divider/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import style from './divider.less';
-
-export const Divider = () => <div className={style.divider} />;
--- src/hobbygroups/components/HobbyCard/Title.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC } from 'react';
-
-import style from './card.less';
-
-export const Title: FC = ({ children }) => <h2 className={style.title}>{children}</h2>;
