ad7a6cfe3eb3d1a9adce9adfd24e0f2c7445fa95
Torjus Iveland <torjusti@gmail.com>
2019-09-11 20:21:34 +0200

    Change path for event payments

--- src/payments/index.tsx
+++ src/payments/index.tsx
@@ -8,14 +8,14 @@ import { Wallet } from 'payments/components/Wallet';
 const BASE_ROUTE = '/payments';
 
 export const routes = {
-  relation: BASE_ROUTE + '/:event',
+  relation: BASE_ROUTE + '/event/:id',
   wallet: BASE_ROUTE + '/wallet',
 };
 
 export const PaymentsRouter: FC = () => (
   <Switch>
     <Route exact path={routes.wallet} component={Wallet} />
-    <Route exact path={routes.relation} render={({ match }) => <EventPayment eventId={match.params.event} />} />
+    <Route exact path={routes.relation} render={({ match }) => <EventPayment eventId={match.params.id} />} />
     <Route path="*" render={() => <HttpError code={404} />} />
   </Switch>
 );
