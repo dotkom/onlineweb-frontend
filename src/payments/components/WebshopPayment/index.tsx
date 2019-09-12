b25fc46af3befaa6352c54cbabd10f490ff8b834
plusk <andelar96@gmail.com>
2019-09-12 21:49:52 +0200

    Expanded information and primitively styled webshop payment

--- src/payments/components/WebshopPayment/index.tsx
+++ src/payments/components/WebshopPayment/index.tsx
@@ -4,6 +4,7 @@ import Spinner from 'common/components/Spinner';
 import { IOrderLine } from 'profile/models/Orders';
 import { getOrderLines } from 'webshop/api';
 import { Payment } from '../Payment';
+import style from '../Payment/payment.less';
 
 export const WebshopPayment = () => {
   const [orderLines, setOrderLines] = useState<IOrderLine[]>();
@@ -19,19 +20,48 @@ export const WebshopPayment = () => {
 
   const latestOrderline = orderLines && orderLines.find((line) => !line.paid);
 
+  let webshopPayment;
+
   if (!orderLines) {
-    return <Spinner />;
+    webshopPayment = <Spinner />;
+  } else if (!latestOrderline || latestOrderline.orders.length === 0) {
+    webshopPayment = (
+      <p>
+        Du har ingen varer i handlekurven din. Gå til <a href="https://online.ntnu.no/webshop">webshop</a> for å kjøpe
+        varer!
+      </p>
+    );
+  } else if (latestOrderline) {
+    const orders = latestOrderline.orders.map((order) => (
+      <div key={order.id} className={style.order}>
+        {order.product.images.length ? (
+          <img src={order.product.images[0].thumb} alt={order.product.name} className={style.image} />
+        ) : (
+          <div className={style.image}>Prokom har ikke lagt til et bilde for dette produktet enda</div>
+        )}
+        <div className={style.orderDetails}>
+          <h3>
+            {order.product.name} {order.size && <>({order.size})</>}
+          </h3>
+          <p>{order.product.description}</p>
+          <p>
+            {order.quantity} x {(order.product.price * 100) / 100}kr = {order.quantity * order.product.price}kr
+          </p>
+        </div>
+      </div>
+    ));
+    webshopPayment = (
+      <Payment payment={latestOrderline.payment} price={latestOrderline.payment.payment_prices[0]} showPayment>
+        <div>{orders}</div>
+        <p className={style.subtotal}>
+          <span>Totalt: </span>
+          {latestOrderline.subtotal}kr
+        </p>
+      </Payment>
+    );
+  } else {
+    webshopPayment = <HttpError code={404} />;
   }
 
-  return (
-    <>
-      {!latestOrderline || latestOrderline.orders.length === 0 ? (
-        <p>du har ingen varer i handlekorga di, kjøp noe daaaa</p>
-      ) : (
-        <Payment payment={latestOrderline.payment} price={latestOrderline.payment.payment_prices[0]} showPayment={true}>
-          <p>ayy lmao you owe us 1x orderline @ {latestOrderline.subtotal} NOK click the things below to pay</p>
-        </Payment>
-      )}
-    </>
-  );
+  return webshopPayment;
 };
