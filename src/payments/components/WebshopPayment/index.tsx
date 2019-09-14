d7d5e99fd78187e4651c67e319f2c73148ccfad3
Torjus Iveland <torjusti@gmail.com>
2019-09-15 00:12:50 +0200

    Move orders together with the rest of the webshop TSX

--- src/payments/components/WebshopPayment/index.tsx
+++ src/payments/components/WebshopPayment/index.tsx
@@ -29,28 +29,28 @@ export const WebshopPayment = () => {
     return <EmptyCart />;
   }
 
-  const orders = latestOrderline.orders.map((order) => (
-    <div key={order.id} className={style.order}>
-      {order.product.images.length ? (
-        <img src={order.product.images[0].thumb} alt={order.product.name} className={style.image} />
-      ) : (
-        <div className={style.image}>Det har ikke blitt lagt til et bilde for dette produktet enda</div>
-      )}
-      <div className={style.orderDetails}>
-        <h3>
-          {order.product.name} {order.size && <>({order.size})</>}
-        </h3>
-        <p>{order.product.description}</p>
-        <p>
-          {order.quantity} x {(order.product.price * 100) / 100}kr = {order.quantity * order.product.price}kr
-        </p>
-      </div>
-    </div>
-  ));
-
   return (
     <Payment payment={latestOrderline.payment} price={latestOrderline.payment.payment_prices[0]} showPayment>
-      <div>{orders}</div>
+      <div>
+        {latestOrderline.orders.map((order) => (
+          <div key={order.id} className={style.order}>
+            {order.product.images.length ? (
+              <img src={order.product.images[0].thumb} alt={order.product.name} className={style.image} />
+            ) : (
+              <div className={style.image}>Det har ikke blitt lagt til et bilde for dette produktet enda</div>
+            )}
+            <div className={style.orderDetails}>
+              <h3>
+                {order.product.name} {order.size && <>({order.size})</>}
+              </h3>
+              <p>{order.product.description}</p>
+              <p>
+                {order.quantity} x {(order.product.price * 100) / 100}kr = {order.quantity * order.product.price}kr
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
       <p className={style.subtotal}>
         <span>Totalt: </span>
         {latestOrderline.subtotal}kr
