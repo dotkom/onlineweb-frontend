624af318777eefc915ed55aa4ab25dc13230472e
oleast <oleast@stud.ntnu.no>
2019-09-15 17:03:24 +0200

    move shop type imports

--- src/profile/api/orders.ts
+++ src/profile/api/orders.ts
@@ -3,7 +3,7 @@ import { DateTime } from 'luxon';
 import { IAuthUser } from 'authentication/models/User';
 import { getAllPages } from 'common/utils/api';
 import { IOrderData } from 'payments/components/Transactions/Purchases';
-import { IOrderLine } from 'profile/models/Orders';
+import { IOrderLine } from 'shop/models';
 
 const API_URL = '/api/v1/profile/orders/';
 
@@ -16,7 +16,7 @@ export const transformOrderData = (orderLines: IOrderLine[]): IOrderData[] => {
         date: DateTime.fromISO(orderLine.datetime),
         paid: orderLine.paid,
         price: data.price,
-        product: data.product.name,
+        product: data.content_object.name,
       });
     }
   }
