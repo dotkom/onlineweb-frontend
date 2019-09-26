d62096ddc572f27ac791a22b73d6120d9ceaf2f4
kharann <anhkhav@gmail.com>
2019-09-26 13:29:37 +0200

    added get method to show correct user emails instead of placeholders

--- src/profile/api/mail.ts
+++ src/profile/api/mail.ts
@@ -1,14 +1,13 @@
 import { IMail } from '../models/Mail';
+import { get, IAPIData } from 'common/utils/api';
+import { getUser } from 'authentication/api';
 
+const API_URL = '/api/v1/user/emails/';
 /**
  *
  */
 export const getMails = async (): Promise<IMail[]> => {
-  // const data = await get(API_URL + '/mails', { format: 'json' });
-  const mails: IMail[] = [
-    { email: 'ola.nordmann@online.ntnu.no', primary: false },
-    { email: 'olanor@stud.ntnu.no', primary: false },
-    { email: 'dragonslayer@1337.no', primary: true },
-  ];
-  return mails;
+  const user = await getUser();
+  const data = await get<IAPIData<IMail>>(API_URL, { format: 'json' }, { user });
+  return data.results;
 };
