d69aca29b4c78eabcac31189c6621f29137452a4
kharann <anhkhav@gmail.com>
2019-09-30 17:34:54 +0200

    added patch function for mails

--- src/profile/api/mail.ts
+++ src/profile/api/mail.ts
@@ -1,6 +1,7 @@
-import { IMail } from '../models/Mail';
-import { get, IAPIData } from 'common/utils/api';
 import { getUser } from 'authentication/api';
+import { get, IAPIData, patch } from 'common/utils/api';
+
+import { IMail } from '../models/Mail';
 
 const API_URL = '/api/v1/user/emails/';
 /**
@@ -10,4 +11,16 @@ export const getMails = async (): Promise<IMail[]> => {
   const user = await getUser();
   const data = await get<IAPIData<IMail>>(API_URL, { format: 'json' }, { user });
   return data.results;
+};
+
+export const patchMails = async (addressId: number, data: Partial<IMail>): Promise<IMail> => {
+  const user = await getUser();
+  const response = await patch<IMail, Partial<IMail>>({
+    query: `${API_URL}/${addressId}/`,
+    data,
+    parameters: { format: 'json' },
+    options: { user },
+  });
+
+  return response;
 };
--- src/profile/components/Settings/Mails/index.tsx
+++ src/profile/components/Settings/Mails/index.tsx
@@ -1,6 +1,6 @@
 import { Pane } from 'common/components/Panes';
 import React, { Component } from 'react';
-import { getMails } from '../../../api/mail';
+import { getMails, patchMails } from '../../../api/mail';
 import { IMail } from '../../../models/Mail';
 import Mail from './Mail';
 
@@ -13,26 +13,22 @@ class Mails extends Component<{}, IState> {
 
   public async componentDidMount() {
     let addresses = await getMails();
-    // Opinion: Primary address should be shown on top when the site first loads.
-    // But the list should not be sorted all over again when an option is clicked.
-    // Therefore the addresses are sorted on component mount.
     addresses = addresses.sort((a, b) => Number(a.primary) - Number(b.primary)).reverse();
     this.setState({ addresses });
   }
 
-  public togglePrimary(index: number) {
-    const { addresses } = this.state;
-    const reset: IMail[] = addresses.map((addr) => ({ ...addr, primary: false }));
-    reset[index].primary = true;
-    this.setState({ addresses: reset });
+  public async togglePrimary(address: IMail) {
+    await patchMails(address.id, { primary: true });
+    const addresses = await getMails();
+    this.setState({ addresses });
   }
 
   public render() {
     const { addresses } = this.state;
     return (
       <Pane>
-        {addresses.map((addr, index) => (
-          <Mail {...addr} toggle={() => this.togglePrimary(index)} key={addr.email} />
+        {addresses.map((addr) => (
+          <Mail {...addr} toggle={() => this.togglePrimary(addr)} key={addr.email} />
         ))}
       </Pane>
     );
