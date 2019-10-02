71ff70bc5d93ec2c194c5fb1c8b5ebbc702017b4
VigdisIS <vigdis.steinsund@hotmail.com>
2019-10-02 20:02:07 +0200

    fixes more imports

--- src/frontpage/components/Articles/MainArticlePlaceholder.tsx
+++ src/frontpage/components/Articles/MainArticlePlaceholder.tsx
@@ -1,6 +1,6 @@
 import classnames from 'classnames';
-import React from 'react';
 import { DefaultEventImage } from 'events/components/DefaultEventImage';
+import React from 'react';
 import style from './articles.less';
 
 const MainArticlePlaceholder = () => {
