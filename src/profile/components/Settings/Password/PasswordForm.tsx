11f6dfde69ed81dfceea97aa2d14113f99595937
kharann <anhkhav@gmail.com>
2019-10-10 01:51:32 +0200

    Added functionality to change user password

--- src/profile/components/Settings/Password/PasswordForm.tsx
+++ src/profile/components/Settings/Password/PasswordForm.tsx
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import style from './input.less';
-import classNames from 'classnames';
-import Markdown, { md } from 'common/components/Markdown';
 import PasswordInput from './PasswordInput';
-
-const OLD_PASSWORD = md`
-### Skriv inn ditt nåverende passord
-`;
-
-const NEW_PASSWORD = md`
-### Skriv inn et nytt passord
-`;
-
-const NEW_PASSWORD_REQUIREMENTS = md`
-- Passordet ditt må bestå av minst 8 tegn.
-`;
-
-const CONFIRM_PASSWORD = md`
-### Gjenta det nye passordet
-`;
+import { putPasswords } from 'profile/api/password';
+import { IChangePasswordData, IChangePasswordResponse } from 'profile/models/Password';
+import { ErrorMessage } from './ErrorMessage';
 
 const PasswordForm = () => {
+  const [inputs, setInputs] = useState<IChangePasswordData>({
+    current_password: '',
+    new_password: '',
+    new_password_confirm: '',
+  });
+  const [errors, setErrors] = useState<IChangePasswordResponse>({});
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    let response = await putPasswords(inputs);
+    if (response) {
+      setErrors(response);
+    }
+  };
+
+  const handleOnChange = (event: FormEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const { name, value } = event.currentTarget;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
   return (
-    <form class={style.editPasswordInput}>
-      <PasswordInput source="### Skriv inn ditt nåverende passord" name="old_password" />
+    <form id={style.editPasswordInput} onSubmit={handleSubmit}>
+      <PasswordInput
+        label="Skriv inn ditt nåverende passord"
+        name="current_password"
+        requiredMessage={errors.current_password}
+        required
+        onChange={handleOnChange}
+      />
+      <PasswordInput
+        label="Skriv inn et nytt passord"
+        name="new_password"
+        requiredMessage={errors.new_password}
+        required
+        onChange={handleOnChange}
+      />
+      <PasswordInput
+        label="Gjenta det nye passordet"
+        name="new_password_confirm"
+        requiredMessage={errors.new_password_confirm}
+        required
+        onChange={handleOnChange}
+      />
+      <ErrorMessage errors={errors.non_field_errors} />
+      <button type="submit" className={style.changePasswordButton}>
+        Endre passord
+      </button>
     </form>
   );
 };
