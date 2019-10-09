e3fc2ea27fa7102c5dae79e69dec92407a245c93
blauks <brooks.jonathan1@gmail.com>
2019-10-09 20:58:08 +0200

    Added logic such that when an event was before the current year, ListEvent displays the event year

--- src/events/components/ListView/ListEvent.tsx
+++ src/events/components/ListView/ListEvent.tsx
@@ -9,7 +9,11 @@ import style from './list.less';
 const ListEvent = ({ title, event_start, attendance_event, event_type, company_event }: IEvent) => {
   const eventColor = getEventColor(event_type);
   const eventType = getEventType(event_type);
-  const eventDate = DateTime.fromISO(event_start).toFormat('dd.MM');
+  const eventDateTime = DateTime.fromISO(event_start);
+  const eventDate =
+    eventDateTime.year < new Date().getFullYear()
+      ? eventDateTime.toFormat('dd.MM.yyyy')
+      : eventDateTime.toFormat('dd.MM');
   const eventAttendees = getEventAttendees(attendance_event);
 
   return (
