1c388c29e330f673180f31b2dbe95b283b8d5649
kharann <anhkhav@gmail.com>
2019-10-10 03:37:13 +0200

    add case when getUser yields undefined

--- src/profile/api/password.ts
+++ src/profile/api/password.ts
@@ -7,12 +7,15 @@ const API_URL = '/api/v1/users/';
 export const putPasswords = async (data: IChangePasswordData): Promise<IChangePasswordResponse | null> => {
   try {
     const user = await getUser();
-    const response = await put<IChangePasswordResponse, IChangePasswordData>({
-      query: API_URL + user.profile.sub + `/change_password`,
-      data,
-      options: { user },
-    });
-    return response;
+    if (user) {
+      const response = await put<IChangePasswordResponse, IChangePasswordData>({
+        query: API_URL + user.profile.sub + `/change_password`,
+        data,
+        options: { user },
+      });
+      return response;
+    }
+    return null;
   } catch {
     return null;
   }
