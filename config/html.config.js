8eba9fc7174aed588ca9f0f6d99ad533b872a907
oleast <oleast@stud.ntnu.no>
2020-04-02 17:24:15 +0200

    Remove lots of now unneeded packages, purge webpack config

--- config/html.config.js
+++
@@ -1,8 +0,0 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  plugin: new HtmlWebpackPlugin({
-    template: './static/index.html',
-    filename: 'index.html'
-  }),
-}
