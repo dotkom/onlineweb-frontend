5ba41c7e52b960dde6ba0403be25f44657b7a9b3
Johannes Kvamme <22146437+FluidSense@users.noreply.github.com>
2020-04-04 22:04:31 +0200

    prettier with same version as lock

--- src/core/redux/Store.tsx
+++ src/core/redux/Store.tsx
@@ -11,7 +11,14 @@ const reduxDevToolsHook = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_D
 const thunkMiddleware = thunk as ThunkMiddleware<State, Action>;
 
 export const initStore = (initialState?: State) =>
-  createStore(rootReducer, initialState, compose(applyMiddleware(thunkMiddleware), reduxDevToolsHook));
+  createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(thunkMiddleware),
+      reduxDevToolsHook
+    )
+  );
 
 export const store = initStore();
 
