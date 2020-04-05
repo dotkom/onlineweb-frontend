f3dfacd858b5579b193b70324853199b91445422
Johannes Kvamme <22146437+FluidSense@users.noreply.github.com>
2020-04-05 17:33:53 +0200

    remove unused import

--- src/core/components/Footer/ContactInfo.tsx
+++ src/core/components/Footer/ContactInfo.tsx
@@ -1,7 +1,6 @@
 import { faBriefcase, faEnvelope, faFile, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import style from './footer.less';
 
 const ContactInfo = () => (
   <>
