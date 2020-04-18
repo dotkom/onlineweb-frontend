4644b0de38d7561022fbbb0df3230357d3cfda0f
Johannes Kvamme <22146437+FluidSense@users.noreply.github.com>
2020-04-18 17:45:34 +0200

    add initial render

--- src/events/components/DetailView/index.tsx
+++ src/events/components/DetailView/index.tsx
@@ -3,15 +3,15 @@ import React, { useEffect } from 'react';
 
 import { DOMAIN } from 'common/constants/endpoints';
 
+import Spinner from 'common/components/Spinner';
+import { useDispatch, useSelector } from 'core/redux/hooks';
+import { eventSelectors, fetchEventById } from 'events/slices/events';
+import NotFoundPage from 'pages/404';
 import Contact from './Contact';
 import style from './detail.less';
 import InfoBox from './InfoBox';
 import PictureCard from './PictureCard';
 import Registration from './Registation';
-import { useDispatch, useSelector } from 'core/redux/hooks';
-import Spinner from 'common/components/Spinner';
-import NotFoundPage from 'pages/404';
-import { eventSelectors, fetchEventById } from 'events/slices/events';
 
 export interface IProps {
   eventId: number;
@@ -19,14 +19,12 @@ export interface IProps {
 
 export const DetailView = ({ eventId }: IProps) => {
   const dispatch = useDispatch();
-  const event = useSelector((state) => eventSelectors.selectById(state, eventId))
+  const event = useSelector((state) => eventSelectors.selectById(state, eventId));
   const isPending = useSelector((state) => state.articles.loading === 'pending');
-  //const event = eventMap.get(eventId) || mockEvent;
-  
+
   useEffect(() => {
     dispatch(fetchEventById(eventId));
-  }, [eventId, dispatch])
-
+  }, [eventId, dispatch]);
 
   if (isPending && !event) {
     return <Spinner />;
--- src/pages/events/[eventId].tsx
+++ src/pages/events/[eventId].tsx
@@ -1,12 +1,29 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 
+import { Store } from 'core/redux/Store';
 import DetailView from 'events/components/DetailView';
+import { eventSelectors, fetchEventById } from 'events/slices/events';
+import { NextPageContext } from 'next';
+
+interface IContext extends NextPageContext {
+  store: Store;
+}
 
 const EventDetailPage = () => {
   const router = useRouter();
   const eventId = Number(router.query.eventId);
   return <DetailView eventId={eventId} />;
+};
+
+EventDetailPage.getInitialProps = async ({ query, store }: IContext) => {
+  const eventId = Number(query.eventId);
+  const isEventInStore = Boolean(eventSelectors.selectById(store.getState(), eventId));
+  const result = store.dispatch(fetchEventById(eventId));
+  if (!isEventInStore) {
+    await result;
+  }
+  return { eventId };
 };
 
 export default EventDetailPage;
