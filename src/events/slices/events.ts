c5cf5d6c7394d6f371498685e18e3408586e44c0
Johannes Kvamme <22146437+FluidSense@users.noreply.github.com>
2020-04-18 17:08:27 +0200

    use Redux for detail events

---
+++ src/events/slices/events.ts
@@ -0,0 +1,67 @@
+import { createAsyncThunk, createEntityAdapter, createSlice, SerializedError } from '@reduxjs/toolkit';
+import { IEvent } from 'events/models/Event';
+import { DateTime } from 'luxon';
+import { State } from 'core/redux/Store';
+import { getEvent, getEvents } from 'events/api/events';
+
+const eventsAdapter = createEntityAdapter<IEvent>({
+    sortComparer: (eventA, eventB) => {
+        return Number(DateTime.fromISO(eventA.event_start) > DateTime.fromISO(eventB.event_start))
+    },
+});
+
+export const eventSelectors = eventsAdapter.getSelectors<State>((state) => state.events);
+
+export const fetchEventsList = createAsyncThunk('events/fetchList', async () => {
+    const events = await getEvents();
+    return events;
+  });
+  
+export const fetchEventById = createAsyncThunk('events/fetchById', async (eventId: number) => {
+    const events = await getEvent(eventId);
+    return events;
+});
+
+interface IState {
+    loading: 'idle' | 'pending';
+    error: SerializedError | null;
+    entities: Record<number, IEvent>;
+}
+
+const INITIAL_STATE: IState = {
+    loading: 'idle',
+    error: null,
+    entities: {},
+};
+
+export const eventsSlice = createSlice({
+    name: 'events',
+    initialState: eventsAdapter.getInitialState(INITIAL_STATE),
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchEventsList.pending, (state) => {
+            state.loading = 'pending';
+        }),
+        builder.addCase(fetchEventsList.fulfilled, (state, action) => {
+            state.loading = 'idle';
+            eventsAdapter.addMany(state, action.payload);
+        }),
+        builder.addCase(fetchEventsList.rejected, (state, action) => {
+            state.loading = 'idle';
+            state.error = action.error;
+        }),
+        builder.addCase(fetchEventById.pending, (state) => {
+            state.loading = 'pending';
+        }),
+        builder.addCase(fetchEventById.fulfilled, (state, action) => {
+            state.loading = 'idle';
+            eventsAdapter.addOne(state, action.payload);
+        }),
+        builder.addCase(fetchEventById.rejected, (state, action) => {
+            state.loading = 'idle';
+            state.error = action.error;
+        })
+    },
+});
+
+export const eventsReducer = eventsSlice.reducer;
