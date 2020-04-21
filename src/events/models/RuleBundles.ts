29287b90871de9c8e1d20cabf9661e8f8813516f
oleast <oleast@stud.ntnu.no>
2020-04-21 13:06:25 +0200

    Add slice and api for rule-bundles

--- src/events/models/RuleBundles.ts
+++ src/events/models/RuleBundles.ts
@@ -1,15 +1,27 @@
 export interface IRuleBundle {
   id: number;
   description: string;
-  field_of_study_rules: FieldOfStudyRule[];
-  grade_rules: GradeRule[];
-  user_group_rules: UserGroupRule[];
+  field_of_study_rules: number[];
+  grade_rules: number[];
+  user_group_rules: number[];
   rule_strings: string[];
 }
 
-export type GradeRule = number;
+export interface IFieldOfStudyRule {
+  id: number;
+  offset: number;
+  field_of_study: number;
+  field_of_study_display: string;
+}
 
-// Do we ever use this? What is the type?
-export type UserGroupRule = void;
+export interface IGradeRule {
+  id: number;
+  offset: number;
+  grade: number;
+}
 
-export type FieldOfStudyRule = number;
+export interface IUserGroupRule {
+  id: number;
+  offset: number;
+  group: number;
+}
