36a9a637126e48d197959731a073f6c7ac34821a
oleast <oleast@stud.ntnu.no>
2020-04-24 17:25:38 +0200

    Select just ids instead of entire event object in calendar

--- src/events/components/CalendarView/CalendarEvent.tsx
+++ src/events/components/CalendarView/CalendarEvent.tsx
@@ -3,26 +3,39 @@ import React, { FC } from 'react';
 
 import HoverCard from 'common/components/HoverCard';
 import { getEventUrl } from 'core/appUrls';
-import { IEvent, getEventColor } from 'events/models/Event';
+import { getEventColor } from 'events/models/Event';
 
 import CalendarHoverCard from './CalendarHoverCard';
 
 import style from './CalendarEvent.less';
+import { State } from 'core/redux/Store';
+import { eventSelectors } from 'events/slices/events';
+import { useSelector } from 'core/redux/hooks';
 
 interface IProps {
-  event: IEvent;
+  eventId: number;
 }
 
-export const CalendarEvent: FC<IProps> = ({ event }) => {
+export const CalendarEvent: FC<IProps> = ({ eventId }) => {
+  const title = useSelector(selectEventTitle(eventId));
+  const eventType = useSelector(selectEventType(eventId));
   return (
-    <Link {...getEventUrl(event.id)}>
+    <Link {...getEventUrl(eventId)}>
       <a>
-        <HoverCard card={<CalendarHoverCard eventId={event.id} />}>
-          <p className={style.title} style={{ background: getEventColor(event.event_type) }}>
-            {event.title}
+        <HoverCard card={<CalendarHoverCard eventId={eventId} />}>
+          <p className={style.title} style={{ background: getEventColor(eventType) }}>
+            {title}
           </p>
         </HoverCard>
       </a>
     </Link>
   );
+};
+
+const selectEventTitle = (eventId: number) => (state: State) => {
+  return eventSelectors.selectById(state, eventId)?.title || '';
+};
+
+const selectEventType = (eventId: number) => (state: State) => {
+  return eventSelectors.selectById(state, eventId)?.event_type || 0;
 };
