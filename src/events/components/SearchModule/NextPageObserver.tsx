b985d04acacbf19ef7b32f2de4c68c5052471e17
oleast <oleast@stud.ntnu.no>
2020-04-26 08:16:12 +0200

    Add information about event search results to infinite scroller

--- src/events/components/SearchModule/NextPageObserver.tsx
+++ src/events/components/SearchModule/NextPageObserver.tsx
@@ -1,20 +1,47 @@
 import React, { FC, useEffect, useState } from 'react';
 
 import { useIntersection } from 'common/hooks/useIntersection';
-import { useDispatch } from 'core/redux/hooks';
+import { useDispatch, useSelector } from 'core/redux/hooks';
+import { State } from 'core/redux/Store';
 import { nextEventPage } from 'events/slices/events';
+
+import style from './NextPageObserver.less';
 
 export const NextPageObserver: FC = () => {
   const dispatch = useDispatch();
+  const isPending = useSelector(selectIsSearchPending());
+  const totalCount = useSelector(selectTotalCount());
+  const currentCount = useSelector(selectCurrentCount());
   const [observer, ref] = useIntersection();
-  const [count, setCount] = useState(0); // TODO: Implement inside useIntersection somehow.
+  const [renderCount, setRenderCount] = useState(0); // TODO: Implement inside useIntersection somehow.
+
+  const isSeachCompleted = totalCount === currentCount;
 
   useEffect(() => {
-    if (observer && observer.isIntersecting && count !== 0) {
+    if (observer && observer.isIntersecting && renderCount !== 0 && !isSeachCompleted && !isPending) {
       dispatch(nextEventPage());
     }
     // TODO: Figure out why this is needed, and implement it inside the hook.
-    setCount((stateCount) => stateCount + 1);
-  }, [observer, dispatch]);
-  return <span ref={ref}></span>;
+    setRenderCount((current) => current + 1);
+  }, [dispatch, observer, isSeachCompleted, isPending]);
+
+  return (
+    <>
+      <p className={style.infoText}>{`Viser ${currentCount} av ${totalCount} resultater for dette søket`}</p>
+      <p className={style.infoText}>{isPending && 'Henter flere resultater...'}</p>
+      <span ref={ref}></span>
+    </>
+  );
+};
+
+const selectIsSearchPending = () => (state: State) => {
+  return state.events.search.loading === 'pending';
+};
+
+const selectTotalCount = () => (state: State) => {
+  return state.events.search.count;
+};
+
+const selectCurrentCount = () => (state: State) => {
+  return state.events.search.ids.length;
 };
