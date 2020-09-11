8bea2b7030a3dca334680af5e743bb8ec26ba16e
FluidSense <22146437+FluidSense@users.noreply.github.com>
2020-09-11 15:38:02 +0200

    Split attendance button to multiple components

---
+++ src/events/components/Attendance/AttendButton.tsx
@@ -0,0 +1,32 @@
+import React, { FC, useState } from 'react';
+import Button from 'core/components/errors/NotAuthenticated/Button';
+import CaptchaModal from './CaptchaModal';
+import { useDispatch } from 'core/redux/hooks';
+import { setAttendeeByEventId } from 'events/slices/attendees';
+
+interface IAttendButtonProps {
+  eventId: number;
+}
+
+const AttendButton: FC<IAttendButtonProps> = (props: IAttendButtonProps) => {
+  const dispatch = useDispatch();
+  const { eventId } = props;
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const signUp = (token: string | null) => {
+    if (token) {
+      dispatch(setAttendeeByEventId({ eventId, captcha: token }));
+    }
+  };
+  const toggleModal = () => setShowModal(!showModal);
+  const modal = <CaptchaModal showModal={showModal} toggleModal={toggleModal} setRecaptcha={signUp} />;
+
+  return (
+    <div>
+      <Button onClick={toggleModal}>Meld meg på</Button>
+      {modal}
+    </div>
+  );
+};
+
+export default AttendButton;
---
+++ src/events/components/Attendance/index.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react';
+import { DateTime } from 'luxon';
+import { ISignupEligibility, IAttendanceEvent } from 'events/models/Event';
+import AttendButton from './AttendButton';
+import UnattendButton from './UnattendButton';
+
+interface IAttendButtonProps {
+  canAttend: ISignupEligibility | null;
+  event: IAttendanceEvent;
+  unattendDeadline: DateTime;
+}
+
+const Attendance: FC<IAttendButtonProps> = (props: IAttendButtonProps) => {
+  const { canAttend, event, unattendDeadline } = props;
+  const { id, is_attendee, is_on_waitlist, what_place_is_user_on_wait_list } = event;
+  const currentTime = DateTime.local();
+
+  // The canAttend-object is null if the user is not logged in, else it will always be present.
+  if (!canAttend) return <p>Du må være logget inn for å se din status.</p>;
+
+  if (currentTime < unattendDeadline && is_attendee) {
+    return (
+      <UnattendButton eventId={id} isOnWaitList={is_on_waitlist} waitListNumber={what_place_is_user_on_wait_list} />
+    );
+  }
+
+  // The user is logged in, is attending, but cannot unattend. This is due to expired unattend-possibility.
+  if (!canAttend.status && is_attendee) return <p>{`${canAttend.message} Avmeldingsfristen har utløpt.`}</p>;
+
+  // The user is logged in, but cannot attend. The reason is given by the canAttend.message-object from OW4.
+  if (!canAttend.status) return <p>{canAttend.message}</p>;
+
+  // Base-case: The user is logged in and can attend!
+  return <AttendButton eventId={id} />;
+};
+
+export default Attendance;
