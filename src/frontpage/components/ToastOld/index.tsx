6a3414158420f7ad2d4ff71a1dff50378a351d5e
Paalar <paaledward@gmail.com>
2020-09-11 20:08:39 +0200

    Hide toast if checked

--- src/frontpage/components/ToastOld/index.tsx
+++ src/frontpage/components/ToastOld/index.tsx
@@ -6,7 +6,10 @@ import { Checkbox } from '@dotkomonline/design-system';
 const SHOW_TOAST = 'showOldOWToast';
 
 const Message: React.FC = () => {
-  const saveDoNotShow = (isChecked: boolean) => window.localStorage.setItem(SHOW_TOAST, isChecked.toString());
+  const saveDoNotShow = (isChecked?: boolean) => {
+    console.log(isChecked);
+    window.localStorage.setItem(SHOW_TOAST, isChecked ? 'true' : 'false');
+  };
 
   return (
     <div>
@@ -27,8 +30,10 @@ const Message: React.FC = () => {
 const ToastOld: React.FC = () => {
   const [displayMessage] = useToast({ type: 'basic', overwrite: true, duration: 1000 * 60 * 60 });
   useEffect(() => {
+    // This should be inside of the useEffect
+    // With NextJs the window element may be null.
     const showToast = window.localStorage.getItem(SHOW_TOAST);
-    if (showToast) {
+    if (showToast === 'true' || showToast === undefined) { // Blame Johannes
       displayMessage(<Message />);
     }
   }, []);
