487a4299902341bb929e737a6f9990e303cacc71
kharann <anhkhav@gmail.com>
2020-09-11 19:47:44 +0200

    Add API call to check if user has accepted mark ruleset

---
+++ src/events/api/marksAcceptance.ts
@@ -0,0 +1,16 @@
+import { DateTime } from 'luxon';
+import { getUser } from 'authentication/api';
+import { get, IAPIData } from 'common/utils/api';
+
+const MARK_ACCEPTANCE_URL = `/api/v1/marks/acceptance`;
+
+interface IMarkAcceptance {
+  rule_set: string;
+  accepted_date: DateTime;
+}
+
+export const getHasMarkAccepted = async (): Promise<boolean> => {
+  const user = await getUser();
+  const markAcceptance = await get<IAPIData<IMarkAcceptance>>(MARK_ACCEPTANCE_URL, { format: 'json' }, { user });
+  return markAcceptance.results.length > 0;
+};
