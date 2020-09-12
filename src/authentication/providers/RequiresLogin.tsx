d8c501a0e5f9cb4b114b7d73582a1d982841ac3f
FluidSense <22146437+FluidSense@users.noreply.github.com>
2020-09-12 14:41:48 +0200

    Prettier

--- src/authentication/providers/RequiresLogin.tsx
+++ src/authentication/providers/RequiresLogin.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Spinner from "common/components/Spinner";
-import { getUser } from "authentication/api";
-import { useEffect, useState } from "react";
+import Spinner from 'common/components/Spinner';
+import { getUser } from 'authentication/api';
+import { useEffect, useState } from 'react';
 
 const RequiresLogin: React.FC = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -12,19 +12,18 @@ const RequiresLogin: React.FC = (props) => {
       const user = await getUser();
       if (user) {
         setIsLoggedIn(true);
-        setIsLoadingUser(false)
-      }
-      else {
+        setIsLoadingUser(false);
+      } else {
         setIsLoggedIn(false);
         setIsLoadingUser(false);
       }
-    }
+    };
     checkUser();
   }, []);
 
-  if (isLoadingUser) return (<Spinner />);
-  if (!isLoggedIn) return (<p>Du må være logget inn for å se denne siden.</p>);
+  if (isLoadingUser) return <Spinner />;
+  if (!isLoggedIn) return <p>Du må være logget inn for å se denne siden.</p>;
   return <>{props.children}</>;
-}
+};
 
 export default RequiresLogin;
