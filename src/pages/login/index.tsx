d6b417bb9b5ce1b25ea1e54f43ad98f6d29060e5
FluidSense <22146437+FluidSense@users.noreply.github.com>
2020-09-12 16:01:04 +0200

    Prettier

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,16 +4,18 @@ import style from './login.less';
 import { Button } from '@dotkomonline/design-system';
 
 const LoginPage = () => {
-    return (
-     <div>
-        <p>Siden du forsøkte å nå krever innlogging.</p>
-        <div className={style.loginMenu}>
-            <Button className={style.loginButton} onClick={logIn}>Logg inn</Button>
-            <Button>Registrer</Button>
-            <Button>Glemt passord</Button>
-        </div>
-     </div>
-    );
-}
+  return (
+    <div>
+      <p>Siden du forsøkte å nå krever innlogging.</p>
+      <div className={style.loginMenu}>
+        <Button className={style.loginButton} onClick={logIn}>
+          Logg inn
+        </Button>
+        <Button>Registrer</Button>
+        <Button>Glemt passord</Button>
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
