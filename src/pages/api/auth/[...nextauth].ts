c68fc9f6108c4936a261aaeae5c672c59cb25b12
FluidSense <22146437+FluidSense@users.noreply.github.com>
2020-09-25 15:02:15 +0200

    remove unneeded callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,23 +9,25 @@ const options = {
             name: 'Onlineweb4',
             type: 'oauth',
             version: '2.0',
-            scope: 'openid profile onlineweb4',
-            params: { response_type: 'code'},
+            scope: 'openid onlineweb4',
+            params: { 
+                grant_type: 'authorization_code',
+            },
             accessTokenUrl: 'https://online.ntnu.no/openid/token',
             requestTokenUrl: '"https://online.ntnu.no/openid/authorize',
-            authorizationUrl: 'https://online.ntnu.no/openid/authorize',
+            authorizationUrl: 'https://online.ntnu.no/openid/authorize?response_type=code',
             profileUrl: 'https://online.ntnu.no/openid/userinfo',
             profile: (profile: Profile) => {
+                console.log("profile", profile);
+                // https://next-auth.js.org/configuration/options#jwt-helpers
                 return {
-                    id: profile.id,
-                    name: profile.name,
-                    email: profile.email,
-                    image: profile.picture
+                    id: profile.sub,
                 }
             },
             clientId: process.env.OW4_SSO_CLIENT_ID,
             clientSecret: process.env.OW4_SSO_CLIENT_SECRET,
-            idToken: true,
+            debug: true,
+            idToken: false,
         }
     ]
 }
