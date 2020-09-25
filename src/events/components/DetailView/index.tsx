18c267612ac97b0a8724521a0a5f42dab79e4ebe
FluidSense <22146437+FluidSense@users.noreply.github.com>
2020-09-25 18:19:43 +0200

    Remove unused import

--- src/events/components/DetailView/index.tsx
+++ src/events/components/DetailView/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import Spinner from 'common/components/Spinner';
 import { DOMAIN } from 'common/constants/endpoints';
