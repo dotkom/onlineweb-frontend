dabf0b96c209efdb75b5d7f98de64ceb7b697e19
Julian Grande <juliangr@stud.ntnu.no>
2020-10-16 17:56:45 +0200

    Removed redundant components and improved code

--- src/profile/components/Settings/Membership/index.tsx
+++ src/profile/components/Settings/Membership/index.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-
+import Markdown from 'common/components/Markdown';
+import Link from 'next/link';
 import { Pane } from 'common/components/Panes';
 import { Button } from '@dotkomonline/design-system';
-import MembersInfo from './Info';
+
+const ABOUT_PROFILE_MEMBERSHIP = `
+  # Medlemskap
+
+  Du trenger medlemskap for å kunne melde deg på linje-spesifikke arrangementer.
+
+  For å søke medlemskap, bruk knappen under
+`;
 
 const Membership = () => {
   return (
     <Pane>
-      <MembersInfo></MembersInfo>
-      <Button
-        onClick={() => {
-          window.open('https://old.online.ntnu.no/profile/membership/');
-        }}
-      >
-        Medlemskap
-      </Button>
+      <Markdown source={ABOUT_PROFILE_MEMBERSHIP} />
+
+      <Link href="https://old.online.ntnu.no/profile/membership/" passHref={true}>
+        <Button>Medlemskap</Button>
+      </Link>
     </Pane>
   );
 };
