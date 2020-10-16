8ae783380bbf0098b134c918d777c6014fe2b3d2
Julian Grande <juliangr@stud.ntnu.no>
2020-10-16 16:43:11 +0200

    Code cleanup and linting

--- src/profile/components/Settings/Membership/index.tsx
+++ src/profile/components/Settings/Membership/index.tsx
@@ -4,15 +4,17 @@ import { Pane } from 'common/components/Panes';
 import { Button } from '@dotkomonline/design-system';
 import MembersInfo from './Info';
 
-function openTab() {
-  window.open('https://old.online.ntnu.no/profile/membership/');
-}
-
 const Membership = () => {
   return (
     <Pane>
       <MembersInfo></MembersInfo>
-      <Button onClick={openTab}>Medlemskap</Button>
+      <Button
+        onClick={() => {
+          window.open('https://old.online.ntnu.no/profile/membership/');
+        }}
+      >
+        Medlemskap
+      </Button>
     </Pane>
   );
 };
