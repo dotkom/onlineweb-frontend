8d4233bb7c722474b1804d7bb57d8b9055dd429d
kharann <anhkhav@gmail.com>
2020-10-16 17:57:02 +0200

    delete mail  information

--- src/profile/components/Settings/Mails/mailInformation.ts
+++ src/profile/components/Settings/Mails/mailInformation.ts
@@ -3,11 +3,6 @@ export const MAIL_INFO = `
 Her er en oversikt over e-postadresser som er tilknyttet din konto. Adressen som er markert som primær brukes til alle våre e-postlister, gjenoppretning av passord og andre varsler som sendes fra oss.
 `;
 
-export const ADD_MAIL_INFO = `
-### Legg til e-post
-Her kan du legge til nye eposter. Husk å verifisere mailen etter å ha lagt til
-`;
-
 export const PRIMARY_MAIL_INFO = `
 Din primær e-post har blitt endret. Det ta opptil én time før endringen er registert i alle mailinglister, mens adresse for andre varsler og gjenoppretning oppdateres umiddelbart.
 `;
