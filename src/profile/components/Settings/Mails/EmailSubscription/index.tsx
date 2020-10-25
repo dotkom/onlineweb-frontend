eba61e33927b54359657e2d0ca39b6dc5de92cd4
Thomhas <thomas.hasvold@gmail.com>
2020-10-25 21:30:22 +0100

    We had 2 motherfuckers onclicks and now we have one. Co-authored-by: Kharann <anhkhav@gmail.com>

--- src/profile/components/Settings/Mails/EmailSubscription/index.tsx
+++ src/profile/components/Settings/Mails/EmailSubscription/index.tsx
@@ -5,11 +5,12 @@ import { SUBSCRIPTION_INFO, OPPORTUNITIES_INFO } from '../mailInformation';
 import { getProfile, putProfile } from 'profile/api';
 import { useToast } from 'core/utils/toast/useToast';
 import { Pane } from 'common/components/Panes';
+import { IUserProfile } from 'profile/models/User';
 
 const EmailSubscription = () => {
   const [infoSubscribed, setInfoSubscribed] = useState(false);
   const [jobmailSubscribed, setJobmailSubscribed] = useState(false);
-  const [addMessage, cancelToast] = useToast({ type: 'success', duration: 5000 });
+
   useEffect(() => {
     const fetchProfile = async () => {
       const profile = await getProfile();
@@ -21,40 +22,26 @@ const EmailSubscription = () => {
     fetchProfile();
   }, [infoSubscribed, jobmailSubscribed]);
 
-  const changeInfoMail = async () => {
-    try {
-      await putProfile({ infomail: !infoSubscribed });
-      setInfoSubscribed(!infoSubscribed);
-      cancelToast();
-      addMessage(`Du har meldt deg ${!infoSubscribed ? 'på' : 'av'} infomail-lista.`);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const changeJobMail = async () => {
-    try {
-      await putProfile({ jobmail: !jobmailSubscribed });
-      setJobmailSubscribed(!jobmailSubscribed);
-      cancelToast();
-      addMessage(`Du har meldt deg ${!jobmailSubscribed ? 'på' : 'av'} muligheter-lista.`);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
   return (
     <>
       <Pane>
         <div>
           <Markdown>{SUBSCRIPTION_INFO}</Markdown>
-          <ActivateButton activated={infoSubscribed} onClick={changeInfoMail} />
+          <ActivateButton
+            activated={infoSubscribed}
+            name="infomail"
+            callback={() => setInfoSubscribed(!infoSubscribed)}
+          />
         </div>
       </Pane>
       <Pane>
         <div>
           <Markdown>{OPPORTUNITIES_INFO}</Markdown>
-          <ActivateButton activated={jobmailSubscribed} onClick={changeJobMail} />
+          <ActivateButton
+            activated={jobmailSubscribed}
+            name="jobmail"
+            callback={() => setJobmailSubscribed(!jobmailSubscribed)}
+          />
         </div>
       </Pane>
     </>
