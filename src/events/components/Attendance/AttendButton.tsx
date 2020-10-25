f58eb7caeee4db302e40708382be97253c87ad38
Thomhas <thomas.hasvold@gmail.com>
2020-10-25 13:57:35 +0100

    change attend button text when event is full

--- src/events/components/Attendance/AttendButton.tsx
+++ src/events/components/Attendance/AttendButton.tsx
@@ -9,11 +9,12 @@ import style from './attendance.less';
 
 interface IAttendButtonProps {
   eventId: number;
+  isEventFull: boolean;
 }
 
 const AttendButton: FC<IAttendButtonProps> = (props: IAttendButtonProps) => {
   const dispatch = useDispatch();
-  const { eventId } = props;
+  const { eventId, isEventFull } = props;
   const [showModal, setShowModal] = useState<boolean>(false);
   const [addToast] = useToast({ type: 'success', duration: 5000 });
 
@@ -34,7 +35,7 @@ const AttendButton: FC<IAttendButtonProps> = (props: IAttendButtonProps) => {
   return (
     <>
       <Button color="success" onClick={toggleModal} className={style.button}>
-        Meld meg på
+        Meld meg på {isEventFull ? 'venteliste' : null}
       </Button>
       {modal}
     </>
