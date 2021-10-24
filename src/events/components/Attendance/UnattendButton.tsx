83f10309d029926c9b20ed7244032a7398f35cc8
gerhargu <gerhard.gustavsen@outlook.com>
2021-10-24 14:42:53 +0200

    made a ConfirmModal component

--- src/events/components/Attendance/UnattendButton.tsx
+++ src/events/components/Attendance/UnattendButton.tsx
@@ -1,10 +1,7 @@
-import React, { FC } from 'react';
-import { useDispatch } from 'core/redux/hooks';
-import { removeAttendeeByEventId } from 'events/slices/attendees';
+import React, { FC, useState } from 'react';
 import { Button } from '@dotkomonline/design-system';
-import { useToast } from 'core/utils/toast/useToast';
-import { unwrapResult } from '@reduxjs/toolkit';
 import style from './attendance.less';
+import ConfirmModal from './ConfirmModal';
 
 interface IAttendButtonProps {
   eventId: number;
@@ -13,24 +10,24 @@ interface IAttendButtonProps {
 }
 
 const UnattendButton: FC<IAttendButtonProps> = ({ eventId, isOnWaitList, waitListNumber }) => {
-  const dispatch = useDispatch();
-  const [addToast] = useToast({ type: 'success', duration: 5000 });
-  const signOff = async () => {
-    const action = await dispatch(removeAttendeeByEventId(eventId));
-    try {
-      unwrapResult(action);
-      addToast('Du har blitt meldt av arrangementet');
-    } catch (err) {
-      addToast(`Noe gikk galt under avmeldelse, ERROR: ${err.message}`, { type: 'error' });
-    }
-  };
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const toggleModal = () => setShowModal(!showModal);
+  const modal = (
+    <ConfirmModal
+      showModal={showModal}
+      text={'Er du sikker på av du vil melde deg av?'}
+      toggleModal={toggleModal}
+      eventId={eventId}
+    />
+  );
 
   return (
     <>
       {isOnWaitList ? <p>{`Du er nummer ${waitListNumber} på venteliste.`}</p> : null}
-      <Button color="danger" onClick={signOff} className={style.button}>
+      <Button color="danger" onClick={toggleModal} className={style.button}>
         Meld meg av
       </Button>
+      {modal}
     </>
   );
 };
