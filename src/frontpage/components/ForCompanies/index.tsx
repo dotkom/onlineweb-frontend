e9536fbf423d21e308a761835945205a67af1b49
kharann <anhkhav@gmail.com>
2021-10-27 14:55:34 +0200

    feat: add contact links

--- src/frontpage/components/ForCompanies/index.tsx
+++ src/frontpage/components/ForCompanies/index.tsx
@@ -10,13 +10,17 @@ const COMPANY_TEXT = `
 Online er en linjeforening for Informatikkstudentene ved NTNU Gløshaugen.
 Informatikkstudiet hører til Institutt for datateknologi og informatikk (IDI).
 Dette innebærer blant annet å lære om utvikling, forbedring, evaluering og bruk av datasystemer.
-For mer informasjon om studiet, se NTNU sine offisielle nettsider for bachelor og master.
+For mer informasjon om studiet, se NTNU sine offisielle nettsider for [bachelor](https://www.ntnu.no/studier/bit) og [master](https://www.ntnu.no/studier/msit).
 
 Linjeforeningen drives av frivillige, og har som formål å være en sosial og faglig ressurs for studentene, samt et bindeledd mellom studentene, universitetets institusjoner og næringslivet.
 Linjeforeningen har gjennom flere år opparbeidet seg et repertoar av tjenester som vi tilbyr våre studenter.
 I samarbeid med næringslivet tilbyr vi kurs og bedriftspresentasjoner for å gi studentene våre en bredere og dypere fagkunnskap samt et innblikk i hverdagen til aktuelle arbeidsplasser.
 
 Send oss en forespørsel for å komme i kontakt. Vi gleder oss til å samarbeide med dere!
+
+- [Send epost til bedriftskontakt](mailto:bedriftskontakt@online.ntnu.no)
+- [Meld interesse gjennom interesseskjemaet](https://interesse.online.ntnu.no)
+
 `;
 
 const PROFILING = ['Bedriftspresentasjon', 'Annonse i Offline', 'Stillingsutlysning'];
