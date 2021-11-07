feb450cf6b804bc13d0ba998b2c4fe656d9343fe
Henrik Skog <henrikskog01@gmail.com>
2021-11-07 15:53:21 +0100

    alter id name

--- src/frontpage/components/ForCompanies/index.tsx
+++ src/frontpage/components/ForCompanies/index.tsx
@@ -29,7 +29,7 @@ const ACADEMIC = ['Kurs', 'Workshop'];
 
 const ForCompanies: FC = () => {
   return (
-    <div className={styles.container} id="forCompanies">
+    <div className={styles.container} id="forCompaniesContainer">
       <Heading title="Bedrifter" />
       <div className={styles.content}>
         <Markdown source={COMPANY_TEXT} />
