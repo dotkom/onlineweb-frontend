5dbc415ef240ef379531500941b2c0a5da6acac5
kharann <anhkhav@gmail.com>
2021-11-21 15:17:55 +0100

    feat: temporary solution for moving offline issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,19 @@ import React from 'react';
 import { fetchFrontPageArticles } from 'articles/slices/articles';
 import { Store } from 'core/redux/Store';
 import FrontPageComponent from 'frontpage';
+import { getAllOfflines } from 'frontpage/api/offline';
+import { IOfflineIssue } from 'frontpage/models/Offline';
 
 interface IContext extends NextPageContext {
   store: Store;
 }
 
-const FrontPage: NextPage = () => {
-  return <FrontPageComponent />;
+interface FrontPageProps {
+  offlines: IOfflineIssue[];
+}
+
+const FrontPage: NextPage<FrontPageProps> = ({ offlines }) => {
+  return <FrontPageComponent offlines={offlines} />;
 };
 
 export default FrontPage;
@@ -19,8 +25,11 @@ FrontPage.getInitialProps = async ({ store }: IContext) => {
   const state = store.getState();
   const isFrontPageArticlesPopulated = Boolean(state.articles.frontPageArticleIds.length);
   const articlesResult = store.dispatch(fetchFrontPageArticles());
+  const offlines = await getAllOfflines();
   if (!isFrontPageArticlesPopulated) {
     await articlesResult;
   }
-  return {};
+  return {
+    offlines,
+  };
 };
