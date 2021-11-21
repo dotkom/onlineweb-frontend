66fc4bdfdf707600617c7d414efa3a529e2ce5c2
kharann <anhkhav@gmail.com>
2021-11-21 16:41:22 +0100

    feat: sort the offline issues descending by release date

--- src/frontpage/api/offline.ts
+++ src/frontpage/api/offline.ts
@@ -1,7 +1,7 @@
 import { IOfflineIssue } from 'frontpage/models/Offline';
 
 const API_URL =
-  'https://wsqi2mae.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D%22offline%22%5D%20%7C%20order(release_date)%20%7B%0A%20%20_id%2C%0A%20%20title%2C%0A%20%20release_date%2C%0A%20%20%22issue%22%3A%20pdf.asset%20-%3E%20url%2C%0A%20%20%22image%22%3A%20thumbnail.asset%20-%3E%20%7B%0A%20%20%20%20url%2C%0A%20%20%20%20title%0A%20%20%7D%0A%7D&%24slug=%22building-my-blog%22';
+  'https://wsqi2mae.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D%22offline%22%5D%20%7C%20order(release_date%20desc)%20%7B%0A%20%20_id%2C%0A%20%20title%2C%0A%20%20release_date%2C%0A%20%20%22issue%22%3A%20pdf.asset%20-%3E%20url%2C%0A%20%20%22image%22%3A%20thumbnail.asset%20-%3E%20%7B%0A%20%20%20%20url%2C%0A%20%20%20%20title%0A%20%20%7D%0A%7D';
 
 export const getAllOfflines = async (): Promise<IOfflineIssue[]> => {
   const res = await fetch(API_URL, {
