3e1e473e2f18b50eebbddc7cff71625fd5d22da1
Njål Sørland <55444303+NjaalSoerland@users.noreply.github.com>
2022-08-03 14:06:01 +0200

    Added more linebreaks because OW's MD formatting is weird

--- src/profile/components/Settings/Penalties/Rules.tsx
+++ src/profile/components/Settings/Penalties/Rules.tsx
@@ -24,32 +24,39 @@ const RULES = md`
   - Å møte opp etter arrangementets start eller innslipp er ferdig.
   - Å ikke svare på tilbakemeldingsskjema innen tidsfristen.
   - Å ikke overholde betalingsfristen. Dette medfører i tillegg suspensjon fra alle Onlines arrangementer inntil betaling er gjennomført.
+  
 
   Den ansvarlige komiteen kan også foreta en skjønnsmessig vurdering som gagner deltakeren.
+
 
   ## Avmelding
 
   - Ved sykdom eller andre ekstraordinære hendelser vil man ikke få prikk ved avmelding 5 timer før arrangementsstart.
   - Alle komiteer ønsker at du melder deg av arrangementer selv om du vet dette vil medføre prikk. Dette er slik at noen andre kan bli obs på plassen sin så tidlig som mulig.
 
+
   ## Venteliste
 
   - Hvis du står på venteliste kan du melde deg av helt til arrangementet starter.
   - Når du står på venteliste er du inneforstått med at du når som helst kan få plass på arrangementet og dermed er bundet til reglene for arrangementet på lik linje med andre påmeldte.
+
 
   ## Betaling
 
   - Ved manglende betaling suspenderes man fra alle Onlines arrangementer inntil betalingen er gjennomført.
   - Ved betalt arrangement, men manglende oppmøte, vil man ikke få tilbakebetalt dersom avmelding skjer etter frist. Dersom neste på venteliste er tilgjengelig kan dette gjøres unntak for.
 
+
   ## Oppførsel
 
   - Ved upassende oppførsel under et av Onlines arrangement vil du stå økonomisk ansvarlig for eventuelle skader, og i verste fall risikere utestengelse fra alle Onlines arrangement.
+
 
   ## Bedriftsarrangementer
 
   - Ved bedriftsarrangementer åpner dørene i henhold til starttid på arrangementet. Ti minutter etter at dørene åpner slippes oppmøte på ventelisten inn dersom det er plass. 15 minutter etter at dørene åpner stenger innslippet.
   - Det kreves at en deltaker svarer på den elektroniske tilbakemeldingen etter bedriftsarrangementer. Det vil komme e-post etter arrangementet med lenke til tilbakemeldingsskjema som må besvares innen denoppgitte fristen. Dersom en deltager ikke svarer innen fristen vil dette gi en prikk.
+
 
   ## Hvorfor har jeg fått prikk?
 
