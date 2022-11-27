31f55495fcac328ae525bb0306ddcae72a9a2f7c
Terbau <bragebau@hotmail.no>
2022-11-27 02:11:57 +0100

    Fix lints

--- src/events/components/DetailView/AttendanceEvent.tsx
+++ src/events/components/DetailView/AttendanceEvent.tsx
@@ -98,10 +98,12 @@ const AttendanceEvent: FC<IProps> = ({ eventId, eventTitle }) => {
           />
         </Block>
       )}
-      {isEligibleForSignup && (isEligibleForSignup.status) && (
+      {isEligibleForSignup && isEligibleForSignup.status && (
         <div className={`${style.agreeRules} ${style.fullBlock}`}>
           <p>Ved å melde deg på godtar du</p>
-          <a href="https://old.online.ntnu.no/wiki/online/info/innsikt-og-interface/prikkeregler/">Onlines prikkeregler</a>
+          <a href="https://old.online.ntnu.no/wiki/online/info/innsikt-og-interface/prikkeregler/">
+            Onlines prikkeregler
+          </a>
         </div>
       )}
       <div className={`${style.attendanceContainer} ${style.fullBlock}`}>
