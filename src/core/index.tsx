ff2dde7c086c65221483e428f691669f9fcfcf01
Njål Sørland <njaalis2000@gmail.com>
2023-07-23 19:19:37 +0200

    splash redirect for fadderuka

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import Footer from './components/Footer/index';
 import Header from './components/Header/index';
@@ -8,18 +8,30 @@ import { ToastMessages } from './utils/toast/ToastMessages';
 import { AuthenticationProvider } from 'authentication/providers/AuthenticationProvider';
 import { SilentRenew } from 'authentication/components/SilentRenew';
 
-const Core: FC = ({ children }) => (
-  <>
-    <Head>
-      <title>Linjeforeningen Online</title>
-    </Head>
-    <Header />
-    <main>{children}</main>
-    <Footer />
-    <ToastMessages />
-    <AuthenticationProvider />
-    <SilentRenew />
-  </>
-);
+const Core: FC = ({ children }) => {
+  useEffect(() => {
+    const splash = sessionStorage.getItem('splash');
+    const isLandingPage = window.location.pathname === '/';
+    const shouldRedirect = !splash || new Date().getTime() - new Date(splash).getTime() > 1000 * 60 * 10;
+    if (shouldRedirect && isLandingPage) {
+      sessionStorage.setItem('splash', `${new Date()}`);
+      window.location.assign('https://splash.online.ntnu.no');
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Linjeforeningen Online</title>
+      </Head>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+      <ToastMessages />
+      <AuthenticationProvider />
+      <SilentRenew />
+    </>
+  );
+};
 
 export default Core;
