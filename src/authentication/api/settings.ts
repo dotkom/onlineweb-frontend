8b28e2d96096f05a9f6fbc2233f05791291ac2d3
Henrik Hørlück Berg <36937807+henrikhorluck@users.noreply.github.com>
2024-03-19 14:39:48 +0100

    Use refresh tokens

--- src/authentication/api/settings.ts
+++ src/authentication/api/settings.ts
@@ -5,12 +5,11 @@ const settings: UserManagerSettings = {
   client_id: process.env.AUTH0_CLIENT_ID || '',
   redirect_uri: `${process.env.HOSTNAME}/authentication/callback`,
   post_logout_redirect_uri: `${process.env.HOSTNAME}`,
-  scope: 'openid profile email',
+  scope: 'openid profile email offline_access',
   automaticSilentRenew: true,
   filterProtocolClaims: true,
   loadUserInfo: true,
   revokeTokensOnSignout: true,
-  revokeTokenTypes: ['refresh_token'],
 };
 
 export default settings;
