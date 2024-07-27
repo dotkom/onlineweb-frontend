781e4e675ebc097dcbb86ce2474fbd2eb901e0b0
Henrik Skog <henrikskog01@gmail.com>
2024-07-27 19:15:11 +0200

    Remove toast about old.online.ntnu.no

--- src/frontpage/index.tsx
+++ src/frontpage/index.tsx
@@ -1,25 +1,23 @@
-import EventsContainer from 'events/components/EventsContainer';
-import React, { FC } from 'react';
-import Articles from './components/Articles';
-import ForCompanies from './components/ForCompanies';
-import Offline from './components/Offline';
-import ToastOld from './components/ToastOld';
-import { IOfflineIssue } from './models/Offline';
-import AprilFoolsCaptcha from './components/AprilFoolsCaptcha';
+import EventsContainer from "events/components/EventsContainer";
+import React, { FC } from "react";
+import Articles from "./components/Articles";
+import ForCompanies from "./components/ForCompanies";
+import Offline from "./components/Offline";
+import { IOfflineIssue } from "./models/Offline";
+import AprilFoolsCaptcha from "./components/AprilFoolsCaptcha";
 
 interface FrontpageProps {
-  offlines: IOfflineIssue[];
+	offlines: IOfflineIssue[];
 }
 
 const Frontpage: FC<FrontpageProps> = ({ offlines }) => (
-  <>
-    <AprilFoolsCaptcha />
-    <ToastOld />
-    <EventsContainer />
-    <Articles />
-    <Offline issues={offlines} />
-    <ForCompanies />
-  </>
+	<>
+		<AprilFoolsCaptcha />
+		<EventsContainer />
+		<Articles />
+		<Offline issues={offlines} />
+		<ForCompanies />
+	</>
 );
 
 export default Frontpage;
