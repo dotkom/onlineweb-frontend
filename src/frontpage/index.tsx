f1d2f62a773cacb9bf16085ab80912e4fbfbb555
Henrik Skog <henrikskog01@gmail.com>
2024-08-26 20:02:18 +0200

    Remove fadderukene message on front page

--- src/frontpage/index.tsx
+++ src/frontpage/index.tsx
@@ -13,25 +13,6 @@ interface FrontpageProps {
 const Frontpage: FC<FrontpageProps> = ({ offlines }) => (
   <>
     <AprilFoolsCaptcha />
-    <div
-      style={{
-        padding: '20px',
-        borderRadius: '8px',
-        textAlign: 'center',
-        boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
-        marginBottom: '20px',
-      }}
-    >
-      <p
-        style={{
-          fontSize: '1.1rem',
-        }}
-      >
-        Ser du etter informasjon om fadderukene?
-        <br />
-        <a href="https://splash.online.ntnu.no/">Gå til programmet</a>
-      </p>
-    </div>
     <EventsContainer />
     <Articles />
     <Offline issues={offlines} />
