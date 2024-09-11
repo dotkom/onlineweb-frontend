b6e56e5161570d72640763dd255a8a0e18b89fba
André Klarpås <andrefkl@ntnu.no>
2024-09-11 20:50:27 +0200

    Remove profile image

--- src/profile/components/Profile/Header.tsx
+++ src/profile/components/Profile/Header.tsx
@@ -1,17 +1,14 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 
-import DEFAULT_USER_IMAGE from 'common/img/profile/user.png';
 import { ProfilePageContext } from 'profile/providers/ProfilePage';
 
 import style from '../../less/profile.less';
 
 export const Header = () => {
-  const { first_name, last_name, image } = useContext(ProfilePageContext);
+  const { first_name, last_name } = useContext(ProfilePageContext);
   const fullName = `${first_name} ${last_name}`;
-  const imgSrc = image || DEFAULT_USER_IMAGE;
   return (
     <div className={style.header}>
-      <img className={style.profileImage} src={imgSrc} />
       <h1>{fullName}</h1>
     </div>
   );
