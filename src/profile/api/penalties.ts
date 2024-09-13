ac7f890ea8b6db933222beba87390a8dc9416bbc
brage-andreas <bragerino+github@gmail.com>
2024-09-13 13:16:49 +0200

    feat: add `getMarkRules`

--- src/profile/api/penalties.ts
+++ src/profile/api/penalties.ts
@@ -1,10 +1,12 @@
 import { get, IAPIData } from 'common/utils/api';
 
-import { IMark, ISuspension } from '../models/Penalty';
+import { IMark, IMarkRule, ISuspension } from '../models/Penalty';
 import { getUser } from 'authentication/api';
 
-const API_URL = '/api/v1/profile';
+const BASE_API_URL = '/api/v1';
+const API_URL = BASE_API_URL + '/profile';
 const MARKS_URL = API_URL + '/marks/';
+const MARK_RULES_URL = BASE_API_URL + '/marks/rule-sets/';
 const SUSPENSIONS_URL = API_URL + '/suspensions/';
 
 /**
@@ -13,6 +15,14 @@ const SUSPENSIONS_URL = API_URL + '/suspensions/';
 export const getMarks = async (): Promise<IMark[]> => {
   const user = await getUser();
   const { results } = await get<IAPIData<IMark>>(MARKS_URL, { format: 'json' }, { user });
+  return results;
+};
+
+/**
+ * @summary Fetch Mark Rules from API.
+ */
+export const getMarkRules = async (): Promise<IMarkRule[]> => {
+  const { results } = await get<IAPIData<IMarkRule>>(MARK_RULES_URL, { format: 'json' });
   return results;
 };
 
