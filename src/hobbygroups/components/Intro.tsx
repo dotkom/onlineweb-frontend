75f4b03beea7d11b68d9ddf414527659f6622ab6
henrikskog <henrikskog01@gmail.com>
2024-10-02 18:47:55 +0200

    Add buttons for new forms for hobby groups

--- src/hobbygroups/components/Intro.tsx
+++ src/hobbygroups/components/Intro.tsx
@@ -1,20 +1,13 @@
 import Markdown from 'common/components/Markdown';
 import React from 'react';
 import style from '../less/hobbygroups.less';
+import { Button } from '@dotkomonline/design-system';
 
 const introText = `
   På denne siden finner du informasjon om alle de forskjellige interessegruppene i online. Ser du noe som ser
   interessant ut? Ta kontakt og møt noen med samme interesser som deg. Interessegruppene i Online er grupper for
   alle mulige slags interesser. Har du og en kompis eller to en sær/stilig/fantastisk interesse? Opprett en
   interessegruppe!
-
-  ## Hvordan opprette en interessegruppe?
-
-  For å opprette en ny interessegruppe, eller ta over en inaktiv en, send en mail til <backlog@online.ntnu.no>
-
-  ## Hvordan søke om støtte til din interessegruppe?
-
-  Du kan søke om økonomisk støtte til din interessegruppe ved å sende en mail til <backlog@online.ntnu.no>
 
   Mer informasjon om hvordan dette gjøres finnes
   [her](https://wiki.online.ntnu.no/info/innsikt-og-interface/interessegrupper/).
@@ -23,6 +16,22 @@ const introText = `
 const Intro = () => (
   <div className={style.intro}>
     <Markdown source={introText} />
+    <a
+      href="https://docs.google.com/forms/d/e/1FAIpQLSebaBslZ3nmh2wubQ_mPJYYU2XNIRlJZ1BooFuH7y6wxylaWA/viewform"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Button className={style.leftbtn} aria-label="Opprett interessegruppe">
+        Opprett interessegruppe
+      </Button>
+    </a>
+    <a
+      href="https://docs.google.com/forms/d/e/1FAIpQLScr27q7C4gDvzHXajydznfFxPs7JaGpgYrNX4RPiVRvUHXVGg/viewform?pli=1"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Button aria-label="Søk om støtte">Søk om støtte</Button>
+    </a>
   </div>
 );
 
